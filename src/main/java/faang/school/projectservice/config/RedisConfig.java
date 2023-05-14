e124a071abfb122cd85cbde0632ee4a1e191e1d7
flyfalldie <kolya_cypandin@mail.ru>
2023-05-15 01:51:42 +0300

    Redis channels config update, now supports many channels

--- src/main/java/faang/school/projectservice/config/RedisConfig.java
+++ src/main/java/faang/school/projectservice/config/RedisConfig.java
@@ -20,7 +20,7 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("${spring.data.redis.channel}")
+    @Value("${spring.data.redis.channels.calculations_channel.name}")
     private String channel;
 
     /**
