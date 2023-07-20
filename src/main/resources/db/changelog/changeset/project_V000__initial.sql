84a7bca54ff8bd6ec0c625a7fa5f6c5f92890967
flyfalldie <kolya_cypandin@mail.ru>
2023-07-21 07:19:54 +0900

    cherry-pick-19052: subprojects added

--- src/main/resources/db/changelog/changeset/project_V000__initial.sql
+++ src/main/resources/db/changelog/changeset/project_V000__initial.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS project
     id               BIGSERIAL PRIMARY KEY,
     name             VARCHAR(128) NOT NULL,
     description      VARCHAR(4096),
+    parent_project_id        BIGINT,
     storage_size     BIGINT,
     max_storage_size BIGINT,
     owner_id         BIGINT,
@@ -10,7 +11,8 @@ CREATE TABLE IF NOT EXISTS project
     updated_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status           VARCHAR(255) NOT NULL,
     visibility       VARCHAR(255) NOT NULL,
-    cover_image_id   VARCHAR(255)
+    cover_image_id   VARCHAR(255),
+    CONSTRAINT fk_project_parent FOREIGN KEY (parent_project_id) REFERENCES project (id)
 );
 
 CREATE TABLE IF NOT EXISTS team
