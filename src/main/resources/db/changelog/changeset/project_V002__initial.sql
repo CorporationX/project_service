30e9d88ed5f45c8436d6599b10dfd109e14cff2b
flyfalldie <kolya_cypandin@mail.ru>
2023-07-21 04:55:22 +0900

    cherry-pick-13512: project_team and vacancy fix

--- src/main/resources/db/changelog/changeset/project_V002__initial.sql
+++ src/main/resources/db/changelog/changeset/project_V002__initial.sql
@@ -1,77 +1,80 @@
-CREATE TABLE IF NOT EXISTS team_member (
-    id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    team_id BIGINT
+CREATE TABLE IF NOT EXISTS project
+(
+    id               BIGSERIAL PRIMARY KEY,
+    name             VARCHAR(128) NOT NULL,
+    description      VARCHAR(4096),
+    storage_size     BIGINT,
+    max_storage_size BIGINT,
+    owner_id         BIGINT,
+    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status           VARCHAR(255) NOT NULL,
+    cover_image_id   VARCHAR(255)
 );
+
+CREATE TABLE IF NOT EXISTS team
+(
+    id         BIGSERIAL PRIMARY KEY,
+    project_id BIGINT NOT NULL,
+    CONSTRAINT fk_team_project FOREIGN KEY (project_id) REFERENCES project (id)
+);
+
+CREATE TABLE IF NOT EXISTS team_member
+(
+    id      BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    team_id BIGINT NOT NULL,
+    CONSTRAINT fk_team_member_team FOREIGN KEY (team_id) REFERENCES team (id)
+);
+
 create table if not exists team_member_roles
 (
     team_member_id bigint,
-    role varchar(20)
+    role           varchar(20) NOT NULL,
+    CONSTRAINT fk_team_member_roles_team_member FOREIGN KEY (team_member_id) REFERENCES team_member (id)
 );
 
-CREATE TABLE IF NOT EXISTS team (
-    id BIGSERIAL PRIMARY KEY,
-    project_id BIGINT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS project (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(128) NOT NULL,
-    description VARCHAR(4096),
-    storage_size BIGINT,
-    max_storage_size BIGINT,
-    owner_id BIGINT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(255) NOT NULL,
-    cover_image_id VARCHAR(255),
-    team_id BIGINT,
-    CONSTRAINT fk_owner
-        FOREIGN KEY (owner_id) REFERENCES team_member(id),
-    CONSTRAINT fk_team
-        FOREIGN KEY (team_id) REFERENCES team(id)
-);
-
-CREATE TABLE IF NOT EXISTS resource (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    key VARCHAR(255),
-    type VARCHAR(255),
-    status VARCHAR(255),
+CREATE TABLE IF NOT EXISTS resource
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    key        VARCHAR(255),
+    type       VARCHAR(255),
+    status     VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     project_id BIGINT,
-    size BIGINT,
+    size       BIGINT,
     CONSTRAINT fk_project
-        FOREIGN KEY (project_id) REFERENCES project(id)
+        FOREIGN KEY (project_id) REFERENCES project (id)
 );
 
 CREATE TABLE IF NOT EXISTS task (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    status VARCHAR(255),
-    performer_user_id BIGINT NOT NULL,
-    reporter_user_id BIGINT NOT NULL,
-    minutes_tracked INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    parent_task_id BIGINT,
-    project_id BIGINT,
-    stage_id BIGINT,
-    CONSTRAINT fk_parent_task
-        FOREIGN KEY (parent_task_id) REFERENCES task(id),
-    CONSTRAINT fk_project
-        FOREIGN KEY (project_id) REFERENCES project(id)
+                                    id BIGSERIAL PRIMARY KEY,
+                                    name VARCHAR(255) NOT NULL,
+                                    description TEXT,
+                                    status VARCHAR(255),
+                                    performer_user_id BIGINT NOT NULL,
+                                    reporter_user_id BIGINT NOT NULL,
+                                    minutes_tracked INT,
+                                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                    parent_task_id BIGINT,
+                                    project_id BIGINT,
+                                    stage_id BIGINT,
+                                    CONSTRAINT fk_parent_task
+                                        FOREIGN KEY (parent_task_id) REFERENCES task(id),
+                                    CONSTRAINT fk_project
+                                        FOREIGN KEY (project_id) REFERENCES project(id)
 );
 
 CREATE TABLE IF NOT EXISTS schedule (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    project_id BIGINT,
-    CONSTRAINT fk_project
-        FOREIGN KEY (project_id) REFERENCES project(id)
+                                        id BIGSERIAL PRIMARY KEY,
+                                        name VARCHAR(255) NOT NULL,
+                                        description VARCHAR(255),
+                                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                        project_id BIGINT,
+                                        CONSTRAINT fk_project
+                                            FOREIGN KEY (project_id) REFERENCES project(id)
 );
