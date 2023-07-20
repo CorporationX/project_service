a5735c8b00580329362227604e56ccc78260dcae
flyfalldie <kolya_cypandin@mail.ru>
2023-07-21 04:55:22 +0900

    cherry-pick-13512: project_team and vacancy fix

--- src/main/java/faang/school/projectservice/model/Team.java
+++ src/main/java/faang/school/projectservice/model/Team.java
@@ -1,18 +1,28 @@
 package faang.school.projectservice.model;
 
-import jakarta.persistence.*;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.List;
 
-@Builder
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name = "team")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +32,7 @@ public class Team {
     @Column(name = "team_member_id")
     private List<TeamMember> teamMembers;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "project_id")
     private Project project;
 }
