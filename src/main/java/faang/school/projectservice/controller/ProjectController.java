7fe87b8354edd8a71315d8d6447a4a62c1a86bed
Dregid <ufl852@gmail.com>
2023-07-21 09:28:25 +0600

    Controller and service base classes are implemented - Auxiliary classes implemented

---
+++ src/main/java/faang/school/projectservice/controller/ProjectController.java
@@ -0,0 +1,22 @@
+package faang.school.projectservice.controller;
+
+import faang.school.projectservice.dto.ProjectDto;
+import faang.school.projectservice.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/projects")
+public class ProjectController {
+    private final ProjectService projectService;
+
+/*    @GetMapping
+    public List<ProjectDto> getAllProjects() {
+        return projectService.getAllProjects();
+    }*/
+}
---
+++ src/main/java/faang/school/projectservice/dto/TeamMemberDto.java
@@ -0,0 +1,15 @@
+package faang.school.projectservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamMemberDto {
+    private Long id;
+    private Long userId;
+}
