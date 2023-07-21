e7d38a2d92768f668356fde117c72ce855153b3b
ZLATA <585zlata@mail.ru>
2023-07-21 23:40:37 +0300

    commit

--- src/main/java/faang/school/projectservice/controller/InternshipController.java
+++ src/main/java/faang/school/projectservice/controller/InternshipController.java
@@ -1,9 +1,31 @@
 package faang.school.projectservice.controller;
 
+import faang.school.projectservice.dto.client.InternshipDto;
+import faang.school.projectservice.service.InternshipService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class InternshipController {
+
+    private final InternshipService internshipService;
+
+    public InternshipDto internshipCreation(InternshipDto internshipDto) {
+        if (!internshipValidation(internshipDto)) {
+            throw new IllegalArgumentException("Validation parameters did not passed!");
+        }
+        return internshipService.internshipCreation(internshipDto);
+    }
+
+    private boolean internshipValidation(InternshipDto internshipDto) {
+        return internshipDto.getName() != null &&
+                internshipDto.getStartDate() != null &&
+                internshipDto.getEndDate() != null &&
+                internshipDto.getDescription() != null &&
+                internshipDto.getMentorId() != null &&
+                internshipDto.getInternsId() != null &&
+                internshipDto.getProjectId() != null &&
+                internshipDto.getStatus() != null;
+    }
 }
--- src/main/java/faang/school/projectservice/service/InternshipService.java
+++ src/main/java/faang/school/projectservice/service/InternshipService.java
@@ -2,6 +2,8 @@ package faang.school.projectservice.service;
 
 import faang.school.projectservice.dto.client.InternshipDto;
 import faang.school.projectservice.jpa.TeamMemberJpaRepository;
+import faang.school.projectservice.mapper.InternshipMapper;
+import faang.school.projectservice.repository.InternshipRepository;
 import faang.school.projectservice.repository.TeamMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,18 +15,31 @@ import java.time.Period;
 public class InternshipService {
     private final TeamMemberJpaRepository teamMemberJpaRepository;
     private final TeamMemberRepository teamMemberRepository;
+    private final InternshipRepository internshipRepository;
+    private final InternshipMapper internshipMapper;
 
-
-    private boolean internshipValidation(InternshipDto internshipDto) {
-        return internshipDto.getProjectId() != null &&
-                internshipDto.getProjectId() < 2 &&
-                internshipDto.getInternsId().size() > 0 &&
-                Period.between(internshipDto.getStartDate().toLocalDate(),
-                        internshipDto.getEndDate().toLocalDate()).getMonths() < 3 &&
-        (teamMemberJpaRepository.findByUserIdAndProjectId(internshipDto.getMentorId(), internshipDto.getProjectId()) != null);
+    public InternshipDto internshipCreation(InternshipDto internshipDto) {
+        if (internshipDto.getProjectId() == null ||
+                internshipDto.getProjectId() > 1 &&
+                        internshipDto.getProjectId() < 1
+        ) {
+            throw new IllegalArgumentException("Invalid project id");
+        }
+        if (internshipDto.getInternsId().size() == 0) {
+            throw new IllegalArgumentException("Empty list of interns");
+        }
+        if (Period.between(internshipDto.getStartDate().toLocalDate(),
+                internshipDto.getEndDate().toLocalDate()).getMonths() > 3) {
+            throw new IllegalArgumentException("Invalid internship period");
+        }
+        if (teamMemberJpaRepository.findByUserIdAndProjectId(internshipDto.getMentorId(),
+                internshipDto.getProjectId()) == null) {
+            throw new IllegalArgumentException("Invalid mentor");
+        }
+        return internshipMapper.toInternshipDto(internshipRepository.save(internshipMapper.toInternship(internshipDto)));
     }
 
-    private updateInternship (InternshipDto internshipDto) {
+    private updateInternship(InternshipDto internshipDto) {
         if (internshipValidation(internshipDto)) {
             teamMemberRepository.updateInternship(internshipDto);
 
@@ -32,9 +47,14 @@ public class InternshipService {
         }
     }
 
-
-
-
+    //private boolean internshipValidation(InternshipDto internshipDto) {
+//        return internshipDto.getProjectId() != null &&
+//                internshipDto.getProjectId() < 2 &&
+//                internshipDto.getInternsId().size() > 0 &&
+//                Period.between(internshipDto.getStartDate().toLocalDate(),
+//                        internshipDto.getEndDate().toLocalDate()).getMonths() < 3 &&
+//        (teamMemberJpaRepository.findByUserIdAndProjectId(internshipDto.getMentorId(), internshipDto.getProjectId()) != null);
+//    }
 
 
 }
