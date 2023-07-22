dc64fb23e32e86854a4f712f99c8e9c9df12706c
Dobrovolsky Artem <artdobrov@mail.ru>
2023-07-23 00:02:57 +0300

    BC-4044: Отправить приглашение

---
+++ src/main/java/faang/school/projectservice/service/stage_invitation/StageInvitationService.java
@@ -0,0 +1,39 @@
+package faang.school.projectservice.service.stage_invitation;
+
+import faang.school.projectservice.dto.stage_invitation.StageInvitationDto;
+import faang.school.projectservice.exception.DataValidationException;
+import faang.school.projectservice.mapper.stage_invitation.StageInvitationMapper;
+import faang.school.projectservice.model.TeamMember;
+import faang.school.projectservice.model.stage.Stage;
+import faang.school.projectservice.repository.StageInvitationRepository;
+import faang.school.projectservice.repository.StageRepository;
+import faang.school.projectservice.repository.TeamMemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+@Data
+@Service
+public class StageInvitationService {
+    private final StageInvitationRepository SIRepository;
+    private final StageRepository stageRepository;
+    private final TeamMemberRepository TMRepository;
+    private final StageInvitationMapper mapper;
+
+    public StageInvitationDto create(StageInvitationDto invitationDto){
+        validate(invitationDto);
+        return mapper.toDTO(SIRepository.save(mapper.toModel(invitationDto)));
+    }
+
+    private void validate(StageInvitationDto invitationDto){
+        Stage stage = stageRepository.getById(invitationDto.getStageId());
+        TMRepository.findById(invitationDto.getInvitedId());
+        TMRepository.findById(invitationDto.getAuthorId());
+        if(!hasStageExecutor(stage, invitationDto.getAuthorId())){
+            throw new DataValidationException("Author is not executor of stage");
+        }
+    }
+    private boolean hasStageExecutor(Stage stage, long executorId){
+        return stage.getExecutors().stream().anyMatch(executor -> executor.getId() == executorId);
+    }
+}
