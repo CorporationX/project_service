778c3246c7169f5a631f0d5ae275f1fc07f7fbfc
Dobrovolsky Artem <artdobrov@mail.ru>
2023-07-23 00:02:57 +0300

    BC-4044: Отправить приглашение

---
+++ src/main/java/faang/school/projectservice/mapper/stage_invitation/StageInvitationMapper.java
@@ -0,0 +1,34 @@
+package faang.school.projectservice.mapper.stage_invitation;
+
+import faang.school.projectservice.dto.stage_invitation.StageInvitationDto;
+import faang.school.projectservice.model.stage_invitation.StageInvitation;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface StageInvitationMapper {
+    @Mapping(source = "stage.stageId", target = "stageId")
+    @Mapping(source = "author.id", target = "authorId")
+    @Mapping(source = "invited.id", target = "invitedId")
+    StageInvitationDto toDTO(StageInvitation stageInvitation);
+
+    @Mapping(source = "stageId", target = "stage.stageId")
+    @Mapping(source = "authorId", target = "author.id")
+    @Mapping(source = "invitedId", target = "invited.id")
+    StageInvitation toModel(StageInvitationDto stageInvitationDto);
+
+    @Mapping(source = "stage.stageId", target = "stageId")
+    @Mapping(source = "author.id", target = "authorId")
+    @Mapping(source = "invited.id", target = "invitedId")
+    List<StageInvitationDto> toDTOList(List<StageInvitation> stageInvitationList);
+
+    @Mapping(source = "stageId", target = "stage.stageId")
+    @Mapping(source = "authorId", target = "author.id")
+    @Mapping(source = "invitedId", target = "invited.id")
+    List<StageInvitation> toModelList(List<StageInvitationDto> stageInvitationDtoList);
+
+
+}
