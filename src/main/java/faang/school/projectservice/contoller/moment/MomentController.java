6cee8c0202ec8f6e8b454be5a8c20e0105e4b6ca
Devilserk <demonserker@gmail.com>
2023-07-23 19:07:06 +0600

    Поправил MomentController

--- src/main/java/faang/school/projectservice/contoller/moment/MomentController.java
+++ src/main/java/faang/school/projectservice/contoller/moment/MomentController.java
@@ -2,16 +2,17 @@ package faang.school.projectservice.contoller.moment;
 
 import faang.school.projectservice.dto.moment.MomentDto;
 import faang.school.projectservice.service.moment.MomentService;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MomentController {
-    private MomentService momentService;
+    private final MomentService momentService;
 
     @PostMapping("/moments")
-    public ResponseEntity<MomentDto> create(MomentDto momentDto) {
-        return ResponseEntity.ok(momentService.create(momentDto));
+    public MomentDto create(MomentDto momentDto) {
+        return momentService.create(momentDto);
     }
 }
