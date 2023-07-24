5bd9cda9530ce7e246815d00603c02066471499c
Gordeens <gordeenvs@yandex.ru>
2023-07-24 07:44:07 +0300

    BC-4006. Init structure.

---
+++ src/main/java/faang/school/projectservice/dto/vacancy/VacancyDto.java
@@ -0,0 +1,7 @@
+package faang.school.projectservice.dto.vacancy;
+
+import lombok.Data;
+
+@Data
+public class VacancyDto {
+}
---
+++ src/main/java/faang/school/projectservice/service/vacancy/VacancyService.java
@@ -0,0 +1,11 @@
+package faang.school.projectservice.service.vacancy;
+
+import faang.school.projectservice.dto.vacancy.VacancyDto;
+import jakarta.transaction.Transactional;
+
+public class VacancyService {
+    @Transactional
+    public VacancyDto createVacancy(VacancyDto vacancyDto) {
+
+    }
+}
