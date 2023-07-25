35796bcbb2b05bb130f157cbb2e29d141cdc5d5b
zhugalcf <zhugalcf@gmail.com>
2023-07-25 14:01:20 +0300

    Conflict

--- src/main/java/faang/school/projectservice/service/ProjectService.java
+++ src/main/java/faang/school/projectservice/service/ProjectService.java
@@ -24,7 +24,7 @@ public class ProjectService {
         String projectName = projectDto.getName();
         if (projectRepository.existsByOwnerUserIdAndName(ownerId, projectName)) {
             throw new DataAlreadyExistingException(String
-                    .format("User with id: %d already exist project %s",ownerId, projectName));
+                    .format("User conflict2 with id: %d already exist project %s",ownerId, projectName));
         }
         Project project = projectMapper.toModel(projectDto);
         LocalDateTime now = LocalDateTime.now();
