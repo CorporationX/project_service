9b9ad40280675af586d374e0cfa4bb436d0ea835
Devilserk <demonserker@gmail.com>
2023-07-26 11:35:04 +0600

    Добавил методы в контроллер

--- src/main/java/faang/school/projectservice/contoller/moment/MomentController.java
+++ src/main/java/faang/school/projectservice/contoller/moment/MomentController.java
@@ -3,9 +3,14 @@ package faang.school.projectservice.contoller.moment;
 import faang.school.projectservice.dto.moment.MomentDto;
 import faang.school.projectservice.service.moment.MomentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/moments")
@@ -13,8 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class MomentController {
     private final MomentService momentService;
 
-    @PostMapping("/")
+    @PostMapping
     public MomentDto create(MomentDto momentDto) {
         return momentService.create(momentDto);
+    }
+
+    @GetMapping("/{id}")
+    public MomentDto getMomentById(long momentId) {
+        return momentService.getMomentById(momentId);
+    }
+
+    @GetMapping
+    public List<MomentDto> getAllMoments() {
+        return momentService.getAllMoments();
+    }
+
+    @GetMapping
+    public List<MomentDto> getMomentsByDate(LocalDateTime startDate, LocalDateTime endDate) {
+        return momentService.getMomentsByDate(startDate, endDate);
+    }
+
+    @GetMapping
+    public List<MomentDto> getMomentsByProjects(List<Long> projectIds) {
+        return momentService.getMomentsByProjects(projectIds);
+    }
+
+    @PutMapping("/{id}")
+    public MomentDto update(MomentDto source) {
+        return momentService.update(source);
     }
 }
