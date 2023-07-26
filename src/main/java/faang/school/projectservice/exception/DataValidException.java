e4f8092ffc996af22bd2d384917d859a8aeea39f
Devilserk <demonserker@gmail.com>
2023-07-26 11:52:39 +0600

    Revert "Feature bc 3870 Моменты (#69)"
    This reverts commit acf0649ccfc8e122c725d78fda3d8ba80db664eb.

--- src/main/java/faang/school/projectservice/exception/DataValidException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.projectservice.exception;
-
-public class DataValidException extends RuntimeException {
-    public DataValidException(String message) {
-        super(message);
-    }
-}
