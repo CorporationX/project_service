aa92ded690a58ad55b41809a9d5ba19854c1e78b
RasulMurtuzaliev <ras8705@mail.ru>
2023-07-27 18:33:44 +0500

    changed endpoint for updateSubProject method

--- src/main/java/faang/school/projectservice/controller/SubProjectController.java
+++ src/main/java/faang/school/projectservice/controller/SubProjectController.java
@@ -5,6 +5,7 @@ import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class SubProjectController {
         return projectService.createSubProject(projectDto);
     }
 
-    @PostMapping("/update-subproject")
+    @PutMapping("/update-subproject")
     public ProjectDto updateSubProject(@RequestBody ProjectDto projectDto) {
         validateSubProject(projectDto);
         return projectService.updateSubProject(projectDto);
