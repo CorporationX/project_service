aef33e9bd15a824b6cba42f31f5536bbf62bde0f
An Dima <an.dima@slexn.com>
2023-07-27 18:12:09 +0900

    init

--- src/main/java/faang/school/projectservice/controller/project/ProjectController.java
+++ src/main/java/faang/school/projectservice/controller/project/ProjectController.java
@@ -1,13 +1,38 @@
 package faang.school.projectservice.controller.project;
 
+import faang.school.projectservice.dto.project.ProjectFilterDto;
+import faang.school.projectservice.dto.project.ResponseProjectDto;
 import faang.school.projectservice.service.ProjectService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/project")
 public class ProjectController {
     private final ProjectService projectService;
+
+    @PostMapping("/byFilter")
+    public ResponseProjectDto getAllByFilter(@Valid @RequestBody ProjectFilterDto dto) {
+        return projectService.getAllByFilter(dto);
+    }
+
+    @GetMapping()
+    public List<ResponseProjectDto> getAll() {
+        return projectService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseProjectDto getById(@PathVariable Long id) {
+        return projectService.getById(id);
+    }
 }
--- src/main/java/faang/school/projectservice/service/ProjectService.java
+++ src/main/java/faang/school/projectservice/service/ProjectService.java
@@ -1,11 +1,24 @@
 package faang.school.projectservice.service;
 
+import faang.school.projectservice.dto.project.ProjectFilterDto;
+import faang.school.projectservice.dto.project.ResponseProjectDto;
 import faang.school.projectservice.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
+
+    public ResponseProjectDto getAllByFilter(ProjectFilterDto dto) {
+    }
+
+    public List<ResponseProjectDto> getAll() {
+    }
+
+    public ResponseProjectDto getById(Long id) {
+    }
 }
