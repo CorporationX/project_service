0cb8cefa61c2b27e1b6c4f89e0e36b179c418675
An Dima <an.dima@slexn.com>
2023-07-27 10:32:22 +0900

    to merge

--- src/main/java/faang/school/projectservice/service/InternshipService.java
+++ src/main/java/faang/school/projectservice/service/InternshipService.java
@@ -15,6 +15,7 @@ import faang.school.projectservice.model.Project;
 import faang.school.projectservice.model.Task;
 import faang.school.projectservice.model.TaskStatus;
 import faang.school.projectservice.model.TeamMember;
+import faang.school.projectservice.model.TeamRole;
 import faang.school.projectservice.repository.InternshipRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -54,46 +56,19 @@ public class InternshipService {
         return internshipMapper.entityToResponseDto(internshipRepository.save(internship));
     }
 
-    private static void validateCreateDto(CreateInternshipDto dto, TeamMember mentor) {
-        if (Period.between(dto.getStartDate().toLocalDate(), dto.getEndDate().toLocalDate()).getMonths() > 3) {
-            throw new IllegalArgumentException("The duration of the internship should not exceed 3 months");
-        }
-        if (mentor == null) {
-            throw new IllegalArgumentException("The internship should contain mentor from project");
-        }
-    }
-
     @Transactional
     public ResponseInternshipDto update(UpdateInternshipDto dto) {
         Internship internship = internshipRepository.findById(dto.getId())
                 .orElseThrow(() -> new IllegalArgumentException("The internship not found"));
         Project project = internship.getProject();
 
-        giveProjectRole(dto, internship, project);
+        finishInternship(dto, internship, project);
         internship.setStatus(dto.getStatus());
         internship.setUpdatedAt(LocalDateTime.now());
         internship.setName(dto.getName());
         internship.setUpdatedBy(dto.getUpdatedBy());
 
         return internshipMapper.entityToResponseDto(internship);
-    }
-
-    private void finishInternship(UpdateInternshipDto dto, Internship internship, Project project) {
-        if (dto.getStatus().equals(InternshipStatus.COMPLETED) && internship.getStatus().equals(InternshipStatus.IN_PROGRESS)) {
-            List<TeamMember> interns = internship.getInterns();
-            for (TeamMember intern : interns) {
-                List<TaskStatus> taskOfIntern =
-                        taskRepository.findAllByProjectIdAndPerformerUserId(internship.getProject().getId(), intern.getId())
-                                .stream()
-                                .map(Task::getStatus)
-                                .toList();
-                if (taskOfIntern.stream().allMatch(task -> task.equals(TaskStatus.DONE))) {
-                    intern.finishInternship();
-                } else {
-                    project.getTeams().forEach(team -> team.getTeamMembers().remove(intern));
-                }
-            }
-        }
     }
 
     @Transactional(readOnly = true)
@@ -166,22 +141,7 @@ public class InternshipService {
         intern.setRoles(roles);
     }
 
-    private static void validateCreateDto(CreateInternshipDto dto, TeamMember mentor) {
-        if (dto.getName().isBlank()) {
-            throw new IllegalArgumentException("Name shouldn't be blank");
-        }
-        if (dto.getInternIds() == null || dto.getInternIds().isEmpty()) {
-            throw new IllegalArgumentException("The internship should contains interns");
-        }
-        if (Period.between(dto.getStartDate().toLocalDate(), dto.getEndDate().toLocalDate()).getMonths() > 3) {
-            throw new IllegalArgumentException("The duration of the internship should not exceed 3 months");
-        }
-        if (mentor == null) {
-            throw new IllegalArgumentException("The internship should contain mentor from project");
-        }
-    }
-
-    private void giveProjectRole(UpdateInternshipDto dto, Internship internship, Project project) {
+    private void finishInternship(UpdateInternshipDto dto, Internship internship, Project project) {
         if (dto.getStatus().equals(InternshipStatus.COMPLETED) && internship.getStatus().equals(InternshipStatus.IN_PROGRESS)) {
             List<TeamMember> interns = internship.getInterns();
             for (TeamMember intern : interns) {
@@ -196,6 +156,15 @@ public class InternshipService {
                     project.getTeams().forEach(team -> team.getTeamMembers().remove(intern));
                 }
             }
+        }
+    }
+
+    private void validateCreateDto(CreateInternshipDto dto, TeamMember mentor) {
+        if (Period.between(dto.getStartDate().toLocalDate(), dto.getEndDate().toLocalDate()).getMonths() > 3) {
+            throw new IllegalArgumentException("The duration of the internship should not exceed 3 months");
+        }
+        if (mentor == null) {
+            throw new IllegalArgumentException("The internship should contain mentor from project");
         }
     }
 }
