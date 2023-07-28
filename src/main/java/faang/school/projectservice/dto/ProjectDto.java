ccec6b2a4be3d84ef68ff4c2ac71b99ee4975dc3
Pashkevich <ppavel012100@gmail.com>
2023-07-28 18:49:04 +0200

    Rework ProjectDto

--- src/main/java/faang/school/projectservice/dto/ProjectDto.java
+++ src/main/java/faang/school/projectservice/dto/ProjectDto.java
@@ -1,18 +1,10 @@
 package faang.school.projectservice.dto;
 
 import faang.school.projectservice.model.ProjectStatus;
-import faang.school.projectservice.model.Resource;
-import faang.school.projectservice.model.Schedule;
-import faang.school.projectservice.model.Task;
-import faang.school.projectservice.model.Team;
-import faang.school.projectservice.model.TeamMember;
-import faang.school.projectservice.model.stage.Stage;
 import lombok.Builder;
 import lombok.Data;
 
-import java.math.BigInteger;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @Builder
@@ -20,17 +12,9 @@ public class ProjectDto {
     private Long id;
     private String name;
     private String description;
-//    private BigInteger storageSize;
-//    private BigInteger maxStorageSize;
     private Long ownerId;
-//    private List<Task> tasks;
-//    private List<Resource> resources;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private ProjectStatus status;
     private boolean privateProject;
-//    private String coverImageId;
-//    private Team team;
-//    private Schedule schedule;
-//    private List<Stage> stages; //TODO: Подумать какие можно убрать
 }
