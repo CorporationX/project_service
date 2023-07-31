b90917839b15ff3dcd743cbb58d44568ebf2a9b7
Ivan Averkov <averkovivan@gmail.com>
2023-07-31 09:48:36 +0300

    imCommit1

--- src/main/java/faang/school/projectservice/controller/vacancy/VacancyController.java
+++ src/main/java/faang/school/projectservice/controller/vacancy/VacancyController.java
@@ -1,5 +1,7 @@
 package faang.school.projectservice.controller.vacancy;
 
+import faang.school.projectservice.dto.vacancy.VacancyDto;
+import faang.school.projectservice.srvice.vacancy.VacancyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("api/v1/vacancy")
 public class VacancyController {
+    private VacancyService service;
+
+
+    public VacancyDto createVacancy(VacancyDto vacancyDto) {
+        return vacancyDto;
+    }
 }
