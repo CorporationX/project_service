fa1e4c9e69e5ea9e201b2fbfe732c5bcee1d5efd
Ivan Averkov <averkovivan@gmail.com>
2023-07-31 13:19:34 +0300

    temp

--- src/main/java/faang/school/projectservice/srvice/vacancy/VacancyService.java
+++ src/main/java/faang/school/projectservice/srvice/vacancy/VacancyService.java
@@ -1,9 +1,14 @@
 package faang.school.projectservice.srvice.vacancy;
 
+import faang.school.projectservice.dto.vacancy.VacancyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class VacancyService {
+
+    public void deleteVacancy(VacancyDto vacancyDto) {
+
+    }
 }
--- src/main/java/faang/school/projectservice/validator/vacancy/VacancyValidator.java
+++ src/main/java/faang/school/projectservice/validator/vacancy/VacancyValidator.java
@@ -1,9 +1,16 @@
 package faang.school.projectservice.validator.vacancy;
 
+import faang.school.projectservice.dto.vacancy.VacancyDto;
+import faang.school.projectservice.exception.vacancy.VacancyValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class VacancyValidator {
+    public void deleteVacancyControllerVAlidation(VacancyDto vacancyDto) {
+        if (vacancyDto == null) {
+            throw new VacancyValidationException("Vacancy not found!");
+        }
+    }
 }
