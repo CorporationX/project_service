4edcde2d0fef29073a4c243cfc8518649f894d8b
Ivan Averkov <averkovivan@gmail.com>
2023-07-31 09:45:00 +0300

    full setup for subtasks

---
+++ src/main/java/faang/school/projectservice/controller/vacancy/VacancyController.java
@@ -0,0 +1,11 @@
+package faang.school.projectservice.controller.vacancy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/vacancy")
+public class VacancyController {
+}
---
+++ src/main/java/faang/school/projectservice/srvice/vacancy/VacancyService.java
@@ -0,0 +1,9 @@
+package faang.school.projectservice.srvice.vacancy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VacancyService {
+}
