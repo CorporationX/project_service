200438fb542d4ad5d1d95d4ad64a99a2fa6d8090
Ivan Averkov <averkovivan@gmail.com>
2023-07-31 09:25:48 +0300

    error x2

--- src/main/java/faang/school/projectservice/dto/vacancy/VacancyDto.java
+++ src/main/java/faang/school/projectservice/dto/vacancy/VacancyDto.java
@@ -1,2 +1,22 @@
-package faang.school.projectservice.dto.vacancy;public class VacancyDto {
+package faang.school.projectservice.dto.vacancy;
+
+import faang.school.projectservice.model.VacancyStatus;
+import faang.school.projectservice.model.WorkSchedule;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class VacancyDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Long projectId;
+    private List<Long> candidates;
+    private VacancyStatus status;
+    private Double salary;
+    private WorkSchedule workSchedule;
+    private List<Long> requiredSkillIds;
 }
--- src/main/java/faang/school/projectservice/mapper/vacancy/VacancyMapper.java
+++ src/main/java/faang/school/projectservice/mapper/vacancy/VacancyMapper.java
@@ -1,2 +1,40 @@
-package faang.school.projectservice.mapper.vacancy;public interface VacancyMapper {
+package faang.school.projectservice.mapper.vacancy;
+
+import faang.school.projectservice.dto.vacancy.VacancyDto;
+import faang.school.projectservice.model.Candidate;
+import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.Vacancy;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.FIELD, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface VacancyMapper {
+    @Mapping(target = "project", source = "projectId", qualifiedByName = "idToProject")
+    @Mapping(target = "candidates", source = "candidates", qualifiedByName = "idsToCandidates")
+    Vacancy toEntity(VacancyDto vacancyDto);
+
+    @Mapping(target = "projectId", source = "project", qualifiedByName = "projectToId")
+    @Mapping(target = "candidates", source = "candidates", qualifiedByName = "CandidatesToIds")
+    VacancyDto toDto(Vacancy vacancy);
+
+    @Named("idToProject")
+    default Project idToProject(Long id) {
+        return Project.builder().id(id).build();
+    }
+
+    @Named("idsToCandidates")
+    default List<Candidate> idsToCandidates(List<Long> ids) {
+        return ids.stream().map(id -> Candidate.builder().id(id).build()).toList();
+    }
+
+    @Named("projectToId")
+    default Long projectToId(Project project) {
+        return project.getId();
+    }
+
+    @Named("CandidatesToIds")
+    default List<Long> CandidatesToIds(List<Candidate> candidates) {
+        return candidates.stream().map(Candidate::getId).toList();
+    }
 }
