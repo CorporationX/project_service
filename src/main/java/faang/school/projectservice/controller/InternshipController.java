aaccd3dc2be97bdc37d78b05097902ec19c6280c
M1khail <mishalion888@gmail.com>
2023-08-01 21:18:07 +0200

    update internship - with annotations in controller

--- src/main/java/faang/school/projectservice/controller/InternshipController.java
+++ src/main/java/faang/school/projectservice/controller/InternshipController.java
@@ -4,18 +4,22 @@ import faang.school.projectservice.dto.client.InternshipDto;
 import faang.school.projectservice.service.InternshipService;
 import faang.school.projectservice.validator.InternshipValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class InternshipController {
     private final InternshipService internshipService;
 
+    @PostMapping("/saveInternship")
     public InternshipDto saveNewInternship(InternshipDto internshipDto) {
         InternshipValidator.validateControllerInternship(internshipDto);
         return internshipService.saveNewInternship(internshipDto);
     }
 
+    @PutMapping("/updateInternship")
     public InternshipDto updateInternship(InternshipDto internshipDto, long id) {
         InternshipValidator.validateControllerInternship(internshipDto);
         return internshipService.updateInternship(internshipDto, id);
