be728dbe3264585ac3f74e6bfd7855e6fbc9d028
M1khail <mishalion888@gmail.com>
2023-08-01 21:23:28 +0200

    Get all internships with filters by status or role + get list of all internships - with annotations in controller

--- src/main/java/faang/school/projectservice/InternshipController/InternshipController.java
+++ src/main/java/faang/school/projectservice/InternshipController/InternshipController.java
@@ -6,25 +6,31 @@ import faang.school.projectservice.dto.client.InternshipFilterDto;
 import faang.school.projectservice.model.Internship;
 import faang.school.projectservice.validator.InternshipValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class InternshipController {
     private final InternshipService internshipService;
 
+    @PostMapping("/saveInternship")
     public InternshipDto saveNewInternship(InternshipDto internshipDto) {
         InternshipValidator.validateControllerInternship(internshipDto);
         return internshipService.saveNewInternship(internshipDto);
     }
 
+    @PutMapping("/updateInternship")
     public InternshipDto updateInternship(Internship oldInternship, InternshipDto internshipDto, long id) {
         InternshipValidator.validateControllerInternship(internshipDto);
         return internshipService.updateInternship(internshipDto, id);
     }
 
+    @GetMapping("/findInternshipsWithFilter")
     public List<InternshipDto> findInternshipsWithFilter(long projectId, InternshipFilterDto filterDto) {
         return internshipService.findInternshipsWithFilter(projectId, filterDto);
     }
