0956bd220d8894ba324c13faf6332ef9e9820d01
Ivan Averkov <averkovivan@gmail.com>
2023-08-01 14:37:18 +0300

    temp

--- src/main/java/faang/school/projectservice/controller/vacancy/VacancyController.java
+++ src/main/java/faang/school/projectservice/controller/vacancy/VacancyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/vacancy")
+@RequestMapping("/api/v1/vacancy")
 public class VacancyController {
     private final VacancyService service;
     private final VacancyValidator vacancyValidator;
