f9f20041347f3e2d2f85e399e327790aa9c365f6
AlexanderKolesnikow <kolesnikov.kite@gmail.com>
2023-08-01 23:17:16 +0100

    For PR wihtout tests

--- src/main/java/faang/school/projectservice/controller/stage/StageController.java
+++ src/main/java/faang/school/projectservice/controller/stage/StageController.java
@@ -1,9 +1,7 @@
 package faang.school.projectservice.controller.stage;
 
 import faang.school.projectservice.dto.stage.StageDto;
-import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.service.stage.StageService;
-import io.micrometer.common.util.StringUtils;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,14 +15,6 @@ public class StageController {
 
     @PostMapping("/stage")//поменяю в будущем когда договоримся об общем виде
     public StageDto createProjectStage(@RequestBody StageDto stageDto) {
-        validateStageName(stageDto);
-
         return stageService.create(stageDto);
-    }
-
-    private void validateStageName(StageDto stageDto) {
-        if (StringUtils.isBlank(stageDto.getStageName())) {
-            throw new DataValidationException("Stage name can't be blank or null");
-        }
     }
 }
--- src/main/java/faang/school/projectservice/dto/stage/StageDto.java
+++ src/main/java/faang/school/projectservice/dto/stage/StageDto.java
@@ -4,13 +4,17 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StageDto {
 
     private Long stageId;
