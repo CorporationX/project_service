3a09e46f84ea2ff7b623973c126401b5fe8eaf0c
gordeens <gordeenvs@yandex.ru>
2023-08-02 15:18:00 +0300

    BC-4009. for review. Implemented logic get vacancy.

--- src/main/java/faang/school/projectservice/mapper/vacancy/VacancyMapper.java
+++ src/main/java/faang/school/projectservice/mapper/vacancy/VacancyMapper.java
@@ -1,14 +1,14 @@
 package faang.school.projectservice.mapper.vacancy;
 
 import faang.school.projectservice.dto.vacancy.VacancyDto;
+import faang.school.projectservice.dto.vacancy.VacancyDtoGetReq;
 import faang.school.projectservice.dto.vacancy.VacancyDtoUpdateReq;
+import faang.school.projectservice.model.Candidate;
 import faang.school.projectservice.model.Vacancy;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mapper(componentModel = "Spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface VacancyMapper {
@@ -16,9 +16,22 @@ public interface VacancyMapper {
     @Mapping(source = "project.id", target = "projectId")
     VacancyDto toDto(Vacancy vacancy);
 
+    @Mapping(source = "id", target = "vacancyId")
+    @Mapping(source = "project.id", target = "projectId")
+    @Mapping(source = "candidates", target = "candidatesId", qualifiedByName = "getListIdsCandidates")
+    VacancyDtoGetReq toDtoGetReq(Vacancy vacancy);
+
     @Mapping(target = "id", ignore = true)
     Vacancy toEntity(VacancyDto vacancyDto);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromDto(VacancyDtoUpdateReq vacancyDto, @MappingTarget Vacancy vacancy);
+
+    @Named(value = "getListIdsCandidates")
+    default List<Long> getListIdsCandidates(List<Candidate> candidates) {
+        if (candidates == null) {
+            return new ArrayList<>();
+        }
+        return candidates.stream().map(Candidate::getId).toList();
+    }
 }
