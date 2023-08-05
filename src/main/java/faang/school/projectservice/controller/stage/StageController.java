f2e01df023a34337c3e11dbbe997c7234ef2e71d
AlexanderKolesnikow <kolesnikov.kite@gmail.com>
2023-08-05 18:03:22 +0100

    Without tests

--- src/main/java/faang/school/projectservice/controller/stage/StageController.java
+++ src/main/java/faang/school/projectservice/controller/stage/StageController.java
@@ -1,6 +1,7 @@
 package faang.school.projectservice.controller.stage;
 
 import faang.school.projectservice.dto.stage.StageDto;
+import faang.school.projectservice.dto.stage.StageRolesDto;
 import faang.school.projectservice.service.stage.StageService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,11 @@ public class StageController {
     @PostMapping("/stage")//поменяю в будущем когда договоримся об общем виде
     public StageDto createProjectStage(@Valid @RequestBody StageDto stageDto) {
         return stageService.create(stageDto);
+    }
+
+    @PutMapping("/stage/{stageId}")
+    public StageDto updateStageRoles(@PathVariable long stageId,@Valid @RequestBody StageRolesDto stageRolesDto) {
+        return stageService.updateStage(stageId, stageRolesDto);
     }
 
     @DeleteMapping("/stage/{stageId}")
--- src/main/java/faang/school/projectservice/service/stage/StageService.java
+++ src/main/java/faang/school/projectservice/service/stage/StageService.java
@@ -4,11 +4,12 @@ import faang.school.projectservice.config.context.UserContext;
 import faang.school.projectservice.dto.stage.StageDto;
 import faang.school.projectservice.dto.stage.StageRolesDto;
 import faang.school.projectservice.exception.DataValidationException;
-import faang.school.projectservice.jpa.StageRolesRepository;
 import faang.school.projectservice.jpa.TaskRepository;
 import faang.school.projectservice.mapper.stage.StageMapper;
+import faang.school.projectservice.mapper.stage.StageRolesMapper;
 import faang.school.projectservice.model.*;
 import faang.school.projectservice.model.stage.Stage;
+import faang.school.projectservice.model.stage.StageRoles;
 import faang.school.projectservice.model.stage_invitation.StageInvitation;
 import faang.school.projectservice.model.stage_invitation.StageInvitationStatus;
 import faang.school.projectservice.repository.ProjectRepository;
@@ -28,8 +29,8 @@ public class StageService {
     private final StageRepository stageRepository;
     private final ProjectRepository projectRepository;
     private final StageMapper stageMapper;
+    private final StageRolesMapper stageRolesMapper;
     private final TaskRepository taskRepository;
-    private final StageRolesRepository stageRolesRepository;
     private final UserContext userContext;
     private final StageInvitationRepository stageInvitationRepository;
 
@@ -42,23 +43,20 @@ public class StageService {
 
         return stageMapper.toDto(stage);
     }
-//Обновить этап.
-//
-//ProcessStageRoles Если на этап требуется участник с определённой ролью, нужно проверить, что в списке задействованных на этапе участников есть пользователь с такой ролью.
-//Если нет, то нужно найти среди участников проекта пользователя с такой ролью и отправить ему приглашение участвовать в этапе.
-//Сколько пользователей с данной ролью требуется, столько приглашений разным пользователям должно быть отправлено.
-//
-//Валидация Если изменяется список участников, нужно проверять, что обновлённые список участников удовлетворяет требованиям ролей.
-//Update
 
     @Transactional
-    public StageDto updateStageRoles(StageDto stageDto) {
-        validateStageProject(stageDto);
-        Stage stageToUpdate = stageRepository.getById(stageDto.getStageId());
+    public StageDto updateStage(long stageId, StageRolesDto stageRolesDto) {
+        Stage stageToUpdate = stageRepository.getById(stageId);
+        int executorsNeeded = executorsNeeded(stageToUpdate, stageRolesDto);
 
+        if (executorsNeeded > 0) {
+            sendInvitationToProjectMembers(executorsNeeded, stageToUpdate, stageRolesDto);
+        }
+
+        List<StageRoles> stageRoles = updateStageRoles(stageRolesDto, stageToUpdate);
+        stageToUpdate.setStageRoles(stageRoles);
 
         stageRepository.save(stageToUpdate);
-
         return stageMapper.toDto(stageToUpdate);
     }
 
@@ -102,6 +100,7 @@ public class StageService {
         stageRepository.delete(stage);
     }
 
+    @Transactional(readOnly = true)
     public List<StageDto> getAllProjectStages(long projectId) {
         List<Stage> stages = projectRepository.getProjectById(projectId).getStages();
 
@@ -110,14 +109,51 @@ public class StageService {
                 .toList();
     }
 
+    @Transactional(readOnly = true)
     public StageDto getStageById(long stageId) {
         Stage stage = stageRepository.getById(stageId);
 
         return stageMapper.toDto(stage);
     }
 
+    private int executorsNeeded(Stage stage, StageRolesDto stageRolesDto) {
+        TeamRole teamRole = stageRolesDto.getTeamRole();
+        List<TeamMember> stageExecutors = stage.getExecutors();
+        int executorsNeeded = stageRolesDto.getCount();
+
+        for (TeamMember stageExecutor : stageExecutors) {
+            if (stageExecutor.getRoles().contains(teamRole)) {
+                executorsNeeded--;
+            }
+        }
+
+        return executorsNeeded;
+    }
+
+    private void sendInvitationToProjectMembers(int executorsNeeded, Stage stage, StageRolesDto stageRolesDto) {
+        List<Team> teams = stage.getProject().getTeams();
+        TeamRole teamRole = stageRolesDto.getTeamRole();
+
+        List<TeamMember> teamMembers = teams.stream()
+                .flatMap(team -> team.getTeamMembers().stream())
+                .filter(teamMember -> !stage.getExecutors().contains(teamMember))
+                .filter(teamMember -> teamMember.getRoles().contains(teamRole))
+                .distinct()
+                .toList();
+
+        for (TeamMember teamMember : teamMembers) {
+            if (executorsNeeded == 0) {
+                break;
+            }
+
+            sendStageInvitation(stage.getStageId(), teamMember.getId());
+            executorsNeeded--;
+        }
+    }
+
     private void sendStageInvitation(long stageId, long invitedTeamMemberId) {
         TeamMember author = TeamMember.builder().id(userContext.getUserId()).build();
+
         StageInvitation stageInvitation = StageInvitation.builder()
                 .author(author)
                 .invited(TeamMember.builder().id(invitedTeamMemberId).build())
@@ -128,10 +164,19 @@ public class StageService {
         stageInvitationRepository.save(stageInvitation);
     }
 
-    private int executorsNeeded(StageDto stageDto) {
-        return stageDto.getStageRoles().stream()
-                .mapToInt(StageRolesDto::getCount)
-                .sum();
+    private List<StageRoles> updateStageRoles(StageRolesDto stageRolesDto, Stage stage) {
+        List<StageRoles> stageRoles = stage.getStageRoles();
+        List<StageRoles> updatedStageRoles = new ArrayList<>(stageRoles);
+
+        for (StageRoles stageRole : stageRoles) {
+            if (stageRole.getId().equals(stageRolesDto.getStageRoleId())) {
+                updatedStageRoles.remove(stageRole);
+                break;
+            }
+        }
+        updatedStageRoles.add(stageRolesMapper.toEntity(stageRolesDto));
+
+        return updatedStageRoles;
     }
 
     private void validateStageProject(StageDto stageDto) {
