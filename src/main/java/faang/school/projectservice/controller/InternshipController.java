f2f5644100c6417dd26e8ae124a5b41c25fdf341
ZLATA <585zlata@mail.ru>
2023-08-06 05:47:05 +0300

    45е

--- src/main/java/faang/school/projectservice/controller/InternshipController.java
+++ src/main/java/faang/school/projectservice/controller/InternshipController.java
@@ -22,6 +22,7 @@ public class InternshipController {
         return internshipService.internshipCreation(internshipDto);
     }
 
+
     public List<InternshipDto> gettingAllInternships() {
         return internshipService.gettingAllInternships();
     }
