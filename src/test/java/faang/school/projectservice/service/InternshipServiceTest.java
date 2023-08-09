192064f7b9ee5ca7e82591c00c27c13adff4e564
Zlata <585zlata@mail.ru>
2023-08-09 20:46:07 +0300

    raw update method12

---
+++ src/test/java/faang/school/projectservice/service/InternshipServiceTest.java
@@ -0,0 +1,28 @@
+package faang.school.projectservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InternshipServiceTest {
+
+    @Test
+    void internshipCreation() {
+    }
+
+    @Test
+    void gettingAllInternshipsAccordingToFilters() {
+    }
+
+    @Test
+    void gettingAllInternships() {
+    }
+
+    @Test
+    void getInternshipById() {
+    }
+
+    @Test
+    void updateInternship() {
+    }
+}
