4881e216af7fec28fe80818e53607c6df4a9405a
Zlata <585zlata@mail.ru>
2023-08-10 16:40:36 +0300

    raw update method12

--- src/test/java/faang/school/projectservice/service/InternshipServiceTest.java
+++ src/test/java/faang/school/projectservice/service/InternshipServiceTest.java
@@ -5,6 +5,8 @@ import faang.school.projectservice.exceptions.InternshipValidationException;
 import faang.school.projectservice.filters.InternshipFilters.InternshipFilter;
 import faang.school.projectservice.jpa.TeamMemberJpaRepository;
 import faang.school.projectservice.mapper.InternshipMapper;
+import faang.school.projectservice.model.Internship;
+import faang.school.projectservice.model.Project;
 import faang.school.projectservice.model.TeamMember;
 import faang.school.projectservice.repository.InternshipRepository;
 import faang.school.projectservice.repository.TeamMemberRepository;
@@ -12,15 +14,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static reactor.core.publisher.Mono.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class InternshipServiceTest {
@@ -40,12 +42,28 @@ class InternshipServiceTest {
 
     @Test
     void internshipCreation() {
-    }
-
+        InternshipDto internshipDto = InternshipDto.builder().id(49L).projectId(5L)
+                .mentorId(4L).internsId(List.of(3L, 5L))
+                .startDate(LocalDateTime.of(2023, 2, 12, 12, 00))
+                .endDate(LocalDateTime.of(2023, 4, 12, 12, 00))
+                .build();
+        Internship internship = Internship.builder().id(49L).project(Project.builder().id(5L).build())
+                .mentorId(TeamMember.builder().id(4L).build())
+                .interns(List.of(TeamMember.builder().id(3L).build(),TeamMember.builder().id(5L).build()))
+                .startDate(LocalDateTime.of(2023, 2, 12, 12, 00))
+                .endDate(LocalDateTime.of(2023, 4, 12, 12, 00))
+                .build();
+        Mockito.when(teamMemberJpaRepository.findByUserIdAndProjectId(internshipDto.getMentorId(), internshipDto.getProjectId())).thenReturn(TeamMember.builder().build());
+        Mockito.when(internshipMapper.toInternship(internshipDto)).thenReturn(any());
+        Mockito.when(internshipMapper.toInternshipDto(internship)).thenReturn(any());
+        internshipService.internshipCreation(internshipDto);
+        Mockito.verify(internshipRepository).save(any());
+}
     @Test
     void testInternshipBusinessValidationFirst() {
         InternshipDto internshipDto = InternshipDto.builder().id(4L).projectId(null)
-                .mentorId(4L).internsId(List.of(3L, 5L)).startDate(LocalDateTime.now())
+                .mentorId(4L).internsId(List.of(3L, 5L))
+                .startDate(LocalDateTime.now())
                 .endDate(LocalDateTime.now())
                 .build();
 
@@ -55,7 +73,7 @@ class InternshipServiceTest {
 
     @Test
     void testInternshipBusinessValidationSecond() {
-        List <Long> internTestList = List.of();
+        List<Long> internTestList = List.of();
         InternshipDto internshipDto = InternshipDto.builder().id(4L).projectId(5L)
                 .mentorId(4L).internsId(internTestList).startDate(LocalDateTime.now())
                 .endDate(LocalDateTime.now())
@@ -67,10 +85,27 @@ class InternshipServiceTest {
 
     @Test
     void testInternshipBusinessValidationThird() {
+        InternshipDto internshipDto = InternshipDto.builder().id(4L).projectId(5L)
+                .mentorId(4L).internsId(List.of(3L, 5L))
+                .startDate(LocalDateTime.of(2023, 2, 12, 12, 00))
+                .endDate(LocalDateTime.of(2023, 7, 12, 12, 00))
+                .build();
+
+        assertThrows(InternshipValidationException.class,
+                () -> internshipService.internshipCreation(internshipDto));
     }
 
     @Test
     void testInternshipBusinessValidationForth() {
+        InternshipDto internshipDto = InternshipDto.builder().id(4L).projectId(5L)
+                .mentorId(4L).internsId(List.of(3L, 5L))
+                .startDate(LocalDateTime.of(2023, 2, 12, 12, 00))
+                .endDate(LocalDateTime.of(2023, 4, 12, 12, 00))
+                .build();
+        Mockito.when(teamMemberJpaRepository.findByUserIdAndProjectId(internshipDto.getMentorId(), internshipDto.getProjectId())).thenReturn(null);
+        assertThrows(InternshipValidationException.class,
+                ()-> internshipService.internshipCreation(internshipDto));
+
     }
 
 
