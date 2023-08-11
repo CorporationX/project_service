421a18c8be84a9f66425b7f38b24427bf1ef821b
DiegaLoL <alex.eey@mail.ru>
2023-08-12 00:21:15 +0900

    bc-4067 Update stage add test

--- src/main/resources/db/changelog/changeset/project_BC_4068_stage_status.sql
+++ src/main/resources/db/changelog/changeset/project_BC_4068_stage_status.sql
@@ -1,2 +1,2 @@
 alter table project_stage
-    add column if not exists project_stage_status varchar(255) not null default 'CREATED';
+    add column if not exists status varchar(255) not null default 'CREATED';
