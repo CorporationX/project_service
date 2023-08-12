8fcf8cc90a51a356c51e7adae0a08da6cfde48a0
b.mukhtarkhanov <b.mukhtarkhanov>
2023-08-12 21:51:08 +0600

    Swagger для Project Service

--- src/main/java/faang/school/projectservice/controller/InternshipController.java
+++ src/main/java/faang/school/projectservice/controller/InternshipController.java
@@ -4,6 +4,8 @@ package faang.school.projectservice.controller;
 import faang.school.projectservice.dto.InternshipDto;
 import faang.school.projectservice.service.InternshipService;
 import faang.school.projectservice.validation.RequestValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
+@Tag(name="Internships", description = "")
 @RestController
 @RequestMapping("qpi/v1/internships")
 @RequiredArgsConstructor
@@ -22,13 +25,18 @@ public class InternshipController {
     private final InternshipService internshipService;
     private final RequestValidator requestValidator;
 
-
+    @Operation(
+            summary = "",
+            tags = { "internships", "get" })
     @GetMapping
     public ResponseEntity<List<InternshipDto>> getAllInternships() {
         List<InternshipDto> internships = internshipService.getAllInternships();
         return ResponseEntity.ok(internships);
     }
 
+    @Operation(
+            summary = "",
+            tags = { "internships", "get" })
     @GetMapping("/{internshipId}")
     public ResponseEntity<InternshipDto> getInternshipBy(@PathVariable Long internshipId) {
         requestValidator.validate(internshipId);
