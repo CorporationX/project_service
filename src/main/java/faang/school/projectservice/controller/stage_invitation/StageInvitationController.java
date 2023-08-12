37ec7cf58ac25e0ddb98a16b74c303007fba8538
Dobrovolsky Artem <artdobrov@mail.ru>
2023-08-12 15:35:07 +0300

    BC-4921: Эндпоинты, валидация и логи

--- src/main/java/faang/school/projectservice/controller/stage_invitation/StageInvitationController.java
+++ src/main/java/faang/school/projectservice/controller/stage_invitation/StageInvitationController.java
@@ -4,55 +4,76 @@ import faang.school.projectservice.dto.stage_invitation.StageInvitationDto;
 import faang.school.projectservice.dto.stage_invitation.StageInvitationFilterDto;
 import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.service.stage_invitation.StageInvitationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/stage/invitations")
+@Slf4j
 public class StageInvitationController {
     private final StageInvitationService service;
 
-    public StageInvitationDto create(StageInvitationDto invitationDto) {
+    @PostMapping
+    public StageInvitationDto create(@Valid @RequestBody StageInvitationDto invitationDto) {
+        log.info("POST endpoint 'create()' was called successfully");
         validate(invitationDto);
         return service.create(invitationDto);
     }
 
-    public StageInvitationDto accept(long invitationId) {
+    @PutMapping("/accept/{invitationId}")
+    public StageInvitationDto accept(@PathVariable long invitationId) {
+        log.info("PUT endpoint 'accept()' was called successfully with invitationId {}", invitationId);
         return service.accept(invitationId);
     }
 
-    public StageInvitationDto reject(long invitationId, String message){
-        if (message == null || message.isBlank()){
+    @PutMapping("/reject/{invitationId}")
+    public StageInvitationDto reject(@PathVariable long invitationId, @RequestParam("message") String message) {
+        log.info("PUT endpoint 'reject()' was called successfully with invitationId {}", invitationId);
+        if (message == null || message.isBlank()) {
             throw new DataValidationException("Rejection must contains message");
         }
         return service.reject(invitationId, message);
     }
 
-    public List<StageInvitationDto> getStageInvitationsWithFilters(StageInvitationFilterDto filterDto) {
+    @GetMapping("/filter")
+    public List<StageInvitationDto> getStageInvitationsWithFilters(@RequestBody StageInvitationFilterDto filterDto) {
+        log.info("GET endpoint 'getStageInvitationsWithFilters()' was called successfully");
         return service.getStageInvitationsWithFilters(filterDto);
     }
 
-    public List<StageInvitationDto> getStageInvitationsForTeamMemberWithFilters(long invitedId, StageInvitationFilterDto filterDto) {
+    @GetMapping("/invited/{invitedId}/filter")
+    public List<StageInvitationDto> getStageInvitationsForTeamMemberWithFilters(@PathVariable long invitedId, @RequestBody StageInvitationFilterDto filterDto) {
+        log.info("GET endpoint 'getStageInvitationsForTeamMemberWithFilters()' was called successfully with invitedId {}", invitedId);
         if (filterDto.getInvitedId() == null || filterDto.getInvitedId() != invitedId) {
             filterDto = StageInvitationFilterDto.builder()
                     .stageId(filterDto.getStageId())
                     .authorId(filterDto.getAuthorId())
-                    .invitedId(invitedId).build();
+                    .invitedId(invitedId)
+                    .status(filterDto.getStatus())
+                    .build();
         }
         return service.getStageInvitationsWithFilters(filterDto);
     }
 
 
     private void validate(StageInvitationDto invitationDto) {
-        if (invitationDto == null ||
-                invitationDto.getStageId() == null ||
-                invitationDto.getAuthorId() == null ||
-                invitationDto.getInvitedId() == null ||
-                invitationDto.getAuthorId() == invitationDto.getInvitedId()) {
-
-            throw new DataValidationException("StageInvitation is invalid");
+        if (invitationDto == null) {
+            throw new DataValidationException("StageInvitation can't be null");
+        } else if (Objects.equals(invitationDto.getAuthorId(), invitationDto.getInvitedId())) {
+            throw new DataValidationException("Author can't be invited");
         }
     }
 }
