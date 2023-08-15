2daa65cea091c83de0583d9d87a973c282a45dd5
Basimov Rustam <sombra1nf@yandex.ru>
2023-08-15 19:01:34 +0500

    Create controller

---
+++ src/main/java/faang/school/projectservice/controller/stage/StageController.java
@@ -0,0 +1,43 @@
+package faang.school.projectservice.controller.stage;
+
+import faang.school.projectservice.dto.stage.StageDto;
+import faang.school.projectservice.service.stage.StageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stage")
+public class StageController {
+
+    private final StageService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public StageDto create(StageDto stageDto) {
+        validateStage(stageDto);
+        return service.create(stageDto);
+    }
+
+    private void validateStage(StageDto stageDto) {
+        if (stageDto.getId() != null) {
+            throw new IllegalArgumentException("Stage must have id");
+        }
+        if (stageDto.getProjectId() == null) {
+            throw new IllegalArgumentException("Stage must have project");
+        }
+        if (stageDto.getStageName() == null || stageDto.getStageName().isEmpty()) {
+            throw new IllegalArgumentException("Stage name must not be empty");
+        }
+        if (stageDto.getStageRoleIds() == null || stageDto.getStageRoleIds().isEmpty()) {
+            throw new IllegalArgumentException("Stage must have at least one role");
+        }
+        if (stageDto.getTeamMemberIds() == null || stageDto.getTeamMemberIds().isEmpty()) {
+            throw new IllegalArgumentException("Stage must have at least one team member");
+        }
+    }
+}
