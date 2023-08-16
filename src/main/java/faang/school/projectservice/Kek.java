9a30ee2e54319ec8925af7fe38f05dc6fd66fc92
Sergey Gerasimov <czar997@yandex.ru>
2023-08-16 21:19:44 +0300

    kek

---
+++ src/main/java/faang/school/projectservice/Kek.java
@@ -0,0 +1,4 @@
+package faang.school.projectservice;
+
+public class Kek {
+}
---
+++ src/test/java/faang/school/projectservice/KekTest.java
@@ -0,0 +1,33 @@
+package faang.school.projectservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KekTest {
+
+    @Test
+    void test1() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test3() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test4() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test5() {
+        assertTrue(true);
+    }
+}
