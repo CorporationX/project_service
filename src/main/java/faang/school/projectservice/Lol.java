3fd872c3ef39e7c387e85d1582a7f220dc4234c1
Sergey Gerasimov <czar997@yandex.ru>
2023-08-16 21:31:41 +0300

    lol

---
+++ src/main/java/faang/school/projectservice/Lol.java
@@ -0,0 +1,5 @@
+package faang.school.projectservice;
+
+public class Lol {
+
+}
---
+++ src/test/java/faang/school/projectservice/LolTest.java
@@ -0,0 +1,14 @@
+package faang.school.projectservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LolTest {
+
+
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+}
