18f042b768968f9d1d518f1670b6089660ed0c36
gordeens <gordeenvs@yandex.ru>
2023-08-16 11:15:12 +0300

    BC-4915. Add visibility check condition. Add test.

--- src/main/java/faang/school/projectservice/service/subproject/SubprojectService.java
+++ src/main/java/faang/school/projectservice/service/subproject/SubprojectService.java
@@ -1,13 +1,19 @@
 package faang.school.projectservice.service.subproject;
 
 import faang.school.projectservice.dto.subproject.SubprojectDtoReqCreate;
+import faang.school.projectservice.exceptions.SubprojectException;
 import faang.school.projectservice.mapper.subproject.SubprojectMapper;
 import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.ProjectVisibility;
 import faang.school.projectservice.repository.ProjectRepository;
 import faang.school.projectservice.repository.StageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.text.MessageFormat;
+
+import static faang.school.projectservice.commonMessage.SubprojectErrMessage.ERR_VISIBILITY_PARENT_PROJECT_FORMAT;
 
 @Service
 @RequiredArgsConstructor
@@ -24,10 +30,27 @@ public class SubprojectService {
         newSubProject.setParentProject(parentProject);
         newSubProject.setChildren(projectRepository.findAllByIds(subprojectDto.getChildrenIds()));
         newSubProject.setStages(stageRepository.findAllByIds(subprojectDto.getStagesIds()));
+        setVisibilitySubproject(newSubProject, subprojectDto.getVisibility());
 
         parentProject.getChildren().add(newSubProject);
         projectRepository.save(parentProject);
 
         return subprojectMapper.toDtoReqCreate(projectRepository.save(newSubProject));
+    }
+
+    private void setVisibilitySubproject(Project subproject, ProjectVisibility visibility) {
+        if (visibility == null) {
+            subproject.setVisibility(ProjectVisibility.PUBLIC);
+            return;
+        }
+        if (isParentVisibilityPublic(subproject.getParentProject()) && visibility == ProjectVisibility.PRIVATE) {
+            String errorMessage = MessageFormat.format(ERR_VISIBILITY_PARENT_PROJECT_FORMAT, visibility);
+            throw new SubprojectException(errorMessage);
+        }
+        subproject.setVisibility(visibility);
+    }
+
+    private boolean isParentVisibilityPublic(Project parentProject) {
+        return parentProject.getVisibility() == ProjectVisibility.PUBLIC;
     }
 }
