8dd03a6dff865a7b025d949dd6af085ef28be4f7
Sergey Gerasimov <czar997@yandex.ru>
2023-08-16 11:30:22 +0300

    добавлена ошибка UserNorAccessRightDeleteException

---
+++ src/main/java/faang/school/projectservice/exception/UserNorAccessRightDeleteException.java
@@ -0,0 +1,7 @@
+package faang.school.projectservice.exception;
+
+public class UserNorAccessRightDeleteException extends RuntimeException {
+    public UserNorAccessRightDeleteException(String message) {
+        super(message);
+    }
+}
