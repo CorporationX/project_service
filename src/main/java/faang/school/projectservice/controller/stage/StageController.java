fba8a9416e8868d1be2d294134d7271daa067657
Basimov Rustam <sombra1nf@yandex.ru>
2023-08-22 22:29:09 +0500

    Add field validation

--- src/main/java/faang/school/projectservice/controller/stage/StageController.java
+++ src/main/java/faang/school/projectservice/controller/stage/StageController.java
@@ -1,7 +1,6 @@
 package faang.school.projectservice.controller.stage;
 
 import faang.school.projectservice.dto.stage.StageDto;
-import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.service.stage.StageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,28 +28,11 @@ public class StageController {
     @GetMapping("/{projectId}/stages")
     @ResponseStatus(HttpStatus.OK)
     public List<StageDto> getStages(@PathVariable Long projectId) {
-        validateId(projectId);
         log.info("Getting stages for project: {}", projectId);
         return service.getStagesByProjectId(projectId);
     }
 
     private void validateStage(StageDto stageDto) {
-        validateId(stageDto.getStageId());
-        validateId(stageDto.getProjectId());
-        if (stageDto.getStageName() == null || stageDto.getStageName().isEmpty()) {
-            throw new DataValidationException("Stage name must not be empty");
-        }
-        if (stageDto.getStageRoleIds() == null || stageDto.getStageRoleIds().isEmpty()) {
-            throw new DataValidationException("Stage must have at least one role");
-        }
-        if (stageDto.getTeamMemberIds() == null || stageDto.getTeamMemberIds().isEmpty()) {
-            throw new DataValidationException("Stage must have at least one team member");
-        }
-    }
 
-    private void validateId(Long id) {
-        if (id == null) {
-            throw new DataValidationException("Id must not be null");
-        }
     }
 }
--- src/main/java/faang/school/projectservice/dto/stage/StageDto.java
+++ src/main/java/faang/school/projectservice/dto/stage/StageDto.java
@@ -1,5 +1,9 @@
 package faang.school.projectservice.dto.stage;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,9 +12,26 @@ import java.util.List;
 @Data
 @Builder
 public class StageDto {
+
+    @NotNull(message = "Stage id must not be null")
+    @Min(value = 0, message = "Stage id must be greater than or equal to 0")
+    @Max(value = Long.MAX_VALUE, message = "Stage id must be less than or equal to Long.MAX_VALUE")
     private Long stageId;
+
+    @NotNull(message = "Stage name must not be null")
+    @NotBlank(message = "Stage name must not be blank")
     private String stageName;
+
+    @NotNull(message = "Project must not be null")
+    @Min(value = 0, message = "Project id must be greater than or equal to 0")
+    @Max(value = Long.MAX_VALUE, message = "Project id must be less than or equal to Long.MAX_VALUE")
     private Long projectId;
+
+    @NotNull(message = "Stage roles must not be null")
+    @NotBlank(message = "Stage roles must not be blank")
     private List<Long> stageRoleIds;
+
+    @NotNull(message = "Team members must not be null")
+    @NotBlank(message = "Team members must not be blank")
     private List<Long> teamMemberIds;
 }
