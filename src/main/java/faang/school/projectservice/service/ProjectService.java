ad21229abc1f5ac137d1ee5e7ed815cd87b084d4
InnMish <inn.mishustina@gmail.com>
2023-08-22 12:11:23 +0300

    added controller getProject

--- src/main/java/faang/school/projectservice/service/ProjectService.java
+++ src/main/java/faang/school/projectservice/service/ProjectService.java
@@ -1,10 +1,13 @@
 package faang.school.projectservice.service;
 
+import faang.school.projectservice.dto.ProjectDto;
 import faang.school.projectservice.exception.EntityNotFoundException;
+import faang.school.projectservice.mapper.ProjectMapper;
 import faang.school.projectservice.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,10 +15,16 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
 
     public void validateProjectId(Long projectId) {
         if (!projectRepository.existsById(projectId)) {
             throw new EntityNotFoundException("Project with id " + projectId + " does not exist");
         }
+    }
+
+    @Transactional
+    public ProjectDto findProjectById(long id) {
+        return projectMapper.toDto(projectRepository.getProjectById(id));
     }
 }
