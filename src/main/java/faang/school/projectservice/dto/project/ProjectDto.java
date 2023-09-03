78c1b6dc2212ffa0d65d15f87be3da9b473f9555
Abramov Nikita <mr.abramovn@gmail.com>
2023-09-04 01:22:04 +0300

    fix bugs

--- src/main/java/faang/school/projectservice/dto/project/ProjectDto.java
+++ src/main/java/faang/school/projectservice/dto/project/ProjectDto.java
@@ -1,5 +1,9 @@
 package faang.school.projectservice.dto.project;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,10 +14,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProjectDto {
+    @Min(0)
     private Long id;
+    @NotBlank(message = "The name is not valid")
+    @Size(min = 3, max = 50, message = "The name must be between 3 and 50 characters")
     private String name;
+    @NotBlank(message = "The description is not valid")
+    @Size(min = 3, max = 100, message = "The description must be between 3 and 100 characters")
     private String description;
+    @Min(0)
+    @NotEmpty(message = "The ownerId is not valid")
     private Long ownerId;
+    @NotBlank(message = "The status is not valid")
     private String status;
+    @NotBlank(message = "The visibility is not valid")
     private String visibility;
 }
