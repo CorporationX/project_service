8e38f20c0cd77067fad13b8b48d7009585f2cbf1
Viktor Ivanov <avacode78@gmail.com>
2023-09-05 18:54:20 +0700

    remove unnecessary annotation

--- src/main/java/faang/school/projectservice/dto/company/CampaignDto.java
+++ src/main/java/faang/school/projectservice/dto/company/CampaignDto.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
 
@@ -16,7 +15,6 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Data
 @Builder
-@Validated
 public class CampaignDto {
     @NotNull
     private Long id;
