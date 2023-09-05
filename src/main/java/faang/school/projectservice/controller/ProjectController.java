2d352d6f7a08f072f37efb9a0a3b3c645bca1581
Abramov Nikita <mr.abramovn@gmail.com>
2023-09-05 15:37:17 +0300

    add validation

--- src/main/java/faang/school/projectservice/controller/ProjectController.java
+++ src/main/java/faang/school/projectservice/controller/ProjectController.java
@@ -2,8 +2,8 @@ package faang.school.projectservice.controller;
 
 import faang.school.projectservice.dto.filter.ProjectFilterDto;
 import faang.school.projectservice.dto.project.ProjectDto;
-import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.service.ProjectService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,14 +22,12 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping()
-    public ProjectDto createProject(@RequestBody ProjectDto projectDto) {
-        validateProject(projectDto);
+    public ProjectDto createProject(@Valid @RequestBody ProjectDto projectDto) {
         return projectService.createProject(projectDto);
     }
 
     @PutMapping()
-    public ProjectDto updateProject(@RequestBody ProjectDto projectDto) {
-        validateProject(projectDto);
+    public ProjectDto updateProject(@Valid @RequestBody ProjectDto projectDto) {
         return projectService.updateProject(projectDto);
     }
 
@@ -46,17 +44,5 @@ public class ProjectController {
     @GetMapping()
     public List<ProjectDto> getAllProject() {
         return projectService.getAllProject();
-    }
-
-    private void validateProject(ProjectDto projectDto) {
-        if (projectDto.getName().isBlank() || projectDto.getName() == null) {
-            throw new DataValidationException("The name is not valid");
-        }
-        if (projectDto.getDescription().isBlank() || projectDto.getDescription() == null) {
-            throw new DataValidationException("The description is not valid");
-        }
-        if (projectDto.getOwnerId() < 0) {
-            throw new DataValidationException("The ownerId is not valid");
-        }
     }
 }
