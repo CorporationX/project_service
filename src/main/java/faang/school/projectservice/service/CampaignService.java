eecebb51163dc1f18d57c3940266ff918db35e4d
Azat <azat.takhautdinov@gmail.com>
2023-10-04 22:22:03 +0500

    коммит

--- src/main/java/faang/school/projectservice/service/CampaignService.java
+++ src/main/java/faang/school/projectservice/service/CampaignService.java
@@ -22,8 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
