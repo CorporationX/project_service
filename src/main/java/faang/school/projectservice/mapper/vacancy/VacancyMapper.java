38273e06bcb72ff915a66e2a93e5b8e113fb5e5e
Alexander Bulgakov <asbulgakov90@gmail.com>
2024-02-01 13:49:33 +0300

    Remove method in mapper task BJS2-1615

--- src/main/java/faang/school/projectservice/mapper/vacancy/VacancyMapper.java
+++ src/main/java/faang/school/projectservice/mapper/vacancy/VacancyMapper.java
@@ -1,15 +1,11 @@
 package faang.school.projectservice.mapper.vacancy;
 
 import faang.school.projectservice.dto.vacancy.VacancyDto;
-import faang.school.projectservice.model.Candidate;
 import faang.school.projectservice.model.Vacancy;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
-
-import java.util.List;
 
 /**
  * @author Alexander Bulgakov
@@ -25,9 +21,4 @@ public interface VacancyMapper {
     @Mapping(target = "candidates", ignore = true)
     @Mapping(target = "project.id", ignore = true)
     Vacancy toEntity(VacancyDto dto);
-
-    @Named("mapCandidates")
-    default List<Long> mapCandidates(List<Candidate> candidates) {
-        return candidates.stream().map(Candidate::getId).toList();
-    }
 }
