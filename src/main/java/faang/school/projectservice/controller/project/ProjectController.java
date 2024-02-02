68ca4ac349a4ec2f467973c373a73be6cf5bc8f5
Alexander Bulgakov <asbulgakov90@gmail.com>
2024-02-02 16:48:41 +0300

    Create BJS2-1760

---
+++ src/main/java/faang/school/projectservice/controller/project/ProjectController.java
@@ -0,0 +1,11 @@
+package faang.school.projectservice.controller.project;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Alexander Bulgakov
+ */
+
+@RestController
+public class ProjectController {
+}
