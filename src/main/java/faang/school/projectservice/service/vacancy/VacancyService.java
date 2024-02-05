ac5adeaa9e3b9339ac4a6c80eea6f1bf96b1d925
Alexander Bulgakov <asbulgakov90@gmail.com>
2024-02-05 03:49:37 +0300

    Update BJS2-1737

--- src/main/java/faang/school/projectservice/service/vacancy/VacancyService.java
+++ src/main/java/faang/school/projectservice/service/vacancy/VacancyService.java
@@ -14,6 +14,7 @@ import faang.school.projectservice.model.Vacancy;
 import faang.school.projectservice.model.VacancyStatus;
 import faang.school.projectservice.repository.VacancyRepository;
 import faang.school.projectservice.service.project.ProjectService;
+import faang.school.projectservice.service.team.TeamMemberService;
 import faang.school.projectservice.validator.vacancy.VacancyValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
