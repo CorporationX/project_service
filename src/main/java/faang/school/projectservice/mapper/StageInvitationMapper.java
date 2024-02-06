d538db4307408d3eece663e6664d39dd69ecd668
Emperor_Samedi <mexanik13970@gmail.com>
2024-02-06 17:38:05 +0300

    fixed StageInvitationMapper

--- src/main/java/faang/school/projectservice/mapper/StageInvitationMapper.java
+++ src/main/java/faang/school/projectservice/mapper/StageInvitationMapper.java
@@ -3,12 +3,12 @@ package faang.school.projectservice.mapper;
 import faang.school.projectservice.dto.StageInvitationDto;
 import faang.school.projectservice.model.stage_invitation.StageInvitation;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Mapper
-@Component
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface StageInvitationMapper {
 
     StageInvitationDto toDto(StageInvitation stageInvitation);
