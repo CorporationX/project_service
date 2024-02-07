670e5a9917b5f3b2436f197941086be48838e5e6
Emperor_Samedi <mexanik13970@gmail.com>
2024-02-07 17:29:04 +0300

    fixed StageInvitationControllerTest

--- src/test/java/faang/school/projectservice/util/controller/StageInvitationControllerTest.java
+++ src/test/java/faang/school/projectservice/util/controller/StageInvitationControllerTest.java
@@ -1,11 +1,17 @@
 package faang.school.projectservice.util.controller;
 
 import faang.school.projectservice.controller.StageInvitationController;
+import faang.school.projectservice.dto.StageInvitationDto;
+import faang.school.projectservice.exception.ValidateStageInvitationException;
+import faang.school.projectservice.model.TeamMember;
+import faang.school.projectservice.model.stage.Stage;
 import faang.school.projectservice.service.StageInvitationService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -17,48 +23,57 @@ public class StageInvitationControllerTest {
     private StageInvitationController stageInvitationController;
 
     @Test
-    void testCreateInvitationMissingStage(){
-
+    void testCreateInvitationMissingStage() {
+        StageInvitationDto stageInvitationDto = new StageInvitationDto();
+        stageInvitationDto.setAuthor(new TeamMember());
+        stageInvitationDto.setInvited(new TeamMember());
+        Assert.assertThrows(ValidateStageInvitationException.class, () -> stageInvitationController.createInvitation(stageInvitationDto));
     }
 
     @Test
-    void testCreateInvitationMissingAuthor(){
-
+    void testCreateInvitationMissingAuthor() {
+        StageInvitationDto stageInvitationDto = new StageInvitationDto();
+        stageInvitationDto.setStage(new Stage());
+        stageInvitationDto.setInvited(new TeamMember());
+        Assert.assertThrows(ValidateStageInvitationException.class, () -> stageInvitationController.createInvitation(stageInvitationDto));
     }
 
     @Test
-    void testCreateInvitationMissingInvited(){
-
+    void testCreateInvitationMissingInvited() {
+        StageInvitationDto stageInvitationDto = new StageInvitationDto();
+        stageInvitationDto.setStage(new Stage());
+        stageInvitationDto.setAuthor(new TeamMember());
+        Assert.assertThrows(ValidateStageInvitationException.class, () -> stageInvitationController.createInvitation(stageInvitationDto));
     }
 
     @Test
-    void testAcceptInvitationMissingUserId(){
-
+    void testAcceptInvitationMissingUserId() {
+        Assert.assertThrows(ValidateStageInvitationException.class, () -> stageInvitationController.acceptInvitation(null, Mockito.anyLong()));
     }
 
     @Test
-    void testAcceptInvitationMissingInvitationId(){
-
+    void testAcceptInvitationMissingInvitationId() {
+        Assert.assertThrows(ValidateStageInvitationException.class, () -> stageInvitationController.acceptInvitation(Mockito.anyLong(), null));
     }
 
     @Test
-    void testRejectInvitationMissingUserId(){
-
+    void testRejectInvitationMissingUserId() {
+        Assert.assertThrows(ValidateStageInvitationException.class, () -> stageInvitationController.rejectInvitation(null, Mockito.anyLong(), Mockito.anyString()));
     }
 
     @Test
-    void testRejectInvitationMissingInvitationId(){
-
+    void testRejectInvitationMissingInvitationId() {
+        Assert.assertThrows(ValidateStageInvitationException.class, () -> stageInvitationController.rejectInvitation(Mockito.anyLong(), null, Mockito.anyString()));
     }
 
     @Test
-    void testRejectInvitationMissingDescription(){
-
+    void testRejectInvitationMissingDescription() {
+        Assert.assertThrows(ValidateStageInvitationException.class, () -> stageInvitationController.rejectInvitation(Mockito.anyLong(), Mockito.anyLong(), null));
     }
 
     @Test
-    void testGetAllInvitationMissingId(){
-
+    void testGetAllInvitationMissingId() {
+        Assert.assertThrows(ValidateStageInvitationException.class, () -> stageInvitationController.getAllInvitation(null));
     }
 
 }
