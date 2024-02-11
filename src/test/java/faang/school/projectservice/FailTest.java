1cc83d64cc18a0b774e155f5932238eca4002f07
starkoracia <starkoracia@gmail.com>
2024-02-11 01:20:13 +0000

    Added fail test

---
+++ src/test/java/faang/school/projectservice/FailTest.java
@@ -0,0 +1,13 @@
+package faang.school.projectservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FailTest {
+
+    @Test
+    void failTest() {
+        Assertions.fail();
+    }
+
+}
