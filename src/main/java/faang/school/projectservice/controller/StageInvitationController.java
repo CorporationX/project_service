613c71ac9aebfaf13ca467c31dc3e3307e104ba0
Emperor_Samedi <mexanik13970@gmail.com>
2024-02-13 17:53:56 +0300

    fixed StageInvitationController

--- src/main/java/faang/school/projectservice/controller/StageInvitationController.java
+++ src/main/java/faang/school/projectservice/controller/StageInvitationController.java
@@ -5,11 +5,17 @@ import faang.school.projectservice.exception.ValidateStageInvitationException;
 import faang.school.projectservice.service.StageInvitationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/stageInvitation")
 public class StageInvitationController {
 
     private final StageInvitationService stageInvitationService;
@@ -18,27 +24,34 @@ public class StageInvitationController {
         if (field == null) throw new ValidateStageInvitationException(message);
     }
 
-    public StageInvitationDto createInvitation(StageInvitationDto stageInvitationDto) {
+    @PostMapping("/create")
+    public StageInvitationDto createInvitation(@RequestBody StageInvitationDto stageInvitationDto) {
         validate(stageInvitationDto.getStage(), "Stage missing from request");
         validate(stageInvitationDto.getAuthor(), "Author missing from request");
         validate(stageInvitationDto.getInvited(), "Invited missing from request");
         return stageInvitationService.create(stageInvitationDto);
     }
 
-    public void acceptInvitation(Long userId, Long invitationId) {
+    @PostMapping("/accept")
+    public void acceptInvitation(@PathVariable("userId") Long userId,
+                                 @PathVariable("invitationId") Long invitationId) {
         validate(userId, "User missing from request");
         validate(invitationId, "Invited missing from request");
         stageInvitationService.accept(userId, invitationId);
     }
 
-    public void rejectInvitation(Long userId, Long invitationId, String description) {
+    @PostMapping("/reject")
+    public void rejectInvitation(@PathVariable("userId") Long userId,
+                                 @PathVariable("invitationId") Long invitationId,
+                                 @PathVariable("description") String description) {
         validate(userId, "User missing from request");
         validate(invitationId, "Invited missing from request");
         validate(description, "Description missing from request");
         stageInvitationService.reject(userId, invitationId, description);
     }
 
-    public List<StageInvitationDto> getAllInvitation(Long id) {
+    @GetMapping("/getAll/{stageInvitationId}")
+    public List<StageInvitationDto> getAllInvitation(@PathVariable("stageInvitationId") Long id) {
         validate(id, "Incorrect user id");
         return stageInvitationService.getAll(id);
     }
