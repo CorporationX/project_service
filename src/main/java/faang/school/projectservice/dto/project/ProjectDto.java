f18d67b965f1fc29737721a52f2fe50c753d7ffd
Edisolt Khatsiev <Edisolt Khatsiev>
2024-02-19 09:56:37 +0300

    ProjectControllerTest

--- src/main/java/faang/school/projectservice/dto/project/ProjectDto.java
+++ src/main/java/faang/school/projectservice/dto/project/ProjectDto.java
@@ -3,12 +3,18 @@ package faang.school.projectservice.dto.project;
 import faang.school.projectservice.model.ProjectStatus;
 import faang.school.projectservice.model.ProjectVisibility;
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProjectDto {
     private Long id;
     private String name;
