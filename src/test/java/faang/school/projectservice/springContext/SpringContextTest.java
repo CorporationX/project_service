53660a880a148bbb7b928ec29eea0632a5d6abae
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-02-29 21:42:50 +0300

    тест поднятия спринг контекста

---
+++ src/test/java/faang/school/projectservice/springContext/SpringContextTest.java
@@ -0,0 +1,12 @@
+package faang.school.projectservice.springContext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringContextTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
