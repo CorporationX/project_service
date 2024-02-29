971303f1141bcf15a36733c61b1418c274771d98
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-03-01 01:24:33 +0300

    тест поднятия спринг контекста

--- src/test/java/faang/school/projectservice/springContext/SpringContextTest.java
+++ src/test/java/faang/school/projectservice/springContext/SpringContextTest.java
@@ -1,10 +1,14 @@
 package faang.school.projectservice.springContext;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class SpringContextTest {
+    @MockBean
+    private AmazonS3 clientAmazonS3;
 
     @Test
     public void contextLoads() {
