18d3aff5c8be4334631fe9b9858affc8db9dca12
Viktor Dyukov <snailrake@mail.ru>
2024-03-11 21:09:51 +0400

    refactor: add branch in ci.yml

---
+++ src/test/java/faang/school/projectservice/CiTest.java
@@ -0,0 +1,12 @@
+package faang.school.projectservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CiTest {
+
+    @Test
+    void fail() {
+        Assertions.fail();
+    }
+}
