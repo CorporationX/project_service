d85f1ea43e9f468c08776f903ea6da1fc179ea56
Viktor Dyukov <snailrake@mail.ru>
2024-03-11 21:11:18 +0400

    test: add test for ci pipeline

---
+++ src/test/java/faang/school/projectservice/CiTest.java
@@ -0,0 +1,14 @@
+package faang.school.projectservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CiTest {
+
+    @Test
+    void failTest() {
+        fail();
+    }
+}
