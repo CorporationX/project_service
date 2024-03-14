18b75481b95d3082dc3ba159c1e17b1096412be0
Bukevsky | Kirill <bykaplay@gmail.com>
2024-03-15 02:26:48 +0300

    minor update

--- src/main/java/faang/school/projectservice/controller/project/ProjectController.java
+++ src/main/java/faang/school/projectservice/controller/project/ProjectController.java
@@ -3,6 +3,7 @@ package faang.school.projectservice.controller.project;
 import faang.school.projectservice.dto.project.ProjectDto;
 import faang.school.projectservice.dto.project.ProjectDtoFilter;
 import faang.school.projectservice.service.project.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,28 +17,33 @@ import java.util.List;
 public class ProjectController {
     private final ProjectService projectService;
 
+    @Operation(summary = "Create project in database")
     @PostMapping("/create")
     public ProjectDto createProject(@RequestBody ProjectDto projectDto) {
         return projectService.createProject(projectDto);
     }
 
+    @Operation(summary = "Update project in database")
     @PutMapping("/update")
     public ProjectDto updateProject(@RequestBody ProjectDto projectDto) {
         return projectService.updateProject(projectDto);
     }
 
+    @Operation(summary = "Get filtered projects")
     @PostMapping("/filter/{userId}")
     public List<ProjectDto> findProjectByFilters(@PathVariable("userId") Long userId, @RequestBody ProjectDtoFilter projectDtoFilter) {
         return projectService.findProjectByFilters(userId, projectDtoFilter);
     }
 
+    @Operation(summary = "Get all project")
     @GetMapping("/all")
     public List<ProjectDto> findAllProjects() {
         return projectService.findAllProjects();
     }
 
+    @Operation(summary = "Get project by id")
     @GetMapping("/{projectId}")
-    public ProjectDto findProjectById(@PathVariable("projectId") Long projectId){
+    public ProjectDto findProjectById(@PathVariable("projectId") Long projectId) {
         return projectService.findProjectById(projectId);
     }
 }
