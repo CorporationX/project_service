c4e18df910fcad83be2171d52ccded5d163801d6
Dmitriy Goncharov <153752474+hhzktoeto@users.noreply.github.com>
2024-03-21 00:18:24 +0500

    Добавил эндпоинты + поправки
    добавил эндпоинты в VacancyController
    Заменил валидацию @NotBlank на @NotNull для всех полей, содержащих Long

--- src/main/java/faang/school/projectservice/controller/vacancy/VacancyController.java
+++ src/main/java/faang/school/projectservice/controller/vacancy/VacancyController.java
@@ -3,35 +3,55 @@ package faang.school.projectservice.controller.vacancy;
 import faang.school.projectservice.dto.vacancy.VacancyDto;
 import faang.school.projectservice.dto.vacancy.VacancyFilterDto;
 import faang.school.projectservice.service.vacancy.VacancyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/vacancies")
+@Tag(name = "Vacancies", description = "Endpoints for managing vacancies")
 public class VacancyController {
     private final VacancyService vacancyService;
 
-
-    public VacancyDto create(@Valid VacancyDto vacancyDto) {
+    @Operation(summary = "Create a vacancy")
+    @PostMapping
+    public VacancyDto create(@Valid @RequestBody VacancyDto vacancyDto) {
         return vacancyService.create(vacancyDto);
     }
 
-    public VacancyDto update(@Valid VacancyDto vacancyDto) {
+    @Operation(summary = "Get vacancy by id")
+    @GetMapping("/{vacancyId}")
+    public VacancyDto getVacancyById(@PathVariable long vacancyId) {
+        return vacancyService.getVacancyById(vacancyId);
+    }
+
+    @Operation(summary = "Update existing vacancy")
+    @PutMapping
+    public VacancyDto update(@Valid @RequestBody VacancyDto vacancyDto) {
         return vacancyService.update(vacancyDto);
     }
 
-    public void delete(VacancyDto vacancyDto) {
+    @Operation(summary = "Delete a vacancy")
+    @DeleteMapping
+    public void delete(@RequestBody VacancyDto vacancyDto) {
         vacancyService.delete(vacancyDto);
     }
 
-    public List<VacancyDto> getFilteredVacancies(VacancyFilterDto filter) {
+    @Operation(summary = "Get list of filtered vacancies")
+    @PostMapping("/filtered")
+    public List<VacancyDto> getFilteredVacancies(@RequestBody VacancyFilterDto filter) {
         return vacancyService.getFilteredVacancies(filter);
-    }
-
-    public VacancyDto getVacancyById(long vacancyId) {
-        return vacancyService.getVacancyById(vacancyId);
     }
 }
--- src/main/java/faang/school/projectservice/dto/vacancy/VacancyDto.java
+++ src/main/java/faang/school/projectservice/dto/vacancy/VacancyDto.java
@@ -2,6 +2,7 @@ package faang.school.projectservice.dto.vacancy;
 
 import faang.school.projectservice.model.VacancyStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,10 +20,10 @@ public class VacancyDto {
     private String name;
     @NotBlank(message = "Vacancy must have a description")
     private String description;
-    @NotBlank(message = "Vacancy must be assigned to a project")
+    @NotNull(message = "Vacancy must be assigned to a project")
     private Long projectId;
     private List<Long> candidatesIds;
-    @NotBlank(message = "Vacancy must have a curator")
+    @NotNull(message = "Vacancy must have a curator")
     private Long curatorId;
     private VacancyStatus status;
     private Integer workersRequired;
