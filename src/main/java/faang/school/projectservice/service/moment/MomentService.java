2ddf3d2f76c3207147a6cab492b4780e5dede588
DadyaVolodya <pl4stmass@yandex.ru>
2024-03-20 11:26:29 +0300

    Создал один метод из основной логики и написал на него тест Начал писать логику update

--- src/main/java/faang/school/projectservice/service/moment/MomentService.java
+++ src/main/java/faang/school/projectservice/service/moment/MomentService.java
@@ -1,28 +1,27 @@
 package faang.school.projectservice.service.moment;
 
-import faang.school.projectservice.validator.moment.ValidatorMoment;
-import faang.school.projectservice.model.Moment;
-import faang.school.projectservice.model.Project;
 import faang.school.projectservice.dto.moment.MomentDto;
 import faang.school.projectservice.mapper.moment.MomentMapper;
+import faang.school.projectservice.model.Moment;
+import faang.school.projectservice.model.Project;
 import faang.school.projectservice.repository.MomentRepository;
-import faang.school.projectservice.repository.ProjectRepository;
-import jakarta.persistence.EntityNotFoundException;
+import faang.school.projectservice.validator.moment.ValidatorMoment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class MomentService {
+public static class MomentService {
     private final MomentRepository momentRepository;
     private final ValidatorMoment validatorMoment;
     private final MomentMapper momentMapper;
+    private final Moment moment;
+
     @Transactional
-    public void createMoment(MomentDto momentDto){
+    public void createMoment(MomentDto momentDto) {
         validatorMoment.ValidatorMomentName(momentDto);
         validatorMoment.ValidatorOpenProject(momentDto);
         validatorMoment.ValidatorMomentProject(momentDto);
@@ -30,17 +29,37 @@ public class MomentService {
         momentRepository.save(moment);
     }
 
-    public void updateMoment(MomentDto momentDto){
-       // Moment moment = momentMapper.toEntity(momentDto);
-            momentDto.getProjectIds() ==
-
-                momentDto.getUserIds()
-                        .distinct()
-                        .collect(Collectors.toList());
+    public void updateMoment(MomentDto momentDto) {
+        List<Long> oldProjectIds = moment.getProjects().stream()
+                .map(Project::getId)
+                .toList();
+        List<Long> newProjectIds = momentDto.getProjectIds()
+                .stream()
+                .toList();
+        if (oldProjectIds.equals(newProjectIds)) {
+            System.out.println("There are no new projects");
+        } else {
+            moment.setUserIds(momentDto.getUserIds().stream()
+                    .distinct()
+                    .toList());
+            //если пришла дто с новым айди приекта , то в момента обновляю айдишники юзеров
+            momentRepository.save(moment);
+            List<Long> oldUserIds = moment.getUserIds()
+                    .stream()
+                    .toList();
+            List<Long> newUserIds = momentDto.getUserIds()
+                    .stream()
+                    .toList();
+            if (oldUserIds.equals(newUserIds)) {
+                System.out.println("No new members");
+            } else {
+                moment.setProjects();
+            }
         }
-
-
     }
+
+
+}
 //        Project projectAdd = projectRepository.getProjectById();
 //        if (!momentDto.getProjects().contains(projectAdd)) {
 //            moment.getUserIds().add(userIds);
@@ -51,7 +70,7 @@ public class MomentService {
 //        momentRepository.save(moment);
 //    }
 
-    public void validatorMoment(Moment data, Project project){
+public void validatorMoment(Moment data, Project project) {
 
-    }
+}
 }
