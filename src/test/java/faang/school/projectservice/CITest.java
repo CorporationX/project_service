7448cd687f654d8733360fb0e10b14a8d95732a8
Adilbek <adilbek.nurmakov@nu.edu.kz>
2024-03-26 11:40:31 +0600

    jacoco to project service

---
+++ src/test/java/faang/school/projectservice/CITest.java
@@ -0,0 +1,15 @@
+package faang.school.projectservice;
+
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+    public class CITest {
+
+        @Test
+        void fail() {
+            Assertions.fail();
+        }
+    }
+
