d090cbd4b51fe61e263938e46219f01e44436b37
Adilbek <adilbek.nurmakov@nu.edu.kz>
2024-03-27 00:23:13 +0600

    CI test2

--- src/main/java/faang/school/projectservice/service/InternshipService.java
+++ src/main/java/faang/school/projectservice/service/InternshipService.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-//@Service
+@Service
 @RequiredArgsConstructor
 public class InternshipService {
     private final InternshipRepository internshipRepository;
