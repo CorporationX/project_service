2f6b2d192737285b2b2e25b43a26d4ce0588bd01
Sergey Ryabov <mre@mail.ru>
2024-03-28 20:48:06 +0800

    @RequestParam

--- src/main/java/faang/school/projectservice/controller/StageInvitationController.java
+++ src/main/java/faang/school/projectservice/controller/StageInvitationController.java
@@ -5,6 +5,7 @@ import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.model.stage_invitation.StageInvitationStatus;
 import faang.school.projectservice.service.StageInvitationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,14 +23,15 @@ public class StageInvitationController {
         return stageInvitationService.sendInvitation(stageInvitationDto);
     }
 
-    @PostMapping("/acceptInvitation")
-    public void acceptInvitation(@RequestParam("invitationId") Long invitationId, @RequestParam("invitedId") Long invitedId) {
+    @PostMapping("/{invitedId}/acceptInvitation")
+    public void acceptInvitation(@RequestParam("invitationId") Long invitationId,
+                                 @PathVariable("invitedId") Long invitedId) {
         stageInvitationService.acceptInvitation(invitationId, invitedId);
     }
 
-    @PostMapping("/declineInvitation")
+    @PostMapping("/{invitatedId}/declineInvitation")
     public void  declineInvitation(@RequestParam("invitationId") Long invitationId,
-                                   @RequestParam("invitatedId")Long invitedId,
+                                   @PathVariable("invitatedId")Long invitedId,
                                    @RequestBody String description) {
         if (description.isEmpty() || description.isBlank()) {
             throw new DataValidationException("You can not decline invitation without cause!");
