5caaf7ecf801784a44e3e8df648dda151204492e
Bukevsky <bykaplay@gmail.com>
2024-04-01 22:49:27 +0300

    minor changes

--- src/main/java/faang/school/projectservice/controller/project/ProjectController.java
+++ src/main/java/faang/school/projectservice/controller/project/ProjectController.java
@@ -5,7 +5,6 @@ import faang.school.projectservice.dto.project.ProjectDtoFilter;
 import faang.school.projectservice.service.project.ProjectService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/faang/school/projectservice/dto/project/ProjectDto.java
+++ src/main/java/faang/school/projectservice/dto/project/ProjectDto.java
@@ -2,8 +2,6 @@ package faang.school.projectservice.dto.project;
 
 import faang.school.projectservice.model.ProjectStatus;
 import faang.school.projectservice.model.ProjectVisibility;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
