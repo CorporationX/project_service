10ed79f89f25972ab478bf87da12db9a80c2d209
Sergey Ryabov <mre@mail.ru>
2024-04-12 21:11:30 +0800

    stage invitation without vacancy

--- src/main/java/faang/school/projectservice/service/filter/StageInvitationFilter.java
+++ src/main/java/faang/school/projectservice/service/filter/StageInvitationFilter.java
@@ -4,7 +4,7 @@ import faang.school.projectservice.dto.filter.StageInvitationFilterDto;
 import faang.school.projectservice.model.stage_invitation.StageInvitation;
 
 public interface StageInvitationFilter {
-    public boolean isApplicable(StageInvitationFilterDto filters);
+    boolean isApplicable(StageInvitationFilterDto filters);
 
     boolean apply(StageInvitation stageInvitation, StageInvitationFilterDto filters);
 }
--- src/main/java/faang/school/projectservice/service/filter/StageInvitationStatusFilter.java
+++ src/main/java/faang/school/projectservice/service/filter/StageInvitationStatusFilter.java
@@ -2,7 +2,9 @@ package faang.school.projectservice.service.filter;
 
 import faang.school.projectservice.dto.filter.StageInvitationFilterDto;
 import faang.school.projectservice.model.stage_invitation.StageInvitation;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StageInvitationStatusFilter implements StageInvitationFilter {
     @Override
     public boolean isApplicable(StageInvitationFilterDto filters) {
