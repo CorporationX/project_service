d1a51a876d2b94fd15a6ec0c55b20af6a9f7a0e7
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-17 21:25:40 +0300

    осознал ошибки

---
+++ src/main/java/faang/school/projectservice/config/context/RedisConfiguration.java
@@ -0,0 +1,45 @@
+package faang.school.projectservice.config.context;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@AllArgsConstructor
+@Configuration
+public class RedisConfiguration {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.channels.project_view_channel.name}")
+    private String channel;
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host,port);
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public ChannelTopic postViewTopic() {
+        return new ChannelTopic(channel);
+    }
+}
---
+++ src/main/java/faang/school/projectservice/pablisher/ProjectViewEventPublisher.java
@@ -0,0 +1,20 @@
+package faang.school.projectservice.pablisher;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ProjectViewEventPublisher {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ChannelTopic channelTopic;
+
+    public void publish(String message) {
+        redisTemplate.convertAndSend(channelTopic.getTopic(), message);
+        log.info("send message to topic: {}", channelTopic.getTopic());
+    }
+}
