35d5949b75356807f063226a20ec40e77720ab6d
Tem <tipokruto014@gmail.com>
2024-05-08 18:07:06 +0300

    feat: добавил документацию Swagger в InitiativeController

--- src/main/java/faang/school/projectservice/controller/InitiativeController.java
+++ src/main/java/faang/school/projectservice/controller/InitiativeController.java
@@ -3,6 +3,8 @@ package faang.school.projectservice.controller;
 import faang.school.projectservice.dto.initiative.InitiativeDto;
 import faang.school.projectservice.dto.initiative.InitiativeFilterDto;
 import faang.school.projectservice.service.initiative.InitiativeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,29 +19,35 @@ import java.util.List;
 @RestController
 @RequestMapping("/initiative")
 @RequiredArgsConstructor
+@Tag(name = "Initiatives")
 public class InitiativeController {
     private final InitiativeService initiativeService;
 
+    @Operation(summary = "Create new initiative")
     @PostMapping("/create")
     public InitiativeDto create(@RequestBody InitiativeDto initiative) {
         return initiativeService.create(initiative);
     }
 
+    @Operation(summary = "Update initiative")
     @PutMapping("/update")
     public InitiativeDto update(@RequestBody InitiativeDto initiative) {
         return initiativeService.update(initiative);
     }
 
+    @Operation(summary = "Get all initiatives by filter")
     @GetMapping
     public List<InitiativeDto> getAllByFilter(@RequestBody InitiativeFilterDto filters) {
         return initiativeService.getAllByFilter(filters);
     }
 
+    @Operation(summary = "Get all initiatives")
     @GetMapping
     public List<InitiativeDto> getAll() {
         return initiativeService.getAll();
     }
 
+    @Operation(summary = "Get initiative by id")
     @GetMapping("/{id}")
     public InitiativeDto getById(@PathVariable long id) {
         return initiativeService.getById(id);
