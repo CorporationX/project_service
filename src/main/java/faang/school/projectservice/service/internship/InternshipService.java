737f2fb833a0064db5f44a76603ac02d2c6734b2
nadir <den4ik.strelok@gmail.com>
2024-05-08 17:06:53 +0300

    Добавил действия "Получить все стажировки" и "Получить стажировку по Id"

--- src/main/java/faang/school/projectservice/service/internship/InternshipService.java
+++ src/main/java/faang/school/projectservice/service/internship/InternshipService.java
@@ -2,32 +2,25 @@ package faang.school.projectservice.service.internship;
 
 import faang.school.projectservice.dto.filter.InternshipFilterDto;
 import faang.school.projectservice.dto.internship.InternshipDto;
-import faang.school.projectservice.exception.RoleProcessingException;
+import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.mapper.InternshipMapper;
 import faang.school.projectservice.model.Internship;
 import faang.school.projectservice.model.InternshipStatus;
-import faang.school.projectservice.model.TaskStatus;
-import faang.school.projectservice.model.TeamMember;
-import faang.school.projectservice.model.TeamRole;
 import faang.school.projectservice.repository.InternshipRepository;
-import faang.school.projectservice.repository.ProjectRepository;
-import faang.school.projectservice.repository.TeamMemberRepository;
 import faang.school.projectservice.service.internship.filter.InternshipFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.function.Predicate;
 
-import static faang.school.projectservice.exception.RoleProcessingExceptionMessage.ABSENT_INTERN_ROLE_EXCEPTION;
+import static faang.school.projectservice.exception.InternshipValidationExceptionMessage.NON_EXISTING_INTERNSHIP_EXCEPTION;
 
 @Service
 @RequiredArgsConstructor
 public class InternshipService {
     private final InternshipServiceValidation internshipServiceValidation;
+    private final InternshipServiceUtility internshipServiceUtility;
     private final InternshipRepository internshipRepository;
-    private final ProjectRepository projectRepository;
-    private final TeamMemberRepository teamMemberRepository;
     private final InternshipMapper internshipMapper;
     private final List<InternshipFilter> filters;
 
@@ -35,7 +28,7 @@ public class InternshipService {
     public InternshipDto create(InternshipDto internshipDto) {
         internshipServiceValidation.validationCreate(internshipDto);
 
-        Internship internship = toEntity(internshipDto);
+        Internship internship = internshipServiceUtility.toEntity(internshipDto);
 
         return internshipMapper.toDto(internshipRepository.save(internship));
     }
@@ -43,10 +36,10 @@ public class InternshipService {
     public InternshipDto update(InternshipDto internshipDto) {
         internshipServiceValidation.validationUpdate(internshipDto);
 
-        Internship internship = toEntity(internshipDto);
+        Internship internship = internshipServiceUtility.toEntity(internshipDto);
 
         if (internship.getStatus().equals(InternshipStatus.COMPLETED)) {
-            processCompletedInternship(internship);
+            internshipServiceUtility.processCompletedInternship(internship);
         }
 
         return internshipMapper.toDto(internshipRepository.save(internship));
@@ -65,65 +58,14 @@ public class InternshipService {
                 .toList();
     }
 
-    private void processCompletedInternship(Internship internship) {
-        List<TeamMember> interns = internship.getInterns();
-        var internsToBeHired = interns.stream()
-                .filter(checkCompletionOfTasks())
-                .toList();
-
-        interns.removeAll(internsToBeHired);
-
-        interns.forEach(firedIntern -> firedIntern.getTeam().getTeamMembers().remove(firedIntern));
-        interns.clear();
-
-        assignRolesToInterns(internsToBeHired);
+    public List<InternshipDto> getAllInternships() {
+        return internshipMapper.toDto(internshipRepository.findAll());
     }
 
-    private Predicate<TeamMember> checkCompletionOfTasks() {
-        return teamMember -> {
-            var allTasks = teamMember.getStages().stream()
-                    .flatMap(stage -> stage.getTasks().stream())
-                    .toList();
+    public InternshipDto getInternshipById(long internshipId) {
+        Internship internship = internshipRepository.findById(internshipId)
+                .orElseThrow(() -> new DataValidationException(NON_EXISTING_INTERNSHIP_EXCEPTION.getMessage()));
 
-            var doneTasks = allTasks.stream()
-                    .filter(task -> task.getStatus().equals(TaskStatus.DONE))
-                    .toList();
-
-            return doneTasks.size() == allTasks.size();
-        };
-    }
-
-    private void assignRolesToInterns(List<TeamMember> interns) {
-        interns.forEach(intern -> {
-            var internRoles = intern.getRoles();
-
-            if (!internRoles.contains(TeamRole.INTERN)) {
-                throw new RoleProcessingException(ABSENT_INTERN_ROLE_EXCEPTION.getMessage());
-            }
-
-            if (internRoles.size() == 1) {
-                internRoles.add(TeamRole.DEVELOPER);
-                //TODO: нормально ли делать по умолчанию бывших стажеров разработчиками, если до этого у них не было другой роли?
-            }
-
-            internRoles.remove(TeamRole.INTERN);
-
-            teamMemberRepository.save(intern);
-        });
-    }
-
-    private Internship toEntity(InternshipDto internshipDto) {
-        Internship internship = internshipMapper.toEntity(internshipDto);
-
-        internship.setProject(projectRepository.getProjectById(internshipDto.getProjectId()));
-        internship.setMentorId(teamMemberRepository.findById(internshipDto.getMentorId()));
-
-        var interns = internshipDto.getInternsIds().stream()
-                .map(teamMemberRepository::findById)
-                .toList();
-
-        internship.setInterns(interns);
-
-        return internship;
+        return internshipMapper.toDto(internship);
     }
 }
