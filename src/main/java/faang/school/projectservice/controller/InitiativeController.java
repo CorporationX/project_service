1d5d1f712c42eb7cd46f93edb6c9acbc482869a8
Tem <tipokruto014@gmail.com>
2024-05-09 17:08:50 +0300

    feat: добавил аннотации @ParameterObject для Swagger в классе InitiativeController

--- src/main/java/faang/school/projectservice/controller/InitiativeController.java
+++ src/main/java/faang/school/projectservice/controller/InitiativeController.java
@@ -4,8 +4,10 @@ import faang.school.projectservice.dto.initiative.InitiativeDto;
 import faang.school.projectservice.dto.initiative.InitiativeFilterDto;
 import faang.school.projectservice.service.initiative.InitiativeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,31 +27,31 @@ public class InitiativeController {
 
     @Operation(summary = "Create new initiative")
     @PostMapping("/create")
-    public InitiativeDto create(@RequestBody InitiativeDto initiative) {
+    public InitiativeDto create(@ParameterObject @RequestBody InitiativeDto initiative) {
         return initiativeService.create(initiative);
     }
 
     @Operation(summary = "Update initiative")
     @PutMapping("/update")
-    public InitiativeDto update(@RequestBody InitiativeDto initiative) {
+    public InitiativeDto update(@ParameterObject @RequestBody InitiativeDto initiative) {
         return initiativeService.update(initiative);
     }
 
     @Operation(summary = "Get all initiatives by filter")
     @GetMapping
-    public List<InitiativeDto> getAllByFilter(@RequestBody InitiativeFilterDto filters) {
+    public List<InitiativeDto> getAllByFilter(@ParameterObject @RequestBody InitiativeFilterDto filters) {
         return initiativeService.getAllByFilter(filters);
     }
 
     @Operation(summary = "Get all initiatives")
-    @GetMapping
+    @GetMapping("/filter")
     public List<InitiativeDto> getAll() {
         return initiativeService.getAll();
     }
 
     @Operation(summary = "Get initiative by id")
     @GetMapping("/{id}")
-    public InitiativeDto getById(@PathVariable long id) {
+    public InitiativeDto getById(@Parameter @PathVariable long id) {
         return initiativeService.getById(id);
     }
 }
