aad069bca64519360d636a5837482870127cdbac
Tem <tipokruto014@gmail.com>
2024-05-10 00:28:20 +0300

    refactor: перенес маппиг Initiative в MomentDto из MomentMapper в MomentServiceImpl

--- src/main/java/faang/school/projectservice/service/moment/MomentServiceImpl.java
+++ src/main/java/faang/school/projectservice/service/moment/MomentServiceImpl.java
@@ -3,10 +3,14 @@ package faang.school.projectservice.service.moment;
 import faang.school.projectservice.dto.moment.MomentDto;
 import faang.school.projectservice.mapper.MomentMapper;
 import faang.school.projectservice.model.Moment;
+import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.TeamMember;
 import faang.school.projectservice.model.initiative.Initiative;
 import faang.school.projectservice.repository.MomentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +28,24 @@ public class MomentServiceImpl implements MomentService {
 
     @Override
     public MomentDto createFromInitiative(Initiative initiative) {
-        MomentDto momentDto = mapper.toDtoFromInitiative(initiative);
+        MomentDto momentDto = new MomentDto();
+
+        momentDto.setName(initiative.getName());
+        momentDto.setDescription(momentDto.getDescription());
+        momentDto.setImageId(initiative.getCoverImageId());
+
+        List<Long> projectIds = initiative.getSharingProjects().stream()
+                .map(Project::getId)
+                .toList();
+
+        momentDto.setProjectIds(projectIds);
+
+        List<Long> userIds = initiative.getStages().stream()
+                .flatMap(stage -> stage.getExecutors().stream())
+                .map(TeamMember::getUserId)
+                .toList();
+
+        momentDto.setUserIds(userIds);
 
         return create(momentDto);
     }
