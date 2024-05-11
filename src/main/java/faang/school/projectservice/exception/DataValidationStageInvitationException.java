356e20bdc9d5ad9e3d308581b298194fcf8a154a
romantarelicev <romuhut@mail.ru>
2024-05-11 15:10:37 +0300

    Основной функционал, нужно еще кое-что доделать!(не забудь про валидацию) Пока не покрыт тестами.

---
+++ src/main/java/faang/school/projectservice/exception/DataValidationStageInvitationException.java
@@ -0,0 +1,7 @@
+package faang.school.projectservice.exception;
+
+public class DataValidationStageInvitationException extends RuntimeException {
+    public DataValidationStageInvitationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/faang/school/projectservice/service/filter/stage_invitation_filter/StageInvitationStatusFilter.java
@@ -0,0 +1,17 @@
+package faang.school.projectservice.service.filter.stage_invitation_filter;
+
+import faang.school.projectservice.dto.filter.StageInvitationFilterDto;
+import faang.school.projectservice.model.stage_invitation.StageInvitation;
+
+public class StageInvitationStatusFilter implements StageInvitationFilter {
+
+    @Override
+    public boolean isApplicable(StageInvitationFilterDto filterDto) {
+        return filterDto.getStatus() != null;
+    }
+
+    @Override
+    public boolean apply(StageInvitation stageInvitation, StageInvitationFilterDto filterDto) {
+        return stageInvitation.getStatus().equals(filterDto.getStatus());
+    }
+}
