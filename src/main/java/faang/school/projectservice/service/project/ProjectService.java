05f843365e96e8d5e28c14313b03d6633c290007
nadir <den4ik.strelok@gmail.com>
2024-05-15 09:02:39 +0300

    Удалил неиспользуемый метод из ProjectService

--- src/main/java/faang/school/projectservice/service/project/ProjectService.java
+++ src/main/java/faang/school/projectservice/service/project/ProjectService.java
@@ -1,6 +1,5 @@
 package faang.school.projectservice.service.project;
 
-import faang.school.projectservice.model.Project;
 import faang.school.projectservice.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,10 +8,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
-
-    public Project getProjectById(Long projectId) {
-        return projectRepository.getProjectById(projectId);
-    }
 
     public boolean existsById(Long projectId) {
         return projectRepository.existsById(projectId);
