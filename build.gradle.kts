dbf569aa0e9e6d57cfc2ea8558a866f1e03c1fb3
KuvarzinAlex <fargus.wow@mail.ru>
2024-05-20 23:54:56 +0500

    Feature-BJS2-6384 Добавил эндпоинты.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springdoc","springdoc-openapi-starter-webmvc-ui", "2.0.2")
 
     /**
      * Database
--- src/main/java/faang/school/projectservice/controller/ProjectController.java
+++ src/main/java/faang/school/projectservice/controller/ProjectController.java
@@ -6,37 +6,43 @@ import faang.school.projectservice.model.Project;
 import faang.school.projectservice.service.ProjectService;
 import faang.school.projectservice.validator.ProjectValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/projects")
 public class ProjectController {
 
     private final ProjectService projectService;
     private final ProjectValidator projectValidator;
 
-    public ProjectDto create(ProjectDto projectDto) {
+    @PostMapping("/create")
+    public ProjectDto create(@RequestBody ProjectDto projectDto) {
         projectValidator.checkIsNull(projectDto);
         return projectService.create(projectDto);
     }
 
-    public ProjectDto update(ProjectDto projectDto) {
+    @PutMapping("/update")
+    public ProjectDto update(@RequestBody ProjectDto projectDto) {
         projectValidator.checkIsNull(projectDto);
         return projectService.update(projectDto);
     }
 
-    public List<Project> getProjectsByFilter(ProjectFilterDto projectFilterDto) {
+    @PostMapping("/filter")
+    public List<Project> getProjectsByFilter(@RequestBody ProjectFilterDto projectFilterDto) {
         projectValidator.checkIsNull(projectFilterDto);
         return projectService.getProjectsByFilter(projectFilterDto);
     }
 
+    @GetMapping()
     public List<Project> getAllProjects() {
         return projectService.getAllProjects();
     }
 
-    public Project getProjectById(Long projectId) {
+    @GetMapping("/{id}")
+    public Project getProjectById(@PathVariable("id") Long projectId) {
         projectValidator.checkIsNull(projectId);
         return projectService.getProjectById(projectId);
     }
