66ca7087acaca0338e773c480db1991f278910c4
Tem <tipokruto014@gmail.com>
2024-05-26 19:01:45 +0300

    Добавил аннотации Size

--- src/main/java/faang/school/projectservice/validation/InitiativeValidator.java
+++ src/main/java/faang/school/projectservice/validation/InitiativeValidator.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class InitiativeValidator {
+
     private final StageRepository stageRepository;
     private final TeamMemberRepository teamMemberRepository;
 
