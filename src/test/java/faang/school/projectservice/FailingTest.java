a6ce3c6c2f3098725ddd24984e8a663afbdc097f
SoslanDzgoev <dzgsoslan@gmail.com>
2024-05-27 17:39:12 +0400

    Fix bugs

--- src/test/java/faang/school/projectservice/FailingTest.java
+++ src/test/java/faang/school/projectservice/FailingTest.java
@@ -2,7 +2,7 @@ package faang.school.projectservice;
 
 import org.junit.jupiter.api.Test;
 
-import static org.aspectj.bridge.MessageUtil.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class FailingTest {
     @Test
