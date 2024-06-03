b33089225353687633a70603484ccf852b3ca706
Andrei Archakov <archandrwork@gmail.com>
2024-06-04 00:45:38 +0300

    BJS2-6448: add swagger description for controllers

--- src/main/java/faang/school/projectservice/controller/InternshipController.java
+++ src/main/java/faang/school/projectservice/controller/InternshipController.java
@@ -3,7 +3,8 @@ package faang.school.projectservice.controller;
 import faang.school.projectservice.dto.InternshipDto;
 import faang.school.projectservice.dto.InternshipFilterDto;
 import faang.school.projectservice.service.InternshipService;
-import faang.school.projectservice.validator.InternshipValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/internship")
 @Validated
+@Tag(name = "Internship", description = "Operations related to internship")
 public class InternshipController {
 
     private final InternshipService internshipService;
@@ -29,27 +31,32 @@ public class InternshipController {
     private static final String ID_ERR_MESSAGE = "Internship id must be more then 0";
 
     @PostMapping
+    @Operation(summary = "Create a new internship", description = "Create a new internship")
     public InternshipDto create(@Valid @RequestBody InternshipDto internshipDto) {
         return internshipService.create(internshipDto);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an internship", description = "Updates an existing internship")
     public InternshipDto update(@Valid @RequestBody InternshipDto internshipDto,
                                 @PathVariable @Min(value = 1, message = ID_ERR_MESSAGE) long id) {
         return internshipService.update(internshipDto, id);
     }
 
     @GetMapping("/filter")
+    @Operation(summary = "Get all internships with filter", description = "Retrieves a list of filtered internships")
     public List<InternshipDto> findAllWithFilter(@Valid @RequestBody InternshipFilterDto internshipFilterDto) {
         return internshipService.findAllWithFilter(internshipFilterDto);
     }
 
     @GetMapping
+    @Operation(summary = "Get all internships", description = "Retrieves a list of all internships")
     public List<InternshipDto> findAll() {
         return internshipService.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get internship by ID", description = "Retrieves an internship by its ID")
     public InternshipDto findById(@PathVariable @Min(value = 1, message = ID_ERR_MESSAGE) long id) {
         return internshipService.findById(id);
     }
--- src/main/java/faang/school/projectservice/controller/VacancyController.java
+++ src/main/java/faang/school/projectservice/controller/VacancyController.java
@@ -3,6 +3,8 @@ package faang.school.projectservice.controller;
 import faang.school.projectservice.dto.VacancyDto;
 import faang.school.projectservice.dto.VacancyFilterDto;
 import faang.school.projectservice.service.VacancyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
@@ -17,41 +19,47 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/vacancies")
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "Vacancies", description = "Operations related to vacancies")
 public class VacancyController {
 
     private final VacancyService vacancyService;
-    private static final String ID_ERR_MESSAGE = "Vacancy id must be more then 0";
+    private static final String ID_ERR_MESSAGE = "Vacancy id must be more than 0";
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new vacancy", description = "Creates a new vacancy with the provided details")
     public VacancyDto create(@Valid @RequestBody VacancyDto vacancyDto) {
         return vacancyService.create(vacancyDto);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a vacancy", description = "Updates an existing vacancy with the provided details")
     public VacancyDto update(@Valid @RequestBody VacancyDto vacancyDto,
                              @PathVariable @Min(value = 1, message = ID_ERR_MESSAGE) Long id) {
         return vacancyService.update(vacancyDto, id);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete a vacancy", description = "Deletes the vacancy with the specified ID")
     public void delete(@PathVariable @Min(value = 1, message = ID_ERR_MESSAGE) Long id) {
         vacancyService.delete(id);
     }
 
     @GetMapping("/filter")
+    @Operation(summary = "Get vacancies by filter", description = "Retrieves a list of vacancies that match the specified filter criteria")
     public List<VacancyDto> getAllByFilter(@Valid @RequestBody VacancyFilterDto vacancyFilterDto) {
         return vacancyService.getAllByFilter(vacancyFilterDto);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find vacancy by ID", description = "Retrieves the details of a vacancy by its ID")
     public VacancyDto findByID(@PathVariable @Min(value = 1, message = ID_ERR_MESSAGE) Long id) {
         return vacancyService.findById(id);
     }
