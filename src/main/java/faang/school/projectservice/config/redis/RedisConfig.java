023361e1a07828283aef502e6d77957382981994
Tem <tipokruto014@gmail.com>
2024-06-03 16:23:48 +0300

    naming fix

--- src/main/java/faang/school/projectservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/projectservice/config/redis/RedisConfig.java
@@ -3,7 +3,6 @@ package faang.school.projectservice.config.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
