0de02f5a498efb52433e747f6ccd83418bace325
Tem <tipokruto014@gmail.com>
2024-06-06 22:49:27 +0300

    fix property files scan

--- src/main/java/faang/school/projectservice/property/AmazonS3Properties.java
+++ src/main/java/faang/school/projectservice/property/AmazonS3Properties.java
@@ -2,12 +2,10 @@ package faang.school.projectservice.property;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.math.BigInteger;
 
 @Data
-@Configuration
 @ConfigurationProperties(prefix = "services.s3")
 public class AmazonS3Properties {
     private String endpoint;
