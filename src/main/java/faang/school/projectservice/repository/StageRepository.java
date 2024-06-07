4ed11f3673a5024785d44361234bdc6c7c08bab2
Tem <tipokruto014@gmail.com>
2024-06-07 22:51:58 +0300

    stage refactor

--- src/main/java/faang/school/projectservice/repository/StageRepository.java
+++ src/main/java/faang/school/projectservice/repository/StageRepository.java
@@ -1,37 +1,9 @@
 package faang.school.projectservice.repository;
 
-import faang.school.projectservice.jpa.StageJpaRepository;
 import faang.school.projectservice.model.stage.Stage;
-import jakarta.persistence.EntityNotFoundException;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-@RequiredArgsConstructor
-public class StageRepository {
-    private final StageJpaRepository jpaRepository;
-
-    public Stage save(Stage stage) {
-        return jpaRepository.save(stage);
-    }
-
-    public void delete(Stage stage) {
-        jpaRepository.delete(stage);
-    }
-
-    public Stage getById(Long stageId) {
-        return jpaRepository.findById(stageId).orElseThrow(
-                () -> new EntityNotFoundException(String.format("Stage not found by id: %s", stageId))
-        );
-    }
-
-    public List<Stage> findAll() {
-        return jpaRepository.findAll();
-    }
-
-    public boolean existsById(Long stageId) {
-        return jpaRepository.existsById(stageId);
-    }
+public interface StageRepository extends JpaRepository<Stage, Long> {
 }
