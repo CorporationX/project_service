b531f8f4d5e61d0ef730f879eaa52506c237c92b
Th0rn-dev <solnek33@gmail.com>
2024-06-15 23:09:03 +0300

    fixed the problem with the filter

--- src/main/java/faang/school/projectservice/filter/project/ProjectNameFilter.java
+++ src/main/java/faang/school/projectservice/filter/project/ProjectNameFilter.java
@@ -2,10 +2,12 @@ package faang.school.projectservice.filter.project;
 
 import faang.school.projectservice.dto.project.ProjectFilterDto;
 import faang.school.projectservice.model.Project;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.stream.Stream;
 
+@Component
 public class ProjectNameFilter implements ProjectFilter{
     @Override
     public boolean isApplicable(ProjectFilterDto filters) {
@@ -14,6 +16,9 @@ public class ProjectNameFilter implements ProjectFilter{
 
     @Override
     public Stream<Project> apply(Stream<Project> projects, ProjectFilterDto filters) {
-        return projects.filter(project -> project.getName().equals(filters.getName()));
+        return projects.filter(project -> project
+                .getName()
+                .toLowerCase()
+                .contains(filters.getName().toLowerCase()));
     }
 }
