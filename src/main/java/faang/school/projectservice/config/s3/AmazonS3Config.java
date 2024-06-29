dddacccf4949035529ba228fc463057e3fef2458
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-06-29 22:19:57 +0300

    some fix 2

--- src/main/java/faang/school/projectservice/config/s3/AmazonS3Config.java
+++ src/main/java/faang/school/projectservice/config/s3/AmazonS3Config.java
@@ -6,36 +6,30 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import faang.school.projectservice.config.properties.S3Properties;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Data
 @Slf4j
-@Configuration
-@ConfigurationProperties(prefix = "services.s3")
-@ConfigurationPropertiesScan
+@RequiredArgsConstructor
 public class AmazonS3Config {
-    private String endpoint;
-    private String accessKey;
-    private String secretKey;
-    private String bucketName;
+    private final S3Properties s3Properties;
 
     @Bean
     public AmazonS3 amazonS3() {
         AmazonS3 s3client = AmazonS3ClientBuilder
                 .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(s3Properties.getAccessKey(), s3Properties.getSecretKey())))
                 .withEndpointConfiguration(new AwsClientBuilder
-                        .EndpointConfiguration(endpoint, Regions.DEFAULT_REGION.getName()))
+                        .EndpointConfiguration(s3Properties.getEndpoint(), Regions.DEFAULT_REGION.getName()))
                 .withPathStyleAccessEnabled(true)
                 .build();
 
-        if (!s3client.doesBucketExistV2(bucketName)) {
-            s3client.createBucket(bucketName);
+        if (!s3client.doesBucketExistV2(s3Properties.getBucketName())) {
+            s3client.createBucket(s3Properties.getBucketName());
         }
         log.info("Connected to AmazonS3.");
         return s3client;
