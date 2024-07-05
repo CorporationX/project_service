cd65e5170a7bd4cf5ed580165e03f3f6b0a1e518
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-07-05 09:16:27 +0300

    инит SwaggerConfig, добавил Swagger анотации в ProjectController, добавил Swagger анотации в ProjectResourceController, добавил Swagger анотации в InternshipController

--- src/main/java/faang/school/projectservice/controller/internship/InternshipController.java
+++ src/main/java/faang/school/projectservice/controller/internship/InternshipController.java
@@ -4,15 +4,12 @@ import faang.school.projectservice.dto.internship.InternshipDto;
 import faang.school.projectservice.dto.internship.filter.InternshipFilterDto;
 import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.service.internship.InternshipService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,11 +17,16 @@ import static faang.school.projectservice.exception.InternshipValidationExceptio
 
 @RequestMapping("/internship")
 @RestController
+@Tag(name = "internship", description = "Контроллеры управления стажировками")
 @RequiredArgsConstructor
 public class InternshipController {
     private final InternshipService internshipService;
     private final InternshipControllerValidation internshipControllerValidation;
 
+
+    @Operation(summary = "Создание стажировки", tags = {"internship"})
+    @ApiResponse(responseCode = "201", description = "Стажировка успешно создана")
+    @ApiResponse(responseCode = "400", description = "Ошибка на стороне клиента")
     @PostMapping
     public InternshipDto create(@Valid @RequestBody InternshipDto internshipDto) {
         internshipControllerValidation.validateInternshipDuration(internshipDto);
@@ -33,6 +35,9 @@ public class InternshipController {
         return internshipService.create(internshipDto);
     }
 
+    @Operation(summary = "Обновление стажировки", tags = {"internship"})
+    @ApiResponse(responseCode = "200", description = "Стажировка успешно обновлена")
+    @ApiResponse(responseCode = "400", description = "Ошибка на стороне клиента")
     @PutMapping
     public InternshipDto update(@Valid @RequestBody InternshipDto internshipDto) {
         internshipControllerValidation.validateInternshipDuration(internshipDto);
@@ -44,17 +49,26 @@ public class InternshipController {
         return internshipService.update(internshipDto);
     }
 
+    @Operation(summary = "Получение стажировок из проекта", tags = {"internship"})
+    @ApiResponse(responseCode = "200", description = "Стажировка из проекта успешно получена")
+    @ApiResponse(responseCode = "400", description = "Ошибка на стороне клиента")
     @PostMapping(value = "/project/{projectId}")
     public List<InternshipDto> getInternshipsOfProject(@PathVariable Long projectId,
                                                        @RequestBody InternshipFilterDto filter) {
         return internshipService.getInternshipsOfProject(projectId, filter);
     }
 
+    @Operation(summary = "Получение всех стажировок", tags = {"internship"})
+    @ApiResponse(responseCode = "200", description = "Все стажировки успешно получены")
+    @ApiResponse(responseCode = "400", description = "Ошибка на стороне клиента")
     @GetMapping
     public List<InternshipDto> getAllInternships() {
         return internshipService.getAllInternships();
     }
 
+    @Operation(summary = "Удаление стажировок согласно id", tags = {"internship"})
+    @ApiResponse(responseCode = "200", description = "Стажировка успешно удалена")
+    @ApiResponse(responseCode = "400", description = "Ошибка на стороне клиента")
     @GetMapping("/{internshipId}")
     public InternshipDto getInternshipById(@PathVariable Long internshipId) {
         return internshipService.getInternshipById(internshipId);
--- src/main/java/faang/school/projectservice/controller/project/ProjectResourceController.java
+++ src/main/java/faang/school/projectservice/controller/project/ProjectResourceController.java
@@ -1,29 +1,29 @@
 package faang.school.projectservice.controller.project;
 
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import faang.school.projectservice.dto.resource.ResourceDto;
 import faang.school.projectservice.service.resource.ProjectResourceType;
 import faang.school.projectservice.service.resource.ResourceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "projectsResource", description = "Контроллеры для загрузки ресурсов")
 @RequestMapping("resources")
 public class ProjectResourceController {
     private final ResourceService resourceService;
-    
+
+    @Operation(summary = "Загрузка ресурсов", tags = {"projectsResource"})
+    @ApiResponse(responseCode = "200", description = "Ресурс успешно загружен")
+    @ApiResponse(responseCode = "400", description = "Ошибка на стороне клиента")
     @PostMapping("{projectId}")
     public ResourceDto uploadResource(
         @PathVariable @NotNull(message = "Project id can't be null")
@@ -39,7 +39,10 @@ public class ProjectResourceController {
     ) {
         return resourceService.uploadResource(projectId, teamMemberId, file, ProjectResourceType.PROJECT_RESOURCE);
     }
-    
+
+    @Operation(summary = "Загрузка обложки проекта", tags = {"projectsResource"})
+    @ApiResponse(responseCode = "200", description = "Обложка проекта успешно загружена")
+    @ApiResponse(responseCode = "400", description = "Ошибка на стороне клиента")
     @PostMapping("{projectId}/cover")
     public ResourceDto uploadProjectCover(
         @PathVariable @NotNull(message = "Project id can't be null")
@@ -55,7 +58,10 @@ public class ProjectResourceController {
     ) {
         return resourceService.uploadProjectCover(projectId, teamMemberId, file);
     }
-    
+
+    @Operation(summary = "Удаление обложки проекта", tags = {"projectsResource"})
+    @ApiResponse(responseCode = "204", description = "Обложка проекта успешно удалена")
+    @ApiResponse(responseCode = "400", description = "Ошибка на стороне клиента")
     @DeleteMapping("{projectId}/cover")
     public void deleteProjectCover(
         @PathVariable @NotNull(message = "Project id can't be null") @PositiveOrZero(message = "Project id can't be negative") Long projectId
