e9ef4201f16ee8ecb23060651e95a60e35ecb6f3
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-07-09 03:39:21 +0500

    BJS2-15587 - Continuous Integration feature testing
    + added failing test

--- src/test/java/faang/school/projectservice/ProjectServiceApplicationTests.java
+++ src/test/java/faang/school/projectservice/ProjectServiceApplicationTests.java
@@ -7,6 +7,6 @@ import org.junit.jupiter.api.Test;
 class ProjectServiceApplicationTests {
     @Test
     void contextLoads() {
-        Assertions.assertThat(40 + 2).isEqualTo(42);
+        Assertions.assertThat(40 + 2).isEqualTo(40);
     }
 }
