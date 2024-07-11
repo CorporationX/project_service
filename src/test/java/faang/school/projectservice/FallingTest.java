a520fc061085e60083c39364d51c50a2b8f845ba
V1nGeri|Artem Radkevich <artemka20040613@gmail.com>
2024-07-11 17:47:11 +0300

    Добавил тест который всегда падает

---
+++ src/test/java/faang/school/projectservice/FallingTest.java
@@ -0,0 +1,13 @@
+package faang.school.projectservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FallingTest {
+
+    @Test
+    public void testFailure(){
+        fail("Этот тест всегда падает");
+    }
+}
