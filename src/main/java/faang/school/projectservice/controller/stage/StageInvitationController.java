e791354be0b0d1db7f8777846ca39658d683d136
Gevorg <a0964593@gmail.com>
2024-07-15 19:53:55 +0300

    Закончил все задачи + тесты

--- src/main/java/faang/school/projectservice/controller/stage/StageInvitationController.java
+++ src/main/java/faang/school/projectservice/controller/stage/StageInvitationController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
@@ -21,26 +24,30 @@ public class StageInvitationController {
     private final StageInvitationService stageInvitationService;
 
     @PostMapping
+    @ResponseBody
     @ResponseStatus(HttpStatus.CREATED)
-    public StageInvitationDto requestStageInvitation(StageInvitationDto stageInvitationDto) {
+    public StageInvitationDto requestStageInvitation(@RequestBody StageInvitationDto stageInvitationDto) {
         return stageInvitationService.create(stageInvitationDto);
     }
 
     @PutMapping("/accept/{id}")
+    @ResponseBody
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public StageInvitationDto acceptStageInvitation(Long id) {
+    public StageInvitationDto acceptStageInvitation(@RequestBody Long id) {
         return stageInvitationService.acceptStageInvitation(id);
     }
 
     @PutMapping("/reject/{id}")
+    @ResponseBody
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public StageInvitationDto rejectStageInvitation(Long id, String rejectionReason) {
+    public StageInvitationDto rejectStageInvitation(@PathVariable Long id, @RequestBody String rejectionReason) {
         return stageInvitationService.rejectStageInvitation(id, rejectionReason);
     }
 
     @GetMapping("/show/{id}")
+    @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    public List<StageInvitationDto> showMemberStageInvitations(Long id, StageInvitationFilterDto stageInvitationFilterDto) {
+    public List<StageInvitationDto> showMemberStageInvitations(@PathVariable Long id, @RequestBody StageInvitationFilterDto stageInvitationFilterDto) {
         return stageInvitationService.getMemberStageInvitations(id, stageInvitationFilterDto);
     }
 }
