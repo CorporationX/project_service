44083c286e9ea55612ed438efc2d96f627f1ba02
Max Prigonyuk <kael.smr@gmail.com>
2024-07-15 23:42:06 +0400

    Fail test

--- src/test/java/faang/school/projectservice/ProjectServiceApplicationTests.java
+++ src/test/java/faang/school/projectservice/ProjectServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package faang.school.projectservice;
 
 import org.assertj.core.api.Assertions;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
 
@@ -8,5 +9,10 @@ class ProjectServiceApplicationTests {
     @Test
     void contextLoads() {
         Assertions.assertThat(40 + 2).isEqualTo(42);
+    }
+
+    @Test
+    void testShouldFail(){
+        fail("Test failed");
     }
 }
