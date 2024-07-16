cb62a6b3c2b56c96841c887aa4eb691b8459d8ad
Gevorg <a0964593@gmail.com>
2024-07-16 13:45:02 +0300

    Закончил все задачи + тесты

--- src/main/java/faang/school/projectservice/controller/stage/StageInvitationController.java
+++ src/main/java/faang/school/projectservice/controller/stage/StageInvitationController.java
@@ -5,14 +5,12 @@ import faang.school.projectservice.dto.stage.StageInvitationFilterDto;
 import faang.school.projectservice.service.stage.StageInvitationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
