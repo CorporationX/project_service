3eeac7125d52f2631f32442ed42d1cd4c75c4c63
V1nGeri|Artem Radkevich <artemka20040613@gmail.com>
2024-07-16 11:24:12 +0300

    проверка успешного теста

--- src/test/java/faang/school/projectservice/FallingTest.java
+++ src/test/java/faang/school/projectservice/FallingTest.java
@@ -2,12 +2,13 @@ package faang.school.projectservice;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class FallingTest {
 
     @Test
     public void testFailure(){
-        fail("Этот тест всегда падает");
+        assertEquals(1,1);
     }
 }
