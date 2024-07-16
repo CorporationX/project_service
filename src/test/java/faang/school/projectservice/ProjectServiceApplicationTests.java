5c227605ff2868c8218567ec39c3a459d557efc3
Max Prigonyuk <kael.smr@gmail.com>
2024-07-16 15:16:48 +0400

    update ci for testing

--- src/test/java/faang/school/projectservice/ProjectServiceApplicationTests.java
+++ src/test/java/faang/school/projectservice/ProjectServiceApplicationTests.java
@@ -1,12 +1,16 @@
 package faang.school.projectservice;
 
 import org.assertj.core.api.Assertions;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
 
 class ProjectServiceApplicationTests {
+
+
     @Test
-    void contextLoads() {
-        Assertions.assertThat(40 + 2).isEqualTo(42);
+    void testFail(){
+        fail("Test fail");
     }
+
 }
