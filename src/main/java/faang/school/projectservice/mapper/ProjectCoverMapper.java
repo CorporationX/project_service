f404365be52a3f2b7d75e6b698ff3be30943e69a
Muhammad <dzhabrailov3.muhammad@yandex.ru>
2024-07-18 23:58:25 +0300

    дебаг ликвибейс дебаг ProjectCoverMapper

--- src/main/java/faang/school/projectservice/mapper/ProjectCoverMapper.java
+++ src/main/java/faang/school/projectservice/mapper/ProjectCoverMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProjectCoverMapper {
 
     ProjectCoverDto toDto(Project project);
--- src/main/resources/db/changelog/changeset/project_V004__alter_resource.sql
+++ src/main/resources/db/changelog/changeset/project_V004__alter_resource.sql
@@ -1,3 +1,3 @@
-alter table project_resource
+alter table resource
 add column if not exists created_by BIGINT,
 add column if not exists updated_by BIGINT;
