277e0b5db0143895de5da52b79a69912f0aad9a1
Nikita <nikita.tolstv@gmail.com>
2024-07-19 17:34:35 +0500

    Check test

---
+++ src/test/java/faang/school/projectservice/FailTest.java
@@ -0,0 +1,13 @@
+package faang.school.projectservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FailTest {
+
+    @Test
+    public void test() {
+        fail("Fail test");
+    }
+}
