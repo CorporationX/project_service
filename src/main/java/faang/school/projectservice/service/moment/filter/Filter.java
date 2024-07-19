085daa6b1bca763903bd6ffac28c811123384b08
MaximVodolazsky <63898235+MaxVodolazhsky@users.noreply.github.com>
2024-07-19 16:39:49 +0300

    Revert "BJS2-16901"

--- src/main/java/faang/school/projectservice/service/moment/filter/Filter.java
+++
@@ -1,8 +0,0 @@
-package faang.school.projectservice.service.moment.filter;
-
-import java.util.stream.Stream;
-
-public interface Filter<T, F> {
-    boolean isApplicable(F filter);
-    Stream<T> getApplicableFilters(Stream<T> stream, F filter);
-}
