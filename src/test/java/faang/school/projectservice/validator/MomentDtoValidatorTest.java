e0ef702c4aa05d7f546b356d3fd7cfb7e4700587
Andrei Kuzmin <Andreytko@mail.ru>
2024-07-26 23:28:38 +0300

    test for MomentFilterDtoValidator

---
+++ src/test/java/faang/school/projectservice/validator/MomentDtoValidatorTest.java
@@ -0,0 +1,8 @@
+package faang.school.projectservice.validator;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MomentDtoValidatorTest {
+}
