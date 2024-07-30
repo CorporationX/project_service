055f295a0ce6f40e811a94e94461c8d288c3697e
Nikita2768 <vechkinnikita22@gmail.com>
2024-07-30 20:41:59 +0500

    Добавление обложки к проекту finish without test

---
+++ src/main/java/faang/school/projectservice/exception/FileDownloadException.java
@@ -0,0 +1,12 @@
+package faang.school.projectservice.exception;
+
+public class FileDownloadException extends RuntimeException {
+
+    public FileDownloadException(String message) {
+        super(message);
+    }
+
+    public FileDownloadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
