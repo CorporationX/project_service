07c012625f96f4923e8265858b119655e6d1313c
Arman Bistibekov <bistibekov.a@gmail.com>
2024-07-31 18:35:51 -0400

    Implemented DataValidationException and ErrorMessage. Need to test after merge

---
+++ src/main/java/faang/school/projectservice/exceptions/DataValidationException.java
@@ -0,0 +1,11 @@
+package faang.school.projectservice.exceptions;
+
+public class DataValidationException extends RuntimeException{
+    public DataValidationException(String message) {
+        super(message);
+    }
+
+    public DataValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
