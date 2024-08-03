03ba3ae7f54dc8cf7ee61cf5e07df9cd71fd3688
Nikita Volzhin <vlzh.nik@mail.ru>
2024-08-04 01:14:33 +0500

    BJS2-15575

--- src/main/java/faang/school/projectservice/service/ProjectService.java
+++ src/main/java/faang/school/projectservice/service/ProjectService.java
@@ -1,22 +1,100 @@
 package faang.school.projectservice.service;
 
+import faang.school.projectservice.config.context.UserContext;
+import faang.school.projectservice.dto.filter.ProjectFilterDto;
+import faang.school.projectservice.dto.project.ProjectDto;
+import faang.school.projectservice.filter.ProjectFilter;
+import faang.school.projectservice.mapper.ProjectMapper;
 import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.ProjectStatus;
+import faang.school.projectservice.model.ProjectVisibility;
 import faang.school.projectservice.model.TeamMember;
 import faang.school.projectservice.repository.ProjectRepository;
 import faang.school.projectservice.repository.TeamMemberRepository;
-import lombok.RequiredArgsConstructor;
+import faang.school.projectservice.validator.ProjectValidator;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+@Slf4j
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+    private final UserContext userContext;
+    private final ProjectValidator projectValidator;
+    private final List<ProjectFilter> projectFilters;
     private final TeamMemberRepository teamMemberRepository;
+
+    @Transactional(readOnly = true)
+    public List<ProjectDto> findAll() {
+        List<Project> projects = projectRepository.findAll();
+        return projectMapper.entitiesToDtos(projects);
+    }
+
+    @Transactional
+    public ProjectDto createProject(ProjectDto projectDto) {
+        if (projectDto.getOwnerId() == 0) {
+            projectDto.setOwnerId(userContext.getUserId());
+        }
+        projectValidator.validateProjectByOwnerWithNameOfProject(projectDto);
+        projectDto.setStatus(ProjectStatus.CREATED);
+        Project project = projectMapper.dtoToEntity(projectDto);
+        return projectMapper.entityToDto(projectRepository.save(project));
+    }
+
+    @Transactional
+    public ProjectDto updateProject(ProjectDto updatedProjectDto) {
+        existById(updatedProjectDto.getId());
+        Project project = projectRepository.getProjectById(updatedProjectDto.getId());
+        project.setName(updatedProjectDto.getName());
+        project.setDescription(project.getDescription());
+        project.setStatus(updatedProjectDto.getStatus());
+        project.setVisibility(updatedProjectDto.getVisibility());
+        return projectMapper.entityToDto(projectRepository.save(project));
+    }
+
+    @Transactional(readOnly = true)
+    public ProjectDto findById(Long id) {
+        existById(id);
+        return projectMapper.entityToDto(projectRepository.getProjectById(id));
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProjectDto> getAllProjectByFilters(ProjectFilterDto projectFilter) {
+        Stream<Project> projects = projectRepository.findAll().stream();
+        Stream<Project> filtredProjects = projectFilters.stream()
+                .filter(filter -> filter.isApplicable(projectFilter))
+                .flatMap(filter -> filter.apply(projects, projectFilter));
+        Predicate<Project> filterByVisibility = project -> !project.getVisibility().equals(ProjectVisibility.PRIVATE)
+                || project.getTeams().stream()
+                .flatMap(team -> team.getTeamMembers().stream())
+                .anyMatch(teamMember -> teamMember.getId().equals(userContext.getUserId()));
+        return filtredProjects
+                .filter(filterByVisibility)
+                .map(projectMapper::entityToDto)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public boolean existById(Long id) {
+        if (!projectRepository.existsById(id)) {
+            String msg = "Project with id:%d doesn't exist";
+            log.error(msg, id);
+            throw new EntityNotFoundException(String.format(msg, id));
+        }
+        return projectRepository.existsById(id);
+    }
 
     public List<Project> findDifferentProjects(List<Project> projectsFromDataBase, List<Long> newProjectIds) {
         List<Long> existingProjectIds = projectsFromDataBase.stream()
---
+++ src/main/java/faang/school/projectservice/validator/ProjectValidator.java
@@ -0,0 +1,23 @@
+package faang.school.projectservice.validator;
+
+import faang.school.projectservice.dto.project.ProjectDto;
+import faang.school.projectservice.exception.DataValidationException;
+import faang.school.projectservice.repository.ProjectRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ProjectValidator {
+    private final ProjectRepository projectRepository;
+    private final String VALIDATION_MASSAGE_NAME_OF_PROJECT_IS_EXISTS =
+            "The owner with id:%d already has a project with  name:%s";
+    public void validateProjectByOwnerWithNameOfProject(ProjectDto projectDto) {
+        if(projectRepository.existsByOwnerUserIdAndName(projectDto.getOwnerId(), projectDto.getName())){
+            throw new DataValidationException(String.format(VALIDATION_MASSAGE_NAME_OF_PROJECT_IS_EXISTS,
+                    projectDto.getOwnerId(), projectDto.getName()));
+        }
+    }
+}
