5e6416a5263d6be1509658aa80482652e3b9eb98
Rauan Kaztaev <rtishtyk@gmail.com>
2024-09-07 22:05:06 +0500

    Вернул конфигурацию топика

---
+++ src/main/java/faang/school/projectservice/config/kafka/KafkaTopic.java
@@ -0,0 +1,19 @@
+package faang.school.projectservice.config.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopic {
+
+    @Value("${spring.kafka.topics.project_topic}")
+    private String projectTopicName;
+
+    @Bean
+    public NewTopic projectTopic() {
+        return TopicBuilder.name(projectTopicName).build();
+    }
+}
