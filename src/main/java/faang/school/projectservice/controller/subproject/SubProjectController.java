d61d2061c28b3606f301dba6b0b477499c50ba15
zeus <denis@fatum.ru>
2024-09-10 13:11:46 +0300

    BJS2-27209

---
+++ src/main/java/faang/school/projectservice/controller/subproject/SubProjectController.java
@@ -0,0 +1,7 @@
+package faang.school.projectservice.controller.subproject;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SubProjectController {
+}
---
+++ src/main/java/faang/school/projectservice/dto/client/subproject/ProjectDto.java
@@ -0,0 +1,7 @@
+package faang.school.projectservice.dto.client.subproject;
+
+import lombok.Data;
+
+@Data
+public class ProjectDto {
+}
