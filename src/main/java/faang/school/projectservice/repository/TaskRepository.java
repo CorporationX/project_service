a5635fd101fc016f0c0662fac6d3c4a30d6a5826
TheGreatLucky <danke_villetti@mail.ru>
2024-09-13 13:21:13 +0500

    created Task utils

---
+++ src/main/java/faang/school/projectservice/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package faang.school.projectservice.repository;
+
+import feign.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository {
+
+}
