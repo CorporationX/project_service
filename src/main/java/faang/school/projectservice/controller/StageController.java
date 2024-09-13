04dcaf7619cd76dafe0092f7605fa3148047244c
TheGreatLucky <danke_villetti@mail.ru>
2024-09-13 09:40:07 +0500

    added method createStage in StageService

--- src/main/java/faang/school/projectservice/controller/StageController.java
+++ src/main/java/faang/school/projectservice/controller/StageController.java
@@ -1,21 +1,25 @@
 package faang.school.projectservice.controller;
 
 import faang.school.projectservice.controller.validation.StageValidator;
-import faang.school.projectservice.dto.project.StageDto;
+import faang.school.projectservice.dto.stage.StageDto;
+import faang.school.projectservice.service.StageService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class StageController {
+    private final StageService stageService;
     private final StageValidator stageValidator;
 
-
     @PostMapping("/stage")
-    public StageDto createStage(StageDto stageDto) {
+    public StageDto createStage(@RequestBody StageDto stageDto) {
         StageValidator.validateCreation(stageDto);
-
-        return stageDto;
+        return stageService.createStage(stageDto);
     }
 }
--- src/main/java/faang/school/projectservice/service/StageService.java
+++ src/main/java/faang/school/projectservice/service/StageService.java
@@ -1,4 +1,44 @@
 package faang.school.projectservice.service;
 
+import faang.school.projectservice.dto.stage.StageDto;
+import faang.school.projectservice.jpa.StageRolesRepository;
+import faang.school.projectservice.mapper.StageMapper;
+import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.stage.Stage;
+import faang.school.projectservice.model.stage.StageRoles;
+import faang.school.projectservice.repository.ProjectRepository;
+import faang.school.projectservice.repository.StageRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class StageService {
+
+    private final StageRepository stageRepository;
+    private final ProjectRepository projectRepository;
+    private final StageMapper stageMapper;
+    private final StageRolesService stageRolesService;
+
+    @Transactional
+    public StageDto createStage(StageDto stageDto) {
+        Project project = projectRepository.getProjectById(stageDto.projectId());
+        if (project == null) {
+            throw new IllegalArgumentException("Project not found by id: " + stageDto.projectId());
+        }
+        Stage stage = stageMapper.toEntity(stageDto);
+
+        stage.setProject(project);
+
+        Stage savedStage = stageRepository.save(stage);
+        List<StageRoles> createdStageRoles = stageRolesService.createStageRolesForStageById(
+                savedStage.getStageId(), stageDto.rolesWithAmount());
+
+        savedStage.setStageRoles(createdStageRoles);
+
+        return stageMapper.toDto(savedStage);
+    }
 }
