80cd78183f923c1f0e9c899f5bba6538fee57f52
Denis Artamonov <dns.artamonov@gmail.com>
2024-09-15 20:53:21 +0300

    BJS2-27209 removed unnecessary annotation

--- src/main/java/faang/school/projectservice/service/subproject/filters/SubProjectFilter.java
+++ src/main/java/faang/school/projectservice/service/subproject/filters/SubProjectFilter.java
@@ -2,11 +2,9 @@ package faang.school.projectservice.service.subproject.filters;
 
 import faang.school.projectservice.dto.client.subproject.ProjectDto;
 import faang.school.projectservice.model.Project;
-import org.springframework.stereotype.Component;
 
 import java.util.stream.Stream;
 
-@Component
 public interface SubProjectFilter {
     boolean isApplicable(ProjectDto projectDto);
 
