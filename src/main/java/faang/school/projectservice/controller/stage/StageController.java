cebdbcb2b303b2c8f397b0c7ea785ecbb1c19db2
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-09-16 21:35:27 +0300

    BJS2-26646 little fix of controller's request mapping

--- src/main/java/faang/school/projectservice/controller/stage/StageController.java
+++ src/main/java/faang/school/projectservice/controller/stage/StageController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/stages")
+@RequestMapping("/stages")
 public class StageController {
     private final StageService stageService;
 
