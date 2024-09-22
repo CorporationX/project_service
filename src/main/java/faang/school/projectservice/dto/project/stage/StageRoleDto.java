78a8715f561d949e5496b9476c32f429ba2ce20a
SharkB38 <val.wolf.ne@gmail.com>
2024-09-22 20:42:04 +0900

    fixes

--- src/main/java/faang/school/projectservice/dto/project/stage/StageRoleDto.java
+++ src/main/java/faang/school/projectservice/dto/project/stage/StageRoleDto.java
@@ -7,13 +7,10 @@ import lombok.Builder;
 
 @Builder
 public record StageRoleDto(
-        Long id,
 
         @NotNull(message = "role name is required")
         TeamRole teamRole,
 
         @NotNull(message = "count for member role is required")
         @Positive(message = "count for member role must be positive")
-        Integer count,
-
-        Long stageId) {}
+        Integer count) {}
