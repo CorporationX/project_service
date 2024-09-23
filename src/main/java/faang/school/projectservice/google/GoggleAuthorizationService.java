9368ffb5a8f0f9555a6f015a729cb4f933ea664d
ifef228 <ifef228@gmail.com>
2024-09-23 19:37:08 +0300

    сделал рефакторинг, добавил пару скоупов для календаря

--- src/main/java/faang/school/projectservice/google/GoggleAuthorizationService.java
+++ src/main/java/faang/school/projectservice/google/GoggleAuthorizationService.java
@@ -1,8 +1,69 @@
 package faang.school.projectservice.google;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service
-public class GoggleAuthorizationService {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.GeneralSecurityException;
+import java.util.List;
 
+@Service
+@Slf4j
+public class GoggleAuthorizationService {
+    @Value("${google.google_calendar.credentials_file_path}")
+    private String credentialsFilePath;
+
+    @Value("${google.google_calendar.scopes}")
+    private List<String> scopes;
+
+    private final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    private final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+    public Calendar authorizeAndGetCalendar() throws IOException, GeneralSecurityException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        return new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName("name")
+                .build();
+    }
+
+
+    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        InputStream in = Files.newInputStream(Path.of(credentialsFilePath));
+        if (in == null) {
+            log.error("Resource not found: " + credentialsFilePath);
+            throw new FileNotFoundException("Resource not found: " + credentialsFilePath);
+        }
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, scopes)
+                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888)
+                .setHost("localhost").build();
+
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
 }
