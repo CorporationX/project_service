231304f49b30671b0e303c845e1b8b6abc6b6d34
Denis Artamonov <dns.artamonov@gmail.com>
2024-09-25 18:46:55 +0300

    BJS2-27242 bug fixes

--- src/main/java/faang/school/projectservice/exception/GlobalExceptionHandler.java
+++ src/main/java/faang/school/projectservice/exception/GlobalExceptionHandler.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import software.amazon.ion.NullValueException;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,50 +23,50 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NullValueException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ErrorResponse handleNullValueException(NullValueException e) {
-        log.error("Data validation exception occurred: {}", e.getMessage());
-        return new ErrorResponse(e.getMessage(), LocalDateTime.now());
+        log.error("Data validation exception occurred: ", e);
+        return new ErrorResponse("Data validation exception occurred", e.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(NotFoundException e) {
-        log.error("Not found exception occurred: {}", e.getErrorMessage());
-        return new ErrorResponse(e.getErrorMessage(), LocalDateTime.now());
+        log.error("Not found exception occurred: ", e);
+        return new ErrorResponse("Not found exception occurred", e.getErrorMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
-        log.error("Illegal argument exception occurred: {}", e.getMessage());
-        return new ErrorResponse(e.getMessage(), LocalDateTime.now());
+        log.error("Illegal argument exception occurred", e);
+        return new ErrorResponse("Illegal argument exception occurred", e.getMessage());
     }
 
     @ExceptionHandler(AlreadyExistsException.class)
     @ResponseStatus(value = HttpStatus.CONFLICT)
     public ErrorResponse handleAlreadyExistsException(AlreadyExistsException e) {
-        log.error("Already exist exception occurred: {}", e.getErrorMessage());
-        return new ErrorResponse(e.getErrorMessage(), LocalDateTime.now());
+        log.error("Already exist exception occurred", e);
+        return new ErrorResponse("Already exist exception occurred", e.getErrorMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(value = HttpStatus.CONFLICT)
     public ErrorResponse handleAlreadyExistsException(IllegalStateException e) {
-        log.error("Illegal state exception occurred: {}", e.getMessage());
-        return new ErrorResponse(e.getMessage(), LocalDateTime.now());
+        log.error("Illegal state exception occurred", e);
+        return new ErrorResponse("Illegal state exception occurred", e.getMessage());
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorResponse handleAlreadyExistsException(EntityNotFoundException e) {
-        log.error("Entity not found exception occurred: {}", e.getMessage());
-        return new ErrorResponse(e.getMessage(), LocalDateTime.now());
+        log.error("Entity not found exception occurred", e);
+        return new ErrorResponse("Entity not found exception occurred", e.getMessage());
     }
 
     @ExceptionHandler(ServletException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorResponse handleAlreadyExistsException(ServletException e) {
-        log.error("Servlet exception occurred: {}", e.getMessage());
-        return new ErrorResponse(e.getMessage(), LocalDateTime.now());
+        log.error("Servlet exception occurred: ", e);
+        return new ErrorResponse("Servlet exception occurred", e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -79,6 +78,13 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e) {
+        log.error("Servlet exception occurred: ", e);
+        return new ErrorResponse("Servlet exception occurred", e.getMessage());
     }
 }
 
