fb7a70083eacc5a192bf284989ec228610489102
Sergey Sklyar <sklyar1091@gmail.com>
2024-09-26 00:13:15 +0300

    Deleted unused imports

--- src/main/java/faang/school/projectservice/service/internship/InternshipService.java
+++ src/main/java/faang/school/projectservice/service/internship/InternshipService.java
@@ -24,7 +24,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
