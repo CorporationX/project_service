b5cac6bcd3f9195a082db1c271b8c7399cfb00f7
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-27 22:38:16 +0300

    Исключение EntityFieldNotFoundException

---
+++ src/main/java/faang/school/projectservice/exception/EntityFieldNotFoundException.java
@@ -0,0 +1,8 @@
+package faang.school.projectservice.exception;
+
+public class EntityFieldNotFoundException extends RuntimeException {
+    public EntityFieldNotFoundException(String message) {
+        super(message);
+    }
+}
+
