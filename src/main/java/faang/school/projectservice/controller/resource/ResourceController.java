07bf91383af1152d1968207a142520125a9ba3e1
Denis Artamonov <dns.artamonov@gmail.com>
2024-09-28 07:18:47 +0300

    BJS2-27247

--- src/main/java/faang/school/projectservice/controller/resource/ResourceController.java
+++ src/main/java/faang/school/projectservice/controller/resource/ResourceController.java
@@ -45,8 +45,6 @@ public class ResourceController {
                     schema = @Schema(type = "string", format = "binary")
             ))
             @RequestPart("file") MultipartFile file) {
-        System.out.println("Before execute");
-
         ResourceDto dto = resourceService.addResource(projectId, file);
         return ResponseEntity.ok(dto);
     }
