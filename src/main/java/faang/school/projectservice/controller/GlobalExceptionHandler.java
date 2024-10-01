ca1cc0ba3597f8237cba8bbdce030a5898195ccc
Dmitriy Varankin <dima.varankin2012@yandex.ru>
2024-10-01 14:57:29 +0300

    v2 task: Обработчик исключений в project_servic (feature-BJS2-26113)

--- src/main/java/faang/school/projectservice/controller/GlobalExceptionHandler.java
+++ src/main/java/faang/school/projectservice/controller/GlobalExceptionHandler.java
@@ -2,126 +2,98 @@ package faang.school.projectservice.controller;
 
 import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.exception.ErrorResponse;
+import faang.school.projectservice.exception.ValidationErrorResponse;
+import faang.school.projectservice.exception.Violation;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(DataValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handlerDataValidationException(DataValidationException e, HttpServletRequest request) {
-        log.error("Data validation exception occurred: {}", e.getMessage());
-        return ErrorResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .url(request.getRequestURI())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .message(e.getMessage())
-                .build();
+        log.error("Data validation exception occurred: {}", e.getMessage(), e);
+        return returnedErrorResponse(e, request);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handlerIllegalArgumentException(IllegalArgumentException e, HttpServletRequest request) {
-        log.error("IllegalArgumentException occurred: {}", e.getMessage());
-        return ErrorResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .url(request.getRequestURI())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .message(e.getMessage())
-                .build();
+        log.error("IllegalArgumentException occurred: {}", e.getMessage(), e);
+        return returnedErrorResponse(e, request);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handlerEntityNotFoundException(EntityNotFoundException e, HttpServletRequest request) {
-        log.error("EntityNotFoundException occurred: {}", e.getMessage());
-        return ErrorResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .url(request.getRequestURI())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .message(e.getMessage())
-                .build();
+        log.error("EntityNotFoundException occurred: {}", e.getMessage(), e);
+        return returnedErrorResponse(e, request);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handlerConstraintViolationException(ConstraintViolationException e, HttpServletRequest request) {
-        log.error("ConstraintViolationException occurred: {}", e.getMessage());
-        Map<String, String> errors = e.getConstraintViolations().stream()
-                .collect(Collectors.toMap(
-                        violation -> violation.getPropertyPath().toString(),
-                        ConstraintViolation::getMessage
-                ));
+    public ValidationErrorResponse handlerConstraintViolationException(ConstraintViolationException e, HttpServletRequest request) {
+        log.error("ConstraintViolationException occurred: {}", e.getMessage(), e);
+        List<Violation> violations = e.getConstraintViolations().stream()
+                .map(violation -> new Violation(violation.getPropertyPath().toString(), violation.getMessage()))
+                .toList();
 
-        return ErrorResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .url(request.getRequestURI())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .errorInFields(errors)
-                .build();
+        return returnedValidationErrorResponse(e, request, violations);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handlerMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
-        log.error("MethodArgumentNotValidException occurred: {}", e.getMessage());
-        Map<String, String> errors = e.getBindingResult().getAllErrors().stream()
-                .collect(Collectors.toMap(
-                        error -> ((FieldError) error).getField(),
-                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), "")
-                ));
+    public ValidationErrorResponse handlerMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        log.error("MethodArgumentNotValidException occurred: {}", e.getMessage(), e);
+        List<Violation> violations = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
+                .toList();
 
-        return ErrorResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .url(request.getRequestURI())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .errorInFields(errors)
-                .build();
+        return returnedValidationErrorResponse(e, request, violations);
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handlerRuntimeException(RuntimeException e, HttpServletRequest request) {
-        log.error("RuntimeException occurred: {}", e.getMessage());
-        return ErrorResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .url(request.getRequestURI())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .message(e.getMessage())
-                .build();
+        log.error("RuntimeException occurred: {}", e.getMessage(), e);
+        return returnedErrorResponse(e, request);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handlerException(Exception e, HttpServletRequest request) {
-        log.error("Exception occurred: {}", e.getMessage());
+        log.error("Exception occurred: {}", e.getMessage(), e);
+        return returnedErrorResponse(e, request);
+    }
+
+    private ErrorResponse returnedErrorResponse(Exception exception, HttpServletRequest request) {
         return ErrorResponse.builder()
                 .timestamp(LocalDateTime.now())
                 .url(request.getRequestURI())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .message(e.getMessage())
+                .message(exception.getMessage())
                 .build();
     }
+
+    private ValidationErrorResponse returnedValidationErrorResponse(Exception exception,
+                                                                    HttpServletRequest request, List<Violation> violations) {
+        return ValidationErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .url(request.getRequestURI())
+                .message(exception.getMessage())
+                .violations(violations)
+                .build();
+    }
+
 }
--- src/main/java/faang/school/projectservice/exception/ValidationErrorResponse.java
+++ src/main/java/faang/school/projectservice/exception/ValidationErrorResponse.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
-public class ErrorResponse {
+public class ValidationErrorResponse {
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 
     private String url;
     private String message;
+    private final List<Violation> violations;
+
 }
