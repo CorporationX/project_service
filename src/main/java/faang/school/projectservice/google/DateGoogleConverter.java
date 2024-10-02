72a031a93fc300089e6dc51790d35ec7ceab8779
ifef228 <ifef228@gmail.com>
2024-10-02 15:20:50 +0300

    переделал конвертер даты

--- src/main/java/faang/school/projectservice/google/DateGoogleConverter.java
+++ src/main/java/faang/school/projectservice/google/DateGoogleConverter.java
@@ -4,20 +4,20 @@ import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.EventDateTime;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.Calendar;
 
 public class DateGoogleConverter {
+    private DateGoogleConverter() {
+
+    }
+
     public static EventDateTime toEventDateTime(LocalDateTime localDateTime) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(localDateTime.getYear(), localDateTime.getMonthValue() - 1,
+                localDateTime.getDayOfMonth(), localDateTime.getHour(), localDateTime.getMinute(),
+                localDateTime.getSecond());
         return new EventDateTime()
-                .setDateTime(new DateTime(
-                        new Date(
-                                localDateTime.getYear() - 1900,
-                                localDateTime.getMonth().getValue() - 1,
-                                localDateTime.getDayOfMonth(),
-                                localDateTime.getHour(),
-                                localDateTime.getMinute()
-                        )
-                ));
+                .setDateTime(new DateTime(calendar.getTime()));
     }
 
 }
--- src/test/java/faang/school/projectservice/google/DateGoogleConverterTest.java
+++ src/test/java/faang/school/projectservice/google/DateGoogleConverterTest.java
@@ -2,29 +2,49 @@ package faang.school.projectservice.google;
 
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.EventDateTime;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Calendar;
 import java.util.Date;
 
 class DateGoogleConverterTest {
-    private LocalDateTime testTime1 = LocalDateTime.of(2005, Month.AUGUST, 15, 17, 30);
-    private LocalDateTime testTime2 = LocalDateTime.of(2020, Month.SEPTEMBER, 1, 13, 0);
-    private LocalDateTime testTime3 = LocalDateTime.of(2000, Month.DECEMBER, 31, 23, 59);
+    private LocalDateTime testTime1 = LocalDateTime.of(2005, Month.AUGUST, 15, 17, 30, 10);
+    private LocalDateTime testTime2 = LocalDateTime.of(2020, Month.SEPTEMBER, 1, 13, 0, 10);
+    private LocalDateTime testTime3 = LocalDateTime.of(2000, Month.DECEMBER, 31, 23, 59, 10);
+    private EventDateTime expectedTime1 = new EventDateTime();
+    private EventDateTime expectedTime2 = new EventDateTime();
+    private EventDateTime expectedTime3 = new EventDateTime();
+    @BeforeEach
+    public void init() {
+        Calendar expected1 = Calendar.getInstance();
+        expected1.set(2005, Calendar.AUGUST, 15, 17, 30, 10);
+        Calendar expected2 = Calendar.getInstance();
+        expected2.set(2020, Calendar.SEPTEMBER, 1, 13, 0, 10);
+        Calendar expected3 = Calendar.getInstance();
+        expected3.set(2000, Calendar.DECEMBER, 31, 23, 59, 10);
 
-    private EventDateTime expectedTime1 = new EventDateTime()
-            .setDateTime(new DateTime(new Date(105, Calendar.AUGUST, 15, 17, 30)));
-    private EventDateTime expectedTime2 = new EventDateTime()
-            .setDateTime(new DateTime(new Date(120, Calendar.SEPTEMBER, 1, 13, 0)));
-    private EventDateTime expectedTime3 = new EventDateTime()
-            .setDateTime(new DateTime(new Date(100, Calendar.DECEMBER, 31, 23, 59)));
+        expectedTime1.setDateTime(new DateTime(expected1.getTime()));
+        expectedTime2.setDateTime(new DateTime(expected2.getTime()));
+        expectedTime3.setDateTime(new DateTime(expected3.getTime()));
+    }
     @Test
     void toEventDateTimeTest() {
-        Assertions.assertEquals(expectedTime1, DateGoogleConverter.toEventDateTime(testTime1));
-        Assertions.assertEquals(expectedTime2, DateGoogleConverter.toEventDateTime(testTime2));
-        Assertions.assertEquals(expectedTime3, DateGoogleConverter.toEventDateTime(testTime3));
+        Assertions.assertTrue(isSameDay(expectedTime1.getDateTime(),
+                DateGoogleConverter.toEventDateTime(testTime1).getDateTime()));
+        Assertions.assertTrue(isSameDay(expectedTime2.getDateTime(),
+                DateGoogleConverter.toEventDateTime(testTime2).getDateTime()));
+        Assertions.assertTrue(isSameDay(expectedTime2.getDateTime(),
+                DateGoogleConverter.toEventDateTime(testTime2).getDateTime()));
+    }
+
+    public static boolean isSameDay(DateTime date1, DateTime date2) {
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd");
+        return fmt.format(new Date(date1.getValue())).equals(fmt.format(new Date(date2.getValue())));
     }
 }
