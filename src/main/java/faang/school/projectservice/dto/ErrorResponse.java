98c46a1d19610d7c7f428e526e1b2cb5620570cc
VladislavVoronov <167184073+VladislavVoronov@users.noreply.github.com>
2024-10-06 18:36:26 +0300

    Revert "Глобальный обработчик ошибок"

--- src/main/java/faang/school/projectservice/dto/ErrorResponse.java
+++ src/main/java/faang/school/projectservice/dto/ErrorResponse.java
@@ -1,13 +1,11 @@
 package faang.school.projectservice.dto;
 
-
-import lombok.Builder;
-import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
 @Data
@@ -16,9 +14,7 @@ import java.util.Map;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
-    private String serviceName;
     private Integer status;
     private String globalMessage;
     private Map<String, String> errors;
-
 }
