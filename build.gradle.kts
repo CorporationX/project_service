be40ad00e1e37c61c83d49a45bda4bbaafa0a9e5
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-10-13 21:34:18 +0300

    BJS2-26722 redis + objectMapper config

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
     /**
      * Database
