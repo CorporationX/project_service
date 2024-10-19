f704dc408c5aa20225a114d87266694d0f8d40bc
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-10-19 20:26:57 +0700

    доработка

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.retry:spring-retry")
-    implementation("org.springframework.spring-aspects")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
