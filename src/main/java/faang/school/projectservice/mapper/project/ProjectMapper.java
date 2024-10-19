bc48f76220e27cc1fe275fbccfa52c1eadb4779c
pgusev <pgusev@fil-it.ru>
2024-10-20 00:13:50 +0300

    BJS2-26158 - CR fixes

--- src/main/java/faang/school/projectservice/mapper/project/ProjectMapper.java
+++ src/main/java/faang/school/projectservice/mapper/project/ProjectMapper.java
@@ -1,10 +1,13 @@
 package faang.school.projectservice.mapper.project;
 
 import faang.school.projectservice.dto.project.ProjectDto;
+import faang.school.projectservice.dto.project.ProjectViewEvent;
 import faang.school.projectservice.model.Project;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -15,4 +18,9 @@ public interface ProjectMapper {
     Project toEntity(ProjectDto projectDto);
 
     List<ProjectDto> toDtos(List<Project> projects);
+
+    @Mapping(source = "userId", target = "userId")
+    @Mapping(source = "project.id", target = "projectId")
+    @Mapping(target = "viewTime", expression = "java(java.time.LocalDateTime.now())")
+    ProjectViewEvent toProjectViewEventDto(Project project, long userId);
 }
