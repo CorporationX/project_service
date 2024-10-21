bfb5cff370a6932c040940c8c2a36b53f4e3e745
Сураган Оразбаев <s.orazbayev@enbek.kz>
2024-10-21 10:52:22 +0500

    убрал валидаций когда возвращаем так как они не нужны

--- src/main/java/faang/school/projectservice/dto/internship/InternshipDto.java
+++ src/main/java/faang/school/projectservice/dto/internship/InternshipDto.java
@@ -10,31 +10,12 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public record InternshipDto(
-        @NotNull(message = "ID is required")
-        @Positive(message = "ID must be positive")
         Long id,
-
-        @NotBlank(message = "Name cannot be blank")
-        @Size(max = 128, message = "Name cannot be longer than 128 characters")
         String name,
-
-        @NotBlank(message = "Description cannot be blank")
-        @Size(max = 500, message = "Description cannot be longer than 500 characters")
         String description,
-
-        @NotNull(message = "Mentor ID is required")
-        @Positive(message = "Mentor ID must be positive")
         Long mentorId,
-
-        @NotNull(message = "Project ID is required")
-        @Positive(message = "Project ID must be positive")
         Long projectId,
-
-        @NotNull(message = "Status is required")
         InternshipStatus status,
-
-        @NotNull(message = "End date is required")
-        @Future(message = "End date must be in the future")
         LocalDateTime endDate
 ) {
 }
--- src/main/java/faang/school/projectservice/dto/project/resource/ResourceDto.java
+++ src/main/java/faang/school/projectservice/dto/project/resource/ResourceDto.java
@@ -9,23 +9,10 @@ import jakarta.validation.constraints.Size;
 import java.math.BigInteger;
 
 public record ResourceDto(
-        @NotNull(message = "Resource ID must not be null")
-        @Positive(message = "Resource ID must be positive")
         Long id,
-
-        @NotBlank( message = "Resource name must not be blank")
-        @Size(max = 255, message = "Resource name must not exceed 255 characters")
         String name,
-
-        @NotBlank(message = "Resource key must not be blank")
-        @Size(max = 255, message = "Resource key must not exceed 255 characters")
         String key,
-
-        @NotNull(message = "Resource size must not be null")
-        @Positive(message = "Resource size must be positive")
         BigInteger size,
-
-        @NotNull(message = "Resource type must not be null")
         ResourceType type
 ) {
 }
