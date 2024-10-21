4ea3d7da8f871ec409d284b91a7e598fa899d2ce
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-10-21 20:30:42 +0300

    BJS2-26693 done controller test

--- src/main/java/faang/school/projectservice/controller/donation/DonationController.java
+++ src/main/java/faang/school/projectservice/controller/donation/DonationController.java
@@ -3,8 +3,10 @@ package faang.school.projectservice.controller.donation;
 import faang.school.projectservice.model.dto.donation.DonationDto;
 import faang.school.projectservice.model.dto.donation.DonationFilterDto;
 import faang.school.projectservice.service.DonationService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,11 +19,12 @@ public class DonationController {
     private final DonationService donationService;
 
     @PostMapping
-    public DonationDto sendDonation(DonationDto donationDto) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public DonationDto sendDonation(@Valid @RequestBody DonationDto donationDto) {
         return donationService.sendDonation(donationDto);
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/users/{id}")
     public DonationDto getDonationById(@PathVariable @Positive long id) {
         return donationService.getDonationById(id);
     }
