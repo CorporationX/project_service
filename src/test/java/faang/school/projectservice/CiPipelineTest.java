d1ea30f043b8dcac5c3b694ce18b8b97c720584b
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-11-07 07:55:01 -0800

    feat: test CI pipeline

---
+++ src/test/java/faang/school/projectservice/CiPipelineTest.java
@@ -0,0 +1,13 @@
+package faang.school.projectservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class CiPipelineTest {
+
+    @Test
+    void testCiPipeline() {
+        fail();
+    }
+}
