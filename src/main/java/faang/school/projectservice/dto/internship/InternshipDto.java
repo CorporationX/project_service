d082cc25cf84237458b2568e59d0971dbd5cec15
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2024-11-21 20:43:53 +0500

    добавил новую аннотацию

--- src/main/java/faang/school/projectservice/dto/internship/InternshipDto.java
+++ src/main/java/faang/school/projectservice/dto/internship/InternshipDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import faang.school.projectservice.model.InternshipStatus;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,15 +19,20 @@ import java.util.List;
 @Builder
 public class InternshipDto {
     @NotNull
+    @PositiveOrZero
     private Long id;
+
     @NotNull
+    @PositiveOrZero
     private Long projectId;
+
     @NotNull
+    @PositiveOrZero
     private Long mentorId;
+
     @NotNull
     @NotEmpty
     private List<Long> internsIds;
-
 
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
