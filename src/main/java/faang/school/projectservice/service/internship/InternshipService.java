3f04239832cd0510483bedeabcb5f160f4a55991
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2024-11-21 21:34:37 +0500

    правки сервиса

--- src/main/java/faang/school/projectservice/service/internship/InternshipService.java
+++ src/main/java/faang/school/projectservice/service/internship/InternshipService.java
@@ -96,7 +96,6 @@ public class InternshipService {
         internshipRepository.save(internshipEntity);
     }
 
-    //3 Получить все стажировки проекта с фильтрами по статусу или роли.
     public List<InternshipDto> getAllInternshipByStatusAndRole(Long projectId, InternshipFilterDto filters) {
         List<Internship> allInternship = internshipRepository.findAll();
         return allInternship.stream()
@@ -108,13 +107,11 @@ public class InternshipService {
                 .map(internshipMapper::toDto).toList();
     }
 
-    //4 Получить все стажировки.
     public List<InternshipDto> getAllInternship() {
         List<Internship> internships = internshipRepository.findAll();
         return internshipMapper.toListDto(internships);
     }
 
-    //5 Получить стажировку по id.
     public InternshipDto getInternshipById(Long id) {
         Internship internships = internshipRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Интернатура с ID " + id + " не найдена"));
