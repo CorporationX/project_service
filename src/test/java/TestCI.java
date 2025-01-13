8c41d8e31f21f6eec80c7cba9ed319a5b717da65
Timka-cloud1 <amanzholtimka@gmail.com>
2025-01-13 14:25:36 +0500

    Добавил CI для запуска тестов на Pull Request

---
+++ src/test/java/TestCI.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCI {
+    @Test
+    public void testCI() {
+        Assertions.assertTrue(true);
+
+    }
+}
