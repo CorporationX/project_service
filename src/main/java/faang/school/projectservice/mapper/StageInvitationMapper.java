98540fe727cb70e50cb080a53a970eac478f4a74
Pavel Schelkin <rhanan1233@gmail.com>
2025-01-23 20:02:22 +0300

    Приглашения присоединиться к этапу.

--- src/main/java/faang/school/projectservice/mapper/StageInvitationMapper.java
+++ src/main/java/faang/school/projectservice/mapper/StageInvitationMapper.java
@@ -2,12 +2,11 @@ package faang.school.projectservice.mapper;
 
 import faang.school.projectservice.dto.client.StageInvitationDto;
 import faang.school.projectservice.model.stage_invitation.StageInvitation;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface StageInvitationMapper {
 
     @Mapping(source = "author.id", target = "authorId")
@@ -15,9 +14,7 @@ public interface StageInvitationMapper {
     @Mapping(source = "stage.stageId", target = "stageId")
     StageInvitationDto toDto(StageInvitation stageInvitation);
 
-    @Mapping(source = "authorId", target = "author", qualifiedByName = "teamMemberId to teamMember")
-    @Mapping(source = "invitedId", target = "invited", qualifiedByName = "teamMemberId to teamMember")
-    @Mapping(source = "stageId", target = "stage", qualifiedByName = "stageId to stage entity")
+
     StageInvitation toEntity(StageInvitationDto stageInvitationDto);
 
 
