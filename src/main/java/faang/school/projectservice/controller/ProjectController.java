bdd1873c8cf52f47d51fe99bf5b3a14678e8061b
Eugene <@evgeniy_kim>
2025-01-27 09:41:38 +0500

    BJS2-52827 | Система постов

--- src/main/java/faang/school/projectservice/controller/ProjectController.java
+++ src/main/java/faang/school/projectservice/controller/ProjectController.java
@@ -2,7 +2,7 @@ package faang.school.projectservice.controller;
 
 import faang.school.projectservice.dto.project.ProjectDto;
 import faang.school.projectservice.mapper.ProjectMapper;
-import faang.school.projectservice.repository.ProjectRepositoryAdapter;
+import faang.school.projectservice.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/projects")
 public class ProjectController {
-    private final ProjectRepositoryAdapter projectRepositoryAdapter;
+    private final ProjectService projectService;
     private final ProjectMapper projectMapper;
 
     private static final String PROJECT_ID_PATH = "/{projectId}";
 
     @GetMapping(PROJECT_ID_PATH)
     public ProjectDto getProject(@PathVariable long projectId) {
-        return projectMapper.toDto(projectRepositoryAdapter.getProjectById(projectId));
+        return projectMapper.toDto(projectService.getProjectById(projectId));
     }
 
 }
---
+++ src/main/java/faang/school/projectservice/service/ProjectService.java
@@ -0,0 +1,7 @@
+package faang.school.projectservice.service;
+
+import faang.school.projectservice.model.Project;
+
+public interface ProjectService {
+    Project getProjectById(long projectId);
+}
