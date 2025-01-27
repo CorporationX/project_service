7e576071bd9dbf97d877b70f77d4c612dac21f94
oxana <nov2303@mail.ru>
2025-01-27 21:18:19 +0500

    BJS2-52750 | Стажировка

--- src/main/java/faang/school/projectservice/dto/internship/InternshipUserInformationDto.java
+++ src/main/java/faang/school/projectservice/dto/internship/InternshipUserInformationDto.java
@@ -1,8 +1,12 @@
 package faang.school.projectservice.dto.internship;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class InternshipUserInformationDto {
     private Long id;
     private Long userId;
--- src/test/java/faang/school/projectservice/service/internship/InternshipServiceImplTest.java
+++ src/test/java/faang/school/projectservice/service/internship/InternshipServiceImplTest.java
@@ -1,12 +1,14 @@
 package faang.school.projectservice.service.internship;
 
-import faang.school.projectservice.adapter.InternshipRepositoryAdapter;
+import faang.school.projectservice.adapter.*;
+import faang.school.projectservice.dto.internship.InternshipDto;
 import faang.school.projectservice.dto.internship.InternshipFilterDto;
+import faang.school.projectservice.dto.internship.InternshipUserInformationDto;
 import faang.school.projectservice.filter.internship.InternshipFilter;
 import faang.school.projectservice.filter.internship.InternshipStatusFilter;
 import faang.school.projectservice.mapper.internship.InternshipMapper;
-import faang.school.projectservice.model.Internship;
-import faang.school.projectservice.model.InternshipStatus;
+import faang.school.projectservice.model.*;
+import faang.school.projectservice.validator.internship.InternshipServiceValidator;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,6 +26,8 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class InternshipServiceImplTest {
+    @InjectMocks
+    InternshipServiceImpl internshipService;
 
     @Mock
     private InternshipMapper internshipMapper;
@@ -31,19 +35,94 @@ public class InternshipServiceImplTest {
     private InternshipRepositoryAdapter internshipRepositoryAdapter;
     @Mock
     private List<InternshipFilter> internshipFilters;
+    @Mock
+    private ProjectRepositoryAdapter projectRepositoryAdapter;
+    @Mock
+    private ScheduleRepositoryAdapter scheduleRepositoryAdapter;
+    @Mock
+    private InternshipServiceValidator internshipServiceValidator;
+    @Mock
+    private TeamRepositoryAdapter teamRepositoryAdapter;
+    @Mock
+    private TeamMemberRepositoryAdapter teamMemberRepositoryAdapter;
 
     private static final Integer NUMBER_INVOCATION = 1;
     private static final Long INTERNSHIP_ID = 1L;
-
-    @InjectMocks
-    InternshipServiceImpl internshipService;
+    private static final Long PROJECT_ID = 1L;
+    private static final Long MENTOR_ID = 1L;
+    private static final Long SCHEDULE_ID = 1L;
+    private static final Long INTERN_ID = 1L;
+    private static final String INTERNSHIP_DESCRIPTION = "Java Block 1";
+    private static final String INTERNSHIP_NAME = "Java";
 
     @Test
-    public void testUpdateInternship(){
+    public void testСreateInternship() {
+        InternshipDto internshipDto = new InternshipDto();
+        internshipDto.setProjectId(PROJECT_ID);
+        internshipDto.setMentorId(MENTOR_ID);
+        internshipDto.setDescription(INTERNSHIP_DESCRIPTION);
+        internshipDto.setName(INTERNSHIP_NAME);
+        internshipDto.setStatus(InternshipStatus.IN_PROGRESS);
+        internshipDto.setScheduleId(SCHEDULE_ID);
+        List<InternshipUserInformationDto> interns = new ArrayList<>();
+        List<TeamMember> teamMembers = new ArrayList<>();
+        List<TeamRole> teamRoles = new ArrayList<>();
+        Project project = new Project();
+        project.setId(internshipDto.getProjectId());
+        when(projectRepositoryAdapter.findById(internshipDto.getProjectId())).thenReturn(project);
+
+        Team team = new Team();
+        team.setProject(project);
+        team.setTeamMembers(teamMembers);
+        when(teamRepositoryAdapter.save(team)).thenReturn(team);
+        teamRoles.add(TeamRole.INTERN);
+
+        InternshipUserInformationDto internshipUserInformationDto = new InternshipUserInformationDto();
+        internshipUserInformationDto.setUserId(INTERN_ID);
+        interns.add(internshipUserInformationDto);
+        internshipDto.setInterns(interns);
+        TeamMember teamMember = new TeamMember();
+        teamMember.setUserId(INTERN_ID);
+        teamMember.setTeam(team);
+        teamMember.setRoles(teamRoles);
+//        teamMember.setStages(new ArrayList<>());
+        when(teamMemberRepositoryAdapter.save(teamMember)).thenReturn(teamMember);
+
+//        internshipDto.setInterns(interns);
+        Schedule schedule = new Schedule();
+//        schedule.setName("Schedule");
+        schedule.setId(SCHEDULE_ID);
+//        schedule.setProject(project);
+        when(scheduleRepositoryAdapter.findById(SCHEDULE_ID)).thenReturn(schedule);
+        Internship internship = new Internship();
+//        internship.setStatus(internshipDto.getStatus());
+//        internship.setInterns(teamMembers);
+//        internship.setProject(project);
+//        internship.setName(internshipDto.getName());
+//        internship.setDescription(internshipDto.getDescription());
+//        internship.setSchedule(schedule);
+//        internship.setStartDate(internshipDto.getStartDate());
+//        internship.setEndDate(internshipDto.getEndDate());
+
+        when(internshipMapper.toEntity(internshipDto)).thenReturn(internship);
+//        when(internship.setProject(project)).thenReturn()
+//
+//        internship.setProject(project);
+//        internship.setSchedule(schedule);
+//        internship.setInterns(interns);
+
+
+        internshipService.createInternship(internshipDto);
+        Mockito.verify(internshipRepositoryAdapter, Mockito.times(NUMBER_INVOCATION)).save(internship);
+    }
+
+    @Test
+    public void testUpdateInternship() {
         //when completed
 //        when new mentor
 //        IsAheadOfSchedule
     }
+
     @Test
     public void testGetInternshipsWithFilters() {
         InternshipFilterDto internshipFilterDto = new InternshipFilterDto();
