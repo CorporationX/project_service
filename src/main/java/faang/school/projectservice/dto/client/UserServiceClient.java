c7adc60a3d1968a83597fd9cc6a89a3768e0d2c4
sergeybabaev96 <166114022+sergeybabaev96@users.noreply.github.com>
2025-01-29 20:46:00 +0300

    Удаление UserServiceClient.java

--- src/main/java/faang/school/projectservice/dto/client/UserServiceClient.java
+++
@@ -1,8 +0,0 @@
-package faang.school.projectservice.dto.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "user_service")
-public interface UserServiceClient {
-
-}
