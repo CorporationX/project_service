c0a7ff41fc4000766ff01845da11ebfb54c565af
OxanaNovozhilova <nov2303@mail.ru>
2025-02-04 10:12:18 +0500

    BJS2-53001 | Добавление обложки вакансии

--- src/main/java/faang/school/projectservice/filter/internship/InternshipProjectFilter.java
+++ src/main/java/faang/school/projectservice/filter/internship/InternshipProjectFilter.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.stream.Stream;
+
 @Component
 public class InternshipProjectFilter implements InternshipFilter {
     @Override
