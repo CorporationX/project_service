9af64f414e935b218576b8e8fbab0c439f70d036
senty <senty1844@gmail.com>
2025-02-04 15:24:56 +0300

    Убраны лишние проверки на null

--- src/main/java/faang/school/projectservice/filter/stage/StageRoleFilter.java
+++ src/main/java/faang/school/projectservice/filter/stage/StageRoleFilter.java
@@ -4,11 +4,8 @@ import faang.school.projectservice.dto.stage.StageFilterDto;
 import faang.school.projectservice.model.TeamRole;
 import faang.school.projectservice.model.stage.Stage;
 import faang.school.projectservice.model.stage.StageRoles;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 @Component
@@ -21,13 +18,9 @@ public class StageRoleFilter implements StageFilter {
     @Override
     public Stream<Stage> apply(Stream<Stage> stages, StageFilterDto filters) {
         TeamRole teamRole = filters.getTeamRole();
-        return stages.filter(stage -> Optional.ofNullable(stage)
-                .map(Stage::getStageRoles)
-                .filter(CollectionUtils::isNotEmpty)
-                .map(stageRoles -> stageRoles.stream()
-                        .filter(Objects::nonNull)
-                        .map(StageRoles::getTeamRole)
-                        .anyMatch(teamRole::equals))
-                .orElse(false));
+        return stages.filter(stage -> stage.getStageRoles()
+                .stream()
+                .map(StageRoles::getTeamRole)
+                .anyMatch(teamRole::equals));
     }
 }
