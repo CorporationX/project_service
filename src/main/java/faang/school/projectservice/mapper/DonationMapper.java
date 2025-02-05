d4a89a22579f8949ec3f3cf73178d48bb2feeba9
Muhammadsolih Abdugafforov <salikhdev@gmail.com>
2025-02-05 22:19:40 +0500

    Donation add testing 1.3v

--- src/main/java/faang/school/projectservice/mapper/DonationMapper.java
+++ src/main/java/faang/school/projectservice/mapper/DonationMapper.java
@@ -3,10 +3,13 @@ package faang.school.projectservice.mapper;
 import faang.school.projectservice.dto.donate.DonationCreateDto;
 import faang.school.projectservice.dto.donate.DonationDto;
 import faang.school.projectservice.model.Donation;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
-@Mapper
+@Mapper(
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface DonationMapper {
 
     @Mapping(target = "campaign.id", source = "campaignId")
--- src/main/java/faang/school/projectservice/util/RandomGenerator.java
+++ src/main/java/faang/school/projectservice/util/RandomGenerator.java
@@ -1,6 +1,5 @@
 package faang.school.projectservice.util;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
@@ -10,7 +9,6 @@ public class RandomGenerator {
 
     private static final Random random = new Random();
 
-    @Bean
     public long getRandomNumber(long min, long max) {
         return random.nextLong(min, max);
     }
