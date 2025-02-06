844076682a401b4259f28b770731b4e24c2336e4
Artem Kopanev <artemkopanev757@gmail.com>
2025-02-06 17:08:46 +0300

    Решил конфликты, теперь точно -_-

--- src/main/java/faang/school/projectservice/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/faang/school/projectservice/exception/handler/GlobalExceptionHandler.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<String> handleDataValidationException(BusinessException e) {
+    public ResponseEntity<String> handleBusinessException(BusinessException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
