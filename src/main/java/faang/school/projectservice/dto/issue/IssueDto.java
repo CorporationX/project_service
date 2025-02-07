0398bbff6eb992744dc8621b1e215e7e8b5b12aa
Aleksandr Kozlov <vismut-888@yandex.ru>
2025-02-07 11:59:49 +0300

    BJS2-54251: Интеграция с Jira

---
+++ src/main/java/faang/school/projectservice/dto/issue/IssueDto.java
@@ -0,0 +1,36 @@
+package faang.school.projectservice.dto.issue;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class IssueDto {
+    @NotEmpty
+    private String key;
+    @Size(max = 1000)
+    private String summary;
+    @Size(max = 10000)
+    private String description;
+    @Positive
+    @Min(1)
+    private Long statusId;
+    private String subtasksKey;
+    @Positive
+    @Min(1)
+    private Long typeId;
+    @NotNull
+    @Future
+    private DateTime endDate;
+}
---
+++ src/main/java/faang/school/projectservice/mapper/IssueMapper.java
@@ -0,0 +1,18 @@
+package faang.school.projectservice.mapper;
+
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import faang.school.projectservice.dto.issue.IssueDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface IssueMapper {
+
+    @Mapping(source = "issueType.id", target = "typeId")
+    @Mapping(source = "status.id", target = "statusId")
+    @Mapping(source = "endDate", target = "dueDate")
+    IssueDto toIssueDto(Issue issue);
+
+    Iterable<IssueDto> toIterableIssueDto(Iterable<Issue> issues);
+}
