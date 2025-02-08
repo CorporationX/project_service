3af70a90063a1300d5b979c619dc6e7508284718
Ilya Vysokolyan <rd2d@list.ru>
2025-02-08 14:40:52 +0300

    BJS2-54267: Поправил контроллер

--- src/main/java/faang/school/projectservice/controller/DonationController.java
+++ src/main/java/faang/school/projectservice/controller/DonationController.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
