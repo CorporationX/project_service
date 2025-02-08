a13a659480f173578e9b1951e77747b0f27aa924
rostex <rostex2@yandex.ru>
2025-02-08 20:10:05 +0300

    Добавлен тест-контейнер для Minio

--- src/test/java/faang/school/projectservice/AppContextTest.java
+++ src/test/java/faang/school/projectservice/AppContextTest.java
@@ -1,25 +1,22 @@
 package faang.school.projectservice;
 
-import faang.school.projectservice.config.s3.S3Config;
-import faang.school.projectservice.service.S3Service;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -36,27 +33,30 @@ public class AppContextTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @MockBean
-    private S3Config s3Config;
-
-    @MockBean
-    private S3Service s3Service;
-
     @Container
     public static PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
 
-    static {
-        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>(DockerImageName.parse("postgres:13.3"));
+    @Container
+    public static MinIOContainer MINIO_CONTAINER;
+
+    static  {
+        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:13.3");
+        MINIO_CONTAINER = new MinIOContainer("minio/minio");
     }
 
     @DynamicPropertySource
-    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+    public static void configureProperties(DynamicPropertyRegistry registry) {
         POSTGRESQL_CONTAINER.start();
         POSTGRESQL_CONTAINER.waitingFor(Wait.forListeningPort());
-
         registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
         registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
+
+        MINIO_CONTAINER.start();
+        MINIO_CONTAINER.waitingFor(Wait.forListeningPort());
+        registry.add("services.s3.endpoint", MINIO_CONTAINER::getS3URL);
+        registry.add("services.s3.accessKey", MINIO_CONTAINER::getUserName);
+        registry.add("services.s3.secretKey", MINIO_CONTAINER::getPassword);
     }
 
     @Test
@@ -66,6 +66,7 @@ public class AppContextTest {
         jdbcTemplate.execute("SELECT 1");
 
         assertTrue(POSTGRESQL_CONTAINER.isRunning());
+        assertTrue(MINIO_CONTAINER.isRunning());
 
         String result = jdbcTemplate.queryForObject("SELECT current_database()", String.class);
         assertNotNull(result);
@@ -74,5 +75,6 @@ public class AppContextTest {
     @AfterAll
     public static void tearDown() {
         POSTGRESQL_CONTAINER.stop();
+        MINIO_CONTAINER.stop();
     }
 }
