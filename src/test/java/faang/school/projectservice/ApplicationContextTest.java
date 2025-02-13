0b8e90babafc3f2f8ab0165c0a7a032be4769339
Dias Kamza <kamzadias31@gmail.com>
2025-02-13 16:27:07 +0500

    BJS2-53826 ApllicationContext

--- src/test/java/faang/school/projectservice/ApplicationContextTest.java
+++ src/test/java/faang/school/projectservice/ApplicationContextTest.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -20,11 +21,24 @@ public class ApplicationContextTest {
             .withUsername("test_user")
             .withPassword("test_password");
 
+    @Container
+    static final MinIOContainer minio = new MinIOContainer("minio/mc:latest")
+            .withUserName("user")
+            .withPassword("password");
+
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
+        // Настройка PostgreSQL
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+
+        // Настройка MinIO. Метод getS3URL() возвращает URL, по которому можно подключиться к сервису S3.
+        registry.add("services.minio.endpoint", minio::getS3URL);
+        registry.add("services.minio.accessKey", minio::getUserName);
+        registry.add("services.minio.secretKey", minio::getPassword);
+        registry.add("services.minio.bucketName", () -> "project-files");
+        registry.add("services.minio.defaultMaxSize", () -> "2147483648");
     }
 
     @Test
