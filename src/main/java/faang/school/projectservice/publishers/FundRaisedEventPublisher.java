00b3904b967a48b290878bde331a9f02eaab0236
KsushaTarasova <ksutaras543@mail.ru>
2025-02-17 12:05:29 +0300

    add publisher

---
+++ src/main/java/faang/school/projectservice/publishers/FundRaisedEventPublisher.java
@@ -0,0 +1,21 @@
+package faang.school.projectservice.publishers;
+
+import faang.school.projectservice.events.FundRaisedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FundRaisedEventPublisher {
+    private final KafkaTemplate<String, FundRaisedEvent> kafkaTemplate;
+    @Value("${kafka.topic.fund-raised-event}")
+    private String topic;
+
+    public void publish(FundRaisedEvent event) {
+        kafkaTemplate.send(topic, event);
+    }
+}
