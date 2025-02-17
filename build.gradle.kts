8406de321d87bef6589bd8e107fd492da8ba8822
KsushaTarasova <ksutaras543@mail.ru>
2025-02-17 11:52:06 +0300

    add dependency for kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,11 @@ dependencies {
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
 
     /**
+     * Kafka
+     */
+    implementation("org.springframework.kafka:spring-kafka")
+
+    /**
      * Test containers
      */
     implementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
@@ -62,6 +67,7 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.mockito:mockito-inline:5.2.0")
+    implementation("org.springframework.kafka:spring-kafka-test")
 }
 
 jacoco {
