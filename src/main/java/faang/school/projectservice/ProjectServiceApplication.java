1abcddd29b02259d683bf1e9d2e682425399f5e8
KsushaTarasova <ksutaras543@mail.ru>
2025-02-17 11:52:47 +0300

    settings for kafka

--- src/main/java/faang/school/projectservice/ProjectServiceApplication.java
+++ src/main/java/faang/school/projectservice/ProjectServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
 @EnableJpaAuditing
+@EnableKafka
 public class ProjectServiceApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ProjectServiceApplication.class)
