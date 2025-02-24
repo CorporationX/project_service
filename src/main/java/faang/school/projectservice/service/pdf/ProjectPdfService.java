db277c1cf7440f48eae160b5b44627af6edf6b52
maksim <maksim_vasilenko@yahoo.com>
2025-02-25 00:14:45 +0300

    BJS2-52924, сreating-PDF-presentation  - conflicts resolved +3

--- src/main/java/faang/school/projectservice/service/pdf/ProjectPdfService.java
+++ src/main/java/faang/school/projectservice/service/pdf/ProjectPdfService.java
@@ -2,10 +2,31 @@ package faang.school.projectservice.service.pdf;
 
 
 import faang.school.projectservice.dto.project.ProjectPresentationDto;
+import faang.school.projectservice.dto.ProjectFilterDto;
+import faang.school.projectservice.dto.ProjectCreateRequestDto;
+import faang.school.projectservice.dto.ProjectResponseDto;
+import faang.school.projectservice.dto.ProjectUpdateRequestDto;
+import faang.school.projectservice.model.Project;
 
 import java.io.InputStream;
+import java.util.List;
 
 
 public interface ProjectPdfService {
+
+    ProjectResponseDto save(ProjectCreateRequestDto projectDto);
+
+    ProjectResponseDto findById(Long id);
+
+    List<ProjectResponseDto> findAll();
+
+    List<ProjectResponseDto> findAllByFilter(ProjectFilterDto filter);
+
+    ProjectResponseDto update(Long id, ProjectUpdateRequestDto projectDto);
+
+    List<Long> getProjectResourceIds(Long projectId);
+
+    Project getProject(Long projectId);
+
     InputStream createProjectPresentation(ProjectPresentationDto dto);
 }
