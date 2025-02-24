46a7ee6171dfa6025de2ba617e59776223ea7314
maksim <maksim_vasilenko@yahoo.com>
2025-02-24 22:47:50 +0300

    BJS2-52924, сreating-PDF-presentation  - conflicts resolved

--- src/main/java/faang/school/projectservice/mapper/ProjectMapper.java
+++ src/main/java/faang/school/projectservice/mapper/ProjectMapper.java
@@ -1,21 +1,36 @@
 package faang.school.projectservice.mapper;
 
+import faang.school.projectservice.dto.ProjectCreateRequestDto;
+import faang.school.projectservice.dto.ProjectResponseDto;
+import faang.school.projectservice.dto.ProjectUpdateRequestDto;
+import faang.school.projectservice.model.Project;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
+
 import faang.school.projectservice.dto.client.UserDto;
 import faang.school.projectservice.dto.project.ProjectPresentationDto;
 import faang.school.projectservice.dto.project.ProjectTeamMemberDto;
-import faang.school.projectservice.model.Project;
 import faang.school.projectservice.model.Task;
 import faang.school.projectservice.model.Team;
-import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
-import org.mapstruct.ReportingPolicy;
 
 import java.util.Collections;
 import java.util.List;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public abstract class ProjectMapper {
+
+    Project toProjectEntity(ProjectCreateRequestDto dto);
+
+    ProjectResponseDto toProjectResponseDto(Project entity);
+
+    List<ProjectResponseDto> toProjectResponseDtos(List<Project> entities);
+
+    void update(ProjectUpdateRequestDto projectUpdateRequestDto, @MappingTarget Project project);
 
     @Mapping(target = "title", source = "project.name")
     @Mapping(target = "createdDate", source = "project.createdAt")
