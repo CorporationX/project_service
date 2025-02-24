2854a49c370abdbba5247c23069f803f1eccee6f
maksim <maksim_vasilenko@yahoo.com>
2025-02-25 00:21:50 +0300

    BJS2-52924, сreating-PDF-presentation  - conflicts resolved +4

--- src/main/java/faang/school/projectservice/service/project/ProjectService.java
+++ src/main/java/faang/school/projectservice/service/project/ProjectService.java
@@ -1,10 +1,31 @@
 package faang.school.projectservice.service.project;
 
+import faang.school.projectservice.dto.ProjectCreateRequestDto;
+import faang.school.projectservice.dto.ProjectFilterDto;
+import faang.school.projectservice.dto.ProjectResponseDto;
+import faang.school.projectservice.dto.ProjectUpdateRequestDto;
 import faang.school.projectservice.dto.resource.S3ObjectDto;
+import faang.school.projectservice.model.Project;
 import org.springframework.core.io.InputStreamResource;
+
+import java.util.List;
 
 
 public interface ProjectService {
+
+    ProjectResponseDto save(ProjectCreateRequestDto projectDto);
+
+    ProjectResponseDto findById(Long id);
+
+    List<ProjectResponseDto> findAll();
+
+    List<ProjectResponseDto> findAllByFilter(ProjectFilterDto filter);
+
+    ProjectResponseDto update(Long id, ProjectUpdateRequestDto projectDto);
+
+    List<Long> getProjectResourceIds(Long projectId);
+
+    Project getProject(Long projectId);
 
     void createPresentation(long projectId);
 
