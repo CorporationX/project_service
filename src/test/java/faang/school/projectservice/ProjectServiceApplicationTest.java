b5ff786d7f48bfbd44dc4882cb297603ad3dac24
lehaps <awda.96@bk.ru>
2025-02-26 21:00:31 +0300

    Добавление контекста

--- src/test/java/faang/school/projectservice/ProjectServiceApplicationTest.java
+++ src/test/java/faang/school/projectservice/ProjectServiceApplicationTest.java
@@ -1,12 +1,21 @@
 package faang.school.projectservice;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {"spring.liquibase.enabled=false"})
 @ActiveProfiles("test")
 class ProjectServiceApplicationTest {
+
+    @MockBean
+    private FeignClient feignClient;
+
+    @MockBean
+    private AmazonS3 amazonS3;
 
     @Test
     void contextLoads() {
