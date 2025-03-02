b4cb02c7513302ee76a307ca4713e4fd4b7a0bd2
KsushaTarasova <ksutaras543@mail.ru>
2025-03-02 17:20:46 +0300

    add builder annotation

--- src/main/java/faang/school/projectservice/dto/donation/DonationDto.java
+++ src/main/java/faang/school/projectservice/dto/donation/DonationDto.java
@@ -1,12 +1,14 @@
 package faang.school.projectservice.dto.donation;
 
 import faang.school.projectservice.dto.client.Currency;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class DonationDto {
     private Long id;
     private Long paymentNumber;
