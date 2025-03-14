4af23e859462ffc1b1ca752522a3549e7a7c97e3
Artem Doronin <doronintema2018@yandex.ru>
2025-03-14 22:34:09 +0500

    Fix bugs

--- src/main/java/faang/school/projectservice/dto/client/internship/InternshipFilterDto.java
+++ src/main/java/faang/school/projectservice/dto/client/internship/InternshipFilterDto.java
@@ -2,11 +2,8 @@ package faang.school.projectservice.dto.client.internship;
 
 import faang.school.projectservice.model.InternshipStatus;
 import faang.school.projectservice.model.TeamRole;
-import lombok.Data;
+import lombok.Builder;
 
-@Data
-public class InternshipFilterDto {
-    private InternshipStatus status;
-    private TeamRole role;
-
+@Builder
+public record InternshipFilterDto(InternshipStatus status,TeamRole role) {
 }
