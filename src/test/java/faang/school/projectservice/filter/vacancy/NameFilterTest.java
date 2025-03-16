8ea0a7af558e6d9b1d54ea38150d925f2a37580e
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-03-16 15:31:12 +0300

    test: Added tests for VacancyController.getFilteredVacancies and VacancyController.getVacancyById

---
+++ src/test/java/faang/school/projectservice/filter/vacancy/NameFilterTest.java
@@ -0,0 +1,99 @@
+package faang.school.projectservice.filter.vacancy;
+
+import faang.school.projectservice.dto.vacancy.VacancyFilterRequestDto;
+import faang.school.projectservice.model.Vacancy;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+public class NameFilterTest {
+
+    @InjectMocks
+    private NameFilter nameFilter;
+
+    @Test
+    public void testIsApplicable_NameNull_ReturnsFalse() {
+        var filterDto = new VacancyFilterRequestDto();
+
+        var result = nameFilter.isApplicable(filterDto);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsApplicable_NameEmpty_ReturnsFalse() {
+        var filterDto = new VacancyFilterRequestDto();
+        filterDto.setNamePattern("");
+
+        var result = nameFilter.isApplicable(filterDto);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsApplicable_NameBlank_ReturnsFalse() {
+        var filterDto = new VacancyFilterRequestDto();
+        filterDto.setNamePattern("   ");
+
+        var result = nameFilter.isApplicable(filterDto);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsApplicable_NameNotBlank_ReturnsTrue() {
+        var filterDto = new VacancyFilterRequestDto();
+        filterDto.setNamePattern("Test");
+
+        var result = nameFilter.isApplicable(filterDto);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void testApply_HaveMatchedVacancies_ReturnsNonEmptyStream() {
+        var filterDto = new VacancyFilterRequestDto();
+        filterDto.setNamePattern("[a-z ]+");
+        var expectedItems = generateExpectedItems();
+        var source = generateSource(expectedItems);
+
+        var result = nameFilter.apply(source, filterDto);
+
+        assertIterableEquals(expectedItems, result.toList());
+    }
+
+    private static List<Vacancy> generateExpectedItems() {
+        return List.of(
+                Vacancy.builder()
+                        .id(1L)
+                        .name("first vacancy")
+                        .build(),
+                Vacancy.builder()
+                        .id(2L)
+                        .name("vacancy")
+                        .build());
+    }
+
+    private static Stream<Vacancy> generateSource(List<Vacancy> expectedItems) {
+        return Stream.concat(
+                expectedItems.stream(),
+                Stream.of(
+                        Vacancy.builder()
+                                .id(10L)
+                                .name("2nd vacancy")
+                                .build(),
+                        Vacancy.builder()
+                                .id(11L)
+                                .name("Third vacancy")
+                                .build()));
+    }
+}
---
+++ src/test/java/faang/school/projectservice/filter/vacancy/PositionFilterTest.java
@@ -0,0 +1,80 @@
+package faang.school.projectservice.filter.vacancy;
+
+import faang.school.projectservice.dto.vacancy.VacancyFilterRequestDto;
+import faang.school.projectservice.model.TeamRole;
+import faang.school.projectservice.model.Vacancy;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+public class PositionFilterTest {
+
+    @InjectMocks
+    private PositionFilter positionFilter;
+
+    @Test
+    public void testIsApplicable_PositionNull_ReturnsFalse() {
+        var filterDto = new VacancyFilterRequestDto();
+
+        var result = positionFilter.isApplicable(filterDto);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsApplicable_PositionPresents_ReturnsTrue() {
+        var filterDto = new VacancyFilterRequestDto();
+        filterDto.setPosition(TeamRole.ANALYST);
+
+        var result = positionFilter.isApplicable(filterDto);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void testApply_HaveMatchedVacancies_ReturnsNonEmptyStream() {
+        var filterDto = new VacancyFilterRequestDto();
+        filterDto.setPosition(TeamRole.ANALYST);
+        var expectedItems = generateExpectedItems();
+        var source = generateSource(expectedItems);
+
+        var result = positionFilter.apply(source, filterDto);
+
+        assertIterableEquals(expectedItems, result.toList());
+    }
+
+    private static List<Vacancy> generateExpectedItems() {
+        return List.of(
+                Vacancy.builder()
+                        .id(1L)
+                        .position(TeamRole.ANALYST)
+                        .build(),
+                Vacancy.builder()
+                        .id(2L)
+                        .position(TeamRole.ANALYST)
+                        .build());
+    }
+
+    private static Stream<Vacancy> generateSource(List<Vacancy> expectedItems) {
+        return Stream.concat(
+                expectedItems.stream(),
+                Stream.of(
+                        Vacancy.builder()
+                                .id(10L)
+                                .position(TeamRole.INTERN)
+                                .build(),
+                        Vacancy.builder()
+                                .id(11L)
+                                .position(TeamRole.DEVELOPER)
+                                .build()));
+    }
+}
