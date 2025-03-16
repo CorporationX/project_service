a3e49c27272d36acd1a33f9d29e7710626ce5c2c
Sergey <bigwave2915@gmail.com>
2025-03-16 15:29:48 +0500

    Подключение плагина JaCoCo 2,0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,76 +1,144 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.0.6"
+    id("io.spring.dependency-management") version "1.1.0"
+    jacoco
+}
+
+group = "faang.school"
+version = "1.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    /**
+     * Spring boot starters
+     */
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    /**
+     * Database
+     */
+    implementation("org.liquibase:liquibase-core")
+    implementation("redis.clients:jedis:4.3.2")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("com.h2database:h2")
+
+    /**
+     * Amazon S3
+     */
+    implementation("software.amazon.awssdk:s3:2.29.20")
+
+    /**
+     * Imgscalr
+     */
+    implementation("org.imgscalr:imgscalr-lib:4.2")
+
+    /**
+     * Apache Tika
+     */
+    implementation("org.apache.tika:tika-core:2.9.2")
+
+    /**
+     * Utils & Logging
+     */
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation("org.slf4j:slf4j-api:2.0.5")
+    implementation("ch.qos.logback:logback-classic:1.4.6")
+    implementation("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    implementation("org.mapstruct:mapstruct:1.5.3.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+
+    /**
+     * Test containers
+     */
+    implementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
+
+    /**
+     * Tests
+     */
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    /**
+     * Swagger
+     */
+    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+val test by tasks.getting(Test::class) { testLogging.showStandardStreams = true }
+
+tasks.bootJar {
+    archiveFileName.set("service.jar")
+}
+
+
 jacoco {
     toolVersion = "0.8.9"
     reportsDirectory.set(layout.buildDirectory.dir("$buildDir/reports/jacoco"))
 }
-
-val exclusions = listOf(
-    "**/UserServiceApplication*",
-    "**/controller/**",
-    "**/mapper/**",
-    "**/entity/**",
-    "**/dto/**",
-    "**/exception/**",
-    "**/com/json/**",
-    "**/client/**",
-    "**/config/**"
-)
-
 tasks.test {
-    finalizedBy(tasks.jacocoTestReport, tasks.jacocoTestCoverageVerification)
-    testLogging {
-        events("passed", "skipped", "failed")
-        showStandardStreams = true
-    }
+    finalizedBy(tasks.jacocoTestReport)
 }
 
-tasks.build {
-    dependsOn(tasks.jacocoTestCoverageVerification)
-    doLast {
-        println("Build completed successfully with test coverage verification.")
-    }
-}
+val jacocoIncludePackagesList = listOf(
+    "**/controller/**",
+    "**/service/**",
+    "**/mapper/**"
+)
+val jacocoExcludePackAgeList = listOf(
+    "**/model/**",
+    "**/repository/**",
+    "**/dto/**"
+)
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+
     reports {
         xml.required.set(true)
         csv.required.set(false)
         html.required.set(true)
-        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
     }
 
     classDirectories.setFrom(
-        files(sourceSets.main.get().output.asFileTree.matching {
-            exclude(exclusions)
-        })
+        sourceSets.main.get().output.asFileTree.matching {
+            include(jacocoIncludePackagesList)
+            exclude(jacocoExcludePackAgeList)
+        }
     )
 }
 
 tasks.jacocoTestCoverageVerification {
     violationRules {
         rule {
-            element = "CLASS"
             limit {
-                minimum = 0.7.toBigDecimal()
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.70".toBigDecimal()
             }
-        }
-        rule {
-            element = "METHOD"
             limit {
-                minimum = 0.8.toBigDecimal()
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.70".toBigDecimal()
             }
-        }
-        rule {
-            element = "LINE"
-            limit {
-                minimum = 0.6.toBigDecimal()
-            }
+
         }
     }
-
-    classDirectories.setFrom(
-        files(sourceSets.main.get().output.asFileTree.matching {
-            exclude(exclusions)
-        })
-    )
 }
