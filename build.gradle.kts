01bc98f800ad9757e88efcc5f7ecb777d6461d36
Sergey <bigwave2915@gmail.com>
2025-03-16 15:42:29 +0500

    Подключение плагина JaCoCo 2,0

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,7 +112,7 @@ tasks.jacocoTestReport {
     dependsOn(tasks.test)
 
     reports {
-        xml.required.set(true)
+        xml.required.set(false)
         csv.required.set(false)
         html.required.set(true)
     }
