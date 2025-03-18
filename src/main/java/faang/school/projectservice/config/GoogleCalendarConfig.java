bdb02006880670bbe7dd0fcc7f41acd4d3d3e053
senty <senty1844@gmail.com>
2025-03-18 21:17:24 +0300

    Исправил аутентификацию google account, сломал отправку приглашений

--- src/main/java/faang/school/projectservice/config/GoogleCalendarConfig.java
+++ src/main/java/faang/school/projectservice/config/GoogleCalendarConfig.java
@@ -1,69 +1,39 @@
 package faang.school.projectservice.config;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-import java.util.List;
+import java.util.Collections;
 
 @Configuration
 public class GoogleCalendarConfig {
     private static final String APPLICATION_NAME = "CorporationX";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-    private static final List<String> SCOPES = CalendarScopes.all().stream().toList();
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-
+    private static final String SERVICE_ACCOUNT_KEY_PATH = "src/main/resources/credentials.json";
 
     @Bean
     public Calendar calendar() throws IOException, GeneralSecurityException {
-        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        return new Calendar.Builder(httpTransport, JSON_FACTORY, getCredentials(httpTransport))
-                        .setApplicationName(APPLICATION_NAME)
-                        .build();
-    }
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
-    /**
-     * Creates an authorized Credential object.
-     *
-     * @param httpTransport The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
-    private static Credential getCredentials(final NetHttpTransport httpTransport)
-            throws IOException {
-        // Load client secrets.
-        InputStream in = GoogleCalendarConfig.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(SERVICE_ACCOUNT_KEY_PATH))
+                .createScoped(Collections.singleton(CalendarScopes.CALENDAR));
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+        HttpCredentialsAdapter requestInitializer = new HttpCredentialsAdapter(credentials);
+
+        return new Calendar.Builder(httpTransport, JSON_FACTORY, requestInitializer)
+                .setApplicationName(APPLICATION_NAME)
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8082).build();
-        //returns an authorized Credential object.
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 }
+
