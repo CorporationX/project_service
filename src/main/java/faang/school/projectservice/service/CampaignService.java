b76a079893a065e03cf1b8d69fa6ff87b9de7f6a
Ilyas Azhdar <azhdar902@gmail.com>
2025-03-21 19:08:08 +0500

    added campaign create method

--- src/main/java/faang/school/projectservice/service/CampaignService.java
+++ src/main/java/faang/school/projectservice/service/CampaignService.java
@@ -1,6 +1,14 @@
 package faang.school.projectservice.service;
 
+import faang.school.projectservice.dto.CampaignDto;
+import faang.school.projectservice.exception.PermissionDeniedException;
+import faang.school.projectservice.exception.ProjectNotFoundException;
+import faang.school.projectservice.mapper.CampaignMapper;
+import faang.school.projectservice.model.Campaign;
+import faang.school.projectservice.model.TeamRole;
 import faang.school.projectservice.repository.CampaignRepository;
+import faang.school.projectservice.repository.ProjectRepository;
+import faang.school.projectservice.repository.TeamMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +16,34 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CampaignService {
     private final CampaignRepository campaignRepository;
+    private final TeamMemberRepository teamMemberRepository;
+    private final ProjectRepository projectRepository;
+    private final CampaignMapper campaignMapper;
+
+    public CampaignDto create(CampaignDto campaignDto) {
+        var userId = campaignDto.getUpdatedBy();
+        campaignDto.setCreatedBy(userId);
+        Campaign campaign = campaignMapper.toEntity(campaignDto);
+
+        if (isManager(userId) || isProjectOwner(campaign, userId, campaignDto.getProjectId())) {
+            return campaignMapper.toDto(campaignRepository.save(campaign));
+        }
+
+        throw new PermissionDeniedException();
+    }
+
+    private boolean isManager(Long userId) {
+        return teamMemberRepository.findByUserId(userId).stream()
+                .flatMap(teamMember -> teamMember.getRoles().stream())
+                .anyMatch(teamRole -> teamRole == TeamRole.MANAGER);
+    }
+
+    private boolean isProjectOwner(Campaign campaign, Long userId, Long projectId) {
+        var project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new ProjectNotFoundException(projectId));
+        campaign.setProject(project);
+
+        return project.getOwnerId().equals(userId);
+    }
 
 }
