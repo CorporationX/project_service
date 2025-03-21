ea320818e85525f0a0f38b9f1def670773b3c150
Ilyas Azhdar <azhdar902@gmail.com>
2025-03-21 19:07:00 +0500

    validate fields

--- src/main/java/faang/school/projectservice/dto/CampaignDto.java
+++ src/main/java/faang/school/projectservice/dto/CampaignDto.java
@@ -3,19 +3,32 @@ package faang.school.projectservice.dto;
 import faang.school.projectservice.dto.client.Currency;
 import faang.school.projectservice.model.CampaignStatus;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Data
 public class CampaignDto {
     private Long id;
+
     @NotNull(message = "Title is required")
+    @Size(max = 128, message = "Title should not be greater than 128 characters")
     private String title;
+
     @NotNull(message = "Description is required")
+    @Size(max = 4096, message = "Description should not be greater than 4096 characters")
     private String description;
+
+    @NotNull(message = "Project is required")
     private Long projectId;
-    private CampaignStatus status;
-    private Currency currency;
-    @NotNull(message = "Creating user is required")
-    private Long createdBy;
+
+    @NotNull(message = "User is required")
     private Long updatedBy;
+
+    private CampaignStatus status;
+    private BigDecimal goal;
+    private Currency currency;
+    private Long createdBy;
+
 }
