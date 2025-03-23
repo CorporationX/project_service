18038e6c6687a475c7ca52895bbf028d92a7d1c1
sanctusgermanus72362 <sanctus-germanus7@yandex.ru>
2025-03-23 12:43:44 +0300

    Исправления в валидации

--- src/main/java/faang/school/projectservice/exception/DataValidationException.java
+++ src/main/java/faang/school/projectservice/exception/DataValidationException.java
@@ -1,5 +1,7 @@
 package faang.school.projectservice.exception;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 public class DataValidationException extends RuntimeException {
 
