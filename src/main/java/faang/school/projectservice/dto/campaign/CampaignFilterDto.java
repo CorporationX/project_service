d339cd88fa0d4fcdd2c535576ef27bb399593864
Ilyas Azhdar <azhdar902@gmail.com>
2025-03-27 18:50:44 +0500

    added tests for controller

--- src/main/java/faang/school/projectservice/dto/campaign/CampaignFilterDto.java
+++ src/main/java/faang/school/projectservice/dto/campaign/CampaignFilterDto.java
@@ -1,6 +1,8 @@
 package faang.school.projectservice.dto.campaign;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import faang.school.projectservice.model.CampaignStatus;
 
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record CampaignFilterDto(String startDate, CampaignStatus status, Long createdId) {
 }
