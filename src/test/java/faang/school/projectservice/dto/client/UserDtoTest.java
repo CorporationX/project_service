ae8c3bbc5b34bd6ecef0c3010b69c338a29426ab
RamilNazmiev192 <nazmiev633@gmail.com>
2025-03-31 12:12:40 +0300

    add invalid test

---
+++ src/test/java/faang/school/projectservice/dto/client/UserDtoTest.java
@@ -0,0 +1,13 @@
+package faang.school.projectservice.dto.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDtoTest {
+
+    @Test
+    public  void checkTest(){
+        assertTrue(false);
+    }
+}
