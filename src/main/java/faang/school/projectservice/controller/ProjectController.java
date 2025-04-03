d3c322eeea36e9d7fc1d0b8c69cc358ad2eba4db
maksim <maksim_vasilenko@yahoo.com>
2025-04-03 09:59:20 +0300

    BJS2-66697-refactoring-project_service - ProjectController.java - renamed from PresentationController.java

--- src/main/java/faang/school/projectservice/controller/ProjectController.java
+++ src/main/java/faang/school/projectservice/controller/ProjectController.java
@@ -28,7 +28,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/projects")
 @RequiredArgsConstructor
-public class PresentationController {
+public class ProjectController {
 
     private final ProjectService projectService;
 
