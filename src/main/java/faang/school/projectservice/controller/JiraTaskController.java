5303e15ee05d42440b2c8c3f62c917ec7624fb34
Max Goloviznin <roxet59rus@mail.ru>
2025-04-04 23:14:51 +0500

    (BJS2-65397) conflicts resolved, other fixes

--- src/main/java/faang/school/projectservice/controller/JiraTaskController.java
+++ src/main/java/faang/school/projectservice/controller/JiraTaskController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -41,7 +42,7 @@ public class JiraTaskController {
         return jiraTaskService.updateJiraTask(issueKey, request);
     }
 
-    @PutMapping("/status/{issueKey}")
+    @PatchMapping("/status/{issueKey}")
     @ResponseStatus(HttpStatus.OK)
     public Mono<Void> updateStatusJiraTask(@PathVariable String issueKey,
                                            @Valid @RequestBody JiraStatusUpdateRequest request) {
