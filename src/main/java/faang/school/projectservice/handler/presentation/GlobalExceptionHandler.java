22ef57cd092808764cc3bb675ca6ed19632fb57a
Anton Kapshukov <kapshukov.04@mail.ru>
2025-04-06 23:26:38 +0300

    BJS2-66206: исправления

--- src/main/java/faang/school/projectservice/handler/presentation/GlobalExceptionHandler.java
+++ src/main/java/faang/school/projectservice/handler/presentation/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
-public class PresentationExceptionController {
+public class GlobalExceptionHandler {
     private final ErrorMessageService errorMessageService;
 
     @ExceptionHandler(ProjectNotFoundException.class)
