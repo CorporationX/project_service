49de18ac44d59bfb77ce6f686fa6ce9964ba474f
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-04-09 22:45:33 +0200

    corrected

--- src/main/java/faang/school/projectservice/ProjectServiceApplication.java
+++ src/main/java/faang/school/projectservice/ProjectServiceApplication.java
@@ -1,9 +1,11 @@
 package faang.school.projectservice;
 
+import faang.school.projectservice.config.s3.S3Properties;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.Async;
@@ -12,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
+@EnableConfigurationProperties({S3Properties.class})
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
 @Async
