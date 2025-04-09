a3ca6d9c3bf7b6c1657f28a7948fce2b5a14ab48
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-04-09 22:42:39 +0200

    corrected

--- src/main/java/faang/school/projectservice/config/s3/S3Properties.java
+++ src/main/java/faang/school/projectservice/config/s3/S3Properties.java
@@ -1,13 +1,17 @@
 package faang.school.projectservice.config.s3;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "services.s3")
+@RequiredArgsConstructor(onConstructor_ = @ConstructorBinding)
 public class S3Properties {
     private String endpoint;
     private String accessKey;
