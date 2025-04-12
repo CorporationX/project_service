defd53b02720b95269eec3bd7183ca23427944fb
Anton Kapshukov <kapshukov.04@mail.ru>
2025-04-12 19:15:03 +0300

    BJS2-66206: исправления

--- src/test/java/faang/school/projectservice/service/presentation/PdfFileStorageServiceTest.java
+++ src/test/java/faang/school/projectservice/service/presentation/PdfFileStorageServiceTest.java
@@ -1,12 +1,10 @@
 package faang.school.projectservice.service.presentation;
 
-import faang.school.projectservice.dto.ErrorResponse;
 import faang.school.projectservice.exception.presentation.FileDownloadException;
 import faang.school.projectservice.exception.presentation.FileUploadException;
 import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
-import io.minio.errors.ErrorResponseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,11 +14,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.io.ByteArrayInputStream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
