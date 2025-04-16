17684d15d5a6d29513fca90d6267f85726827870
Ekaterina Ramakoti <zubkova-92@mail.ru>
2025-04-16 19:21:35 +0300

    Продолжение

--- src/main/java/faang/school/projectservice/controller/ProjectController.java
+++ src/main/java/faang/school/projectservice/controller/ProjectController.java
@@ -10,20 +10,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("gallery")
 @Data
 public class ProjectController {
 
     private final ProjectService projectService;
 
-    @PostMapping("/{projectId}/resources")
+    @PostMapping("/project/{projectId}/upload")
     public ResponseEntity<ResourceReadDto> uploadResource(
             @PathVariable long projectId,
-            @RequestBody MultipartFile file)  {
+            @RequestParam("files") MultipartFile file)  {
         System.out.println();
         return ResponseEntity.ok(projectService.uploadResourceToGallery(projectId, file));
     }
