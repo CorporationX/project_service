8058e1891376a4105b6cf0803387294a7112d037
Sergey Selischev <ssb22@mail.ru>
2025-05-13 06:17:12 +0300

    test gradle .yml

---
+++ src/test/java/faang/school/projectservice/TestClass.java
@@ -0,0 +1,13 @@
+package faang.school.projectservice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class TestClass {
+
+    @Test
+    public void failTest(){
+        fail();
+    }
+}
