0aa6397edce41171bc38cfa4bc81bda58b2c6f47
Sergey Selischev <ssb22@mail.ru>
2025-05-13 04:47:43 +0300

    BJS2-77190 Настройка CI/CD

--- src/main/java/faang/school/projectservice/dto/client/PaymentRequest.java
+++ src/main/java/faang/school/projectservice/dto/client/PaymentRequest.java
@@ -1,14 +1,14 @@
 package faang.school.projectservice.dto.client;
 
-import java.math.BigDecimal;
-
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-public record PaymentRequest (
+import java.math.BigDecimal;
+
+public record PaymentRequest(
         Long paymentNumber,
 
-        @Min(1)
+        @Min (1)
         @NotNull
         BigDecimal amount,
 
