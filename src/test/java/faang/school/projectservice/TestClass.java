3b7740b17816fdfaed6588aea454766fb5765cfc
Sergey Selischev <ssb22@mail.ru>
2025-05-13 09:04:59 +0300

    test gradle .yml

--- src/test/java/faang/school/projectservice/TestClass.java
+++ src/test/java/faang/school/projectservice/TestClass.java
@@ -1,8 +1,9 @@
 package faang.school.projectservice;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 public class TestClass {
 
