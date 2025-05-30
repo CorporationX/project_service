d6f26bb13ec986ea62a454625528d6be92670c8d
Vladimir Levshin <levshin@intervolga.ru>
2025-05-31 00:22:26 +0300

    BJS2-77638: Added api for resources. WIP.

--- src/main/java/faang/school/projectservice/repository/ResourceRepository.java
+++ src/main/java/faang/school/projectservice/repository/ResourceRepository.java
@@ -2,7 +2,14 @@ package faang.school.projectservice.repository;
 
 import faang.school.projectservice.model.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
+    @Query(nativeQuery = true, value = "select * from project_resource where key = :key;")
+    Optional<Resource> findByKey(String key);
 
+    @Query(nativeQuery = true, value = "select * from project_resource where name = :fileName and project_id = :projectId;")
+    Optional<Resource> findByNameAndProjectId(String fileName, long projectId);
 }
--- src/main/java/faang/school/projectservice/service/ResourceService.java
+++ src/main/java/faang/school/projectservice/service/ResourceService.java
@@ -1,12 +1,13 @@
 package faang.school.projectservice.service;
 
+import faang.school.projectservice.dto.ResourceDto;
 import faang.school.projectservice.model.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URL;
 
 public interface ResourceService {
-    Resource uploadEntityFile(MultipartFile file, long id);
+    ResourceDto uploadEntityFile(MultipartFile file, long id);
 
     URL getFileUrl(String fileKey);
 
