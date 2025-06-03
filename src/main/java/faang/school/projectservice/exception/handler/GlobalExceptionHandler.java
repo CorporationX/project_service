f8276dba49d11953b3306349fd1727fa127b286b
pirojok_yt <tamik34@mail.ru>
2025-06-03 21:09:28 +0500

    ci.yml

--- src/main/java/faang/school/projectservice/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/faang/school/projectservice/exception/handler/GlobalExceptionHandler.java
@@ -1,14 +1,18 @@
 package faang.school.projectservice.exception.handler;
 
+import jakarta.servlet.ServletException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -20,13 +24,31 @@ public class GlobalExceptionHandler {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
+        log.error(ex.getMessage());
         return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleIllegalArgumentExceptions(IllegalArgumentException ex) {
+        log.error(ex.getMessage());
         return ResponseEntity
                 .badRequest()
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<Object> handleIOExceptions(IOException ex) {
+        log.error(ex.getMessage());
+        return ResponseEntity
+                .badRequest()
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ServletException.class)
+    public ResponseEntity<Object> handleServletExceptions(ServletException ex) {
+        log.error(ex.getMessage());
+        return ResponseEntity
+                .internalServerError()
                 .body(ex.getMessage());
     }
 }
