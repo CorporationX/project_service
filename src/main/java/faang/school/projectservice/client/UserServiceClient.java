e39da0ec5358017d9c3330c33badc3514e51c339
KirillSMv <88364531+KirillSMv@users.noreply.github.com>
2025-06-05 20:22:07 +0200

    Add service discovery (#1241)
    * "Добавить конфигурацию для подключения к Service Discovery"
    
    * "Добавить конфигурацию для подключения к Service Discovery, обновить конфигурацию feign client"

--- src/main/java/faang/school/projectservice/client/UserServiceClient.java
+++ src/main/java/faang/school/projectservice/client/UserServiceClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "user-service", url = "${services.user-service.host}:${services.user-service.port}")
+@FeignClient(name = "${services.user-service.name}")
 public interface UserServiceClient {
 
     @GetMapping("/user/{userId}")
