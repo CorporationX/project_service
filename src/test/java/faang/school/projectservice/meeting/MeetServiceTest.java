953570868c84f6e7909e080da3d7c6d8b276951c
Nazar <kateryna@Katerynas-MacBook-Pro.local>
2025-06-11 14:28:28 +0200

    fix in MeetService

--- src/test/java/faang/school/projectservice/meeting/MeetServiceTest.java
+++ src/test/java/faang/school/projectservice/meeting/MeetServiceTest.java
@@ -9,6 +9,7 @@ import faang.school.projectservice.model.Meet;
 import faang.school.projectservice.model.MeetStatus;
 import faang.school.projectservice.model.Project;
 import faang.school.projectservice.repository.MeetRepository;
+import faang.school.projectservice.repository.adapter.meeting.MeetRepositoryAdapter;
 import faang.school.projectservice.service.meeting.MeetService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,6 +39,9 @@ class MeetServiceTest {
 
     @InjectMocks
     MeetService meetService;
+
+    @Mock
+    private MeetRepositoryAdapter meetRepositoryAdapter;
 
     MeetDto dto;
     Meet entity;
@@ -82,89 +86,113 @@ class MeetServiceTest {
         verify(meetRepository).save(any(Meet.class));
     }
 
-//    @Test
-//    void cancel_ok() {
-//        long meetId = 3L;
-//        long creator = 2L;
-//        entity.setId(meetId);
-//        entity.setCreatorId(creator);
-//        when(meetRepository.findById(meetId)).thenReturn(Optional.of(entity));
-//        when(meetRepository.save(entity)).thenReturn(entity);
-//        when(meetMapper.toDto(entity)).thenReturn(new MeetDto());
-//
-//        meetService.cancel(meetId, creator);
-//
-//        assertThat(entity.getStatus()).isEqualTo(MeetStatus.CANCELLED);
-//        assertThat(entity.isActive()).isFalse();
-//    }
-//
-//    @Test
-//    void cancel_wrongUser() {
-//        entity.setId(7L);
-//        entity.setCreatorId(1L);
-//        when(meetRepository.findById(7L)).thenReturn(Optional.of(entity));
-//
-//        assertThatThrownBy(() -> meetService.cancel(7L, 9L))
-//                .isInstanceOf(AccessDeniedException.class);
-//    }
-//
-//    @Test
-//    void removeParticipant_ok() {
-//        when(meetRepository.findById(10L)).thenReturn(Optional.of(entity));
-//        when(meetRepository.save(entity)).thenReturn(entity);
-//        MeetDto dto = new MeetDto();
-//        when(meetMapper.toDto(entity)).thenReturn(dto);
-//
-//        MeetDto result = meetService.removeParticipant(10L, 2L, 3L);
-//
-//        assertThat(result).isSameAs(dto);
-//        assertThat(entity.getUserIds()).doesNotContain(3L);
-//        verify(meetRepository).save(entity);
-//    }
-//
-//    @Test
-//    void removeParticipant_wrongCreator() {
-//        when(meetRepository.findById(10L)).thenReturn(Optional.of(entity));
-//
-//        assertThatThrownBy(() -> meetService.removeParticipant(10L, 99L, 3L))
-//                .isInstanceOf(AccessDeniedException.class);
-//    }
-//
-//    @Test
-//    void removeParticipant_notPresent() {
-//        when(meetRepository.findById(10L)).thenReturn(Optional.of(entity));
-//
-//        assertThatThrownBy(() -> meetService.removeParticipant(10L, 2L, 99L))
-//                .isInstanceOf(ParticipantNotFoundException.class);
-//    }
-//
-//    @Test
-//    void update_ok() {
-//        long meetId = 8L;
-//        long creator = 4L;
-//        entity.setId(meetId);
-//        entity.setCreatorId(creator);
-//        when(meetRepository.findById(meetId)).thenReturn(Optional.of(entity));
-//        when(meetRepository.save(entity)).thenReturn(entity);
-//        when(meetMapper.toDto(entity)).thenReturn(new MeetDto());
-//
-//        MeetDto patch = new MeetDto();
-//        patch.setTitle("New");
-//        patch.setDescription("NewDesc");
-//        patch.setScheduledAt(LocalDateTime.now().plusDays(2));
-//
-//        meetService.update(meetId, creator, patch);
-//
-//        assertThat(entity.getTitle()).isEqualTo("New");
-//        assertThat(entity.getDescription()).isEqualTo("NewDesc");
-//    }
-//
-//    @Test
-//    void findById_notFound() {
-//        when(meetRepository.findById(100L)).thenReturn(Optional.empty());
-//        assertThatThrownBy(() -> meetService.findById(100L))
-//                .isInstanceOf(ProjectNotFoundException.class);
-//    }
+    @Test
+    void cancel_ok() {
+        long meetId = 3L;
+        long creator = 2L;
+        entity.setId(meetId);
+        entity.setCreatorId(creator);
+
+        when(meetRepositoryAdapter.fetchByIdOrThrow(meetId))
+                .thenReturn(entity);
+        when(meetMapper.toDto(entity)).thenReturn(new MeetDto());
+
+        meetService.cancel(meetId, creator);
+
+        assertThat(entity.getStatus()).isEqualTo(MeetStatus.CANCELLED);
+        assertThat(entity.isActive()).isFalse();
+    }
+
+    @Test
+    void cancel_wrongUser() {
+        entity.setId(7L);
+        entity.setCreatorId(1L);
+
+        when(meetRepositoryAdapter.fetchByIdOrThrow(7L))
+                .thenReturn(entity);
+
+        assertThatThrownBy(() -> meetService.cancel(7L, 9L))
+                .isInstanceOf(AccessDeniedException.class);
+    }
+
+    @Test
+    void removeParticipant_ok() {
+        long meetId = 10L;
+        entity.setCreatorId(2L);
+        entity.setId(meetId);
+
+        when(meetRepositoryAdapter.fetchByIdOrThrow(meetId))
+                .thenReturn(entity);
+        when(meetRepository.save(entity)).thenReturn(entity);
+        MeetDto dto = new MeetDto();
+        when(meetMapper.toDto(entity)).thenReturn(dto);
+
+        MeetDto result = meetService.removeParticipant(meetId, 2L, 3L);
+
+        assertThat(result).isSameAs(dto);
+        assertThat(entity.getUserIds()).doesNotContain(3L);
+        verify(meetRepository).save(entity);
+    }
+
+    @Test
+    void removeParticipant_wrongCreator() {
+        long meetId = 10L;
+        entity.setCreatorId(2L);
+        entity.setId(meetId);
+
+        when(meetRepositoryAdapter.fetchByIdOrThrow(meetId))
+                .thenReturn(entity);
+
+        assertThatThrownBy(() -> meetService.removeParticipant(meetId, 99L, 3L))
+                .isInstanceOf(AccessDeniedException.class);
+    }
+
+    @Test
+    void removeParticipant_notPresent() {
+        long meetId = 10L;
+        entity.setCreatorId(2L);
+        entity.setUserIds(new ArrayList<>(List.of(4L,5L)));
+        entity.setId(meetId);
+
+        when(meetRepositoryAdapter.fetchByIdOrThrow(meetId))
+                .thenReturn(entity);
+
+        assertThatThrownBy(() -> meetService.removeParticipant(meetId, 2L, 3L))
+                .isInstanceOf(ParticipantNotFoundException.class);
+    }
+
+    @Test
+    void update_ok() {
+        long meetId = 8L;
+        long creator = 4L;
+        entity.setId(meetId);
+        entity.setCreatorId(creator);
+
+        when(meetRepositoryAdapter.fetchByIdOrThrow(meetId))
+                .thenReturn(entity);
+        when(meetRepository.save(entity)).thenReturn(entity);
+        when(meetMapper.toDto(entity)).thenReturn(new MeetDto());
+
+        MeetDto patch = new MeetDto();
+        patch.setTitle("New");
+        patch.setDescription("NewDesc");
+        patch.setScheduledAt(LocalDateTime.now().plusDays(2));
+
+        meetService.update(meetId, creator, patch);
+
+        assertThat(entity.getTitle()).isEqualTo("New");
+        assertThat(entity.getDescription()).isEqualTo("NewDesc");
+    }
+
+    @Test
+    void findById_notFound() {
+        long meetId = 100L;
+        when(meetRepositoryAdapter.fetchByIdOrThrow(meetId))
+                .thenThrow(new ProjectNotFoundException(meetId));
+
+        assertThatThrownBy(() -> meetService.findById(meetId))
+                .isInstanceOf(ProjectNotFoundException.class);
+    }
 
     @Test
     void filter_projectAndTitle() {
