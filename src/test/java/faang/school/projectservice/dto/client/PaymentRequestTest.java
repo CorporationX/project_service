30113c8663984ac687b4322f5b0c3dd13e308401
pirojok_yt <tamik34@mail.ru>
2025-06-12 21:52:31 +0500

    ci.yml

---
+++ src/test/java/faang/school/projectservice/dto/client/PaymentRequestTest.java
@@ -0,0 +1,11 @@
+package faang.school.projectservice.dto.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentRequestTest {
+
+    @Test
+    public void test() {}
+}
