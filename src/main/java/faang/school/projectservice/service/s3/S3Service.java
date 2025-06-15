cfbd21603f7516f271583e3934bf2d67c2930df2
v888.rus <v888.rus@gmail.com>
2025-06-15 16:46:13 +0000

    Fix(3). 77850 Team avatar upload. Mentor's comments.

--- src/main/java/faang/school/projectservice/service/s3/S3Service.java
+++ src/main/java/faang/school/projectservice/service/s3/S3Service.java
@@ -3,57 +3,9 @@ package faang.school.projectservice.service.s3;
 import java.io.File;
 import java.io.InputStream;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class S3Service {
-    private final AmazonS3 s3Client;
-    
-    @Value("${services.s3.bucketName}")
-    private String bucketName;
-
-    public String uploadFile(File file, String contentType, String folder) {
-        String key = String.format("%s/%d%s", folder, System.currentTimeMillis(), file.getName());
-        PutObjectRequest putObjectRequest = new PutObjectRequest(
-            bucketName, 
-            key, 
-            file
-        );
-        s3Client.putObject(putObjectRequest);
-        log.debug("File is saved with name {}", key);
-        return key;
-    }
-
-    public InputStream downloadFile(String objectKey) {
-        try {
-            S3Object s3Object = s3Client.getObject(bucketName, objectKey);
-            return s3Object.getObjectContent();
-        } catch (Exception e) {
-            throw new RuntimeException("Error downloading file from S3");
-        }
-    }
-
-    public String getAvatarContentType(String objectKey) {
-        try {
-            S3Object s3Object = s3Client.getObject(bucketName, objectKey);
-            return s3Object.getObjectMetadata().getContentType();
-        } catch (Exception e) {
-            throw new RuntimeException("Error downloading file from S3");
-        }
-    }
-
-    public void deleteFile(String key) {
-        s3Client.deleteObject(bucketName, key);
-    }
-
+public interface S3Service {
+    public String uploadFile(File file, String contentType, String folder);
+    public InputStream downloadFile(String objectKey);
+    public String getAvatarContentType(String objectKey);
+    public void deleteFile(String key);
 }
--- src/main/java/faang/school/projectservice/utils/FileProcessor.java
+++ src/main/java/faang/school/projectservice/utils/FileProcessor.java
@@ -3,24 +3,23 @@ package faang.school.projectservice.utils;
 import java.io.File;
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnails;
 
 @Slf4j
-@Component
+@UtilityClass
 public class FileProcessor {
-    @Value("${team-avatar-file.maxDimension}")
-    private int avatarMaxDimension;
+    private static final int AVATAR_MAX_DEMINSION = 50;
 
     public File resizeImage(MultipartFile file) {
+        log.info("Max picture size is: {}", AVATAR_MAX_DEMINSION);
         try {
             File outputFile = new File(file.getOriginalFilename());
             Thumbnails.of(file.getInputStream())
-                .size(avatarMaxDimension, avatarMaxDimension)
+                .size(AVATAR_MAX_DEMINSION, AVATAR_MAX_DEMINSION)
                 .keepAspectRatio(true)
                 .toFile(outputFile);
             return outputFile;
