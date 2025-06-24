2726d86d67c7132d9151d215ef82509e6ac579ce
Miras <mirasospan62@gmail.com>
2025-06-24 21:21:11 +0500

    checkstyle corrections

--- src/main/java/faang/school/projectservice/config/swagger/OpenApiConfig.java
+++ src/main/java/faang/school/projectservice/config/swagger/OpenApiConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApi {
+public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
