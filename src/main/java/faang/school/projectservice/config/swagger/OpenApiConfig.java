def705b9db05b4e747e7cae03a4dc7398af31737
Miras <mirasospan62@gmail.com>
2025-06-24 22:35:38 +0500

    checkstyle corrections

--- src/main/java/faang/school/projectservice/config/swagger/OpenApiConfig.java
+++ src/main/java/faang/school/projectservice/config/swagger/OpenApiConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenApi() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Project Service API")
