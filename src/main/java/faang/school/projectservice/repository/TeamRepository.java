af1e66feba6d1327b0b9a768de47ec7dd61daf98
Miras <mirasospan62@gmail.com>
2025-06-26 16:58:32 +0500

    fixed problems

--- src/main/java/faang/school/projectservice/repository/TeamRepository.java
+++ src/main/java/faang/school/projectservice/repository/TeamRepository.java
@@ -1,7 +1,10 @@
 package faang.school.projectservice.repository;
 
 import faang.school.projectservice.model.Team;
+import faang.school.projectservice.model.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Team findByTeamMembers(TeamMember teamMember);
 }
