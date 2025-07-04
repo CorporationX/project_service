b7940a0b48b059876d2e42d549b8364bb637abe9
Yulia <bualex1983@mail.ru>
2025-07-04 23:46:17 +0300

    error fixes

---
+++ src/main/java/faang/school/projectservice/bjs_77845/ErrorResponse.java
@@ -0,0 +1,33 @@
+package faang.school.projectservice.bjs_77845;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    private int status;
+    private String error;
+    private String message;
+
+    private String name;
+    private String email;
+    private String address;
+
+    public ErrorResponse(HttpStatus httpStatus, String message) {
+        this.timestamp = LocalDateTime.now();
+        this.status = httpStatus.value();
+        this.error = httpStatus.getReasonPhrase();
+        this.message = message;
+    }
+}
---
+++ src/main/java/faang/school/projectservice/bjs_77845/GlobalExeptionHandler.java
@@ -0,0 +1,60 @@
+package faang.school.projectservice.bjs_77845;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExeptionHandler {
+
+    @ExceptionHandler(DataValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse HandleDataValidationException(DataValidationException e) {
+        log.error("Data Validation Exception occurred: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidExcetion(MethodArgumentNotValidException e) {
+        log.error("Method Argument Not Valid Exception occurred: {}", e.getMessage());
+        return e.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(
+                    error ->(fieldError)error).getField(),
+                    error -> Objects.requireNonNullElse(error.getDedaultMessage(), "")
+                );
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse HandleEntityNotFoundException(EntityNotFoundException e) {
+        log.error("Entity Not Found Exception occurred: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleRuntimeExcption(RuntimeException e) {
+        log.error("Runtime Exception occurred: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e) {
+        log.error("Exception occurred: {}", e.getMessage());
+        return new ErrorResponse(e.getMessage());
+
+    }
+}
