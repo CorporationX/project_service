fe567540cf40c6e2d042692412f4cb8957a73b38
hohlovps95 <wiwisector@gmail.com>
2025-07-20 23:56:40 +0300

    test unit

--- src/test/java/faang.school.projectservice/service/TestServiceTest.java
+++ src/test/java/faang.school.projectservice/service/TestServiceTest.java
@@ -17,7 +17,7 @@ public class TestServiceTest {
     private TestService testService;
 
     @Test
-    @DisplayName("Теста для TestService")
+    @DisplayName("Теста TestService")
     public void exampleTest() {
         assertThat(testService.testMethod(1,2)).isEqualTo(3);
     }
