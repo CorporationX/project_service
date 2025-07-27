2edbeb451053a32bf3e66902ed604d2177fff9d4
mrnght <misterday116@gmail.com>
2025-07-27 18:32:35 +0800

    BJS2-84085 добавил документацию

--- src/main/java/faang/school/projectservice/service/task/TaskService.java
+++ src/main/java/faang/school/projectservice/service/task/TaskService.java
@@ -7,13 +7,45 @@ import faang.school.projectservice.dto.client.task.TaskViewDto;
 
 import java.util.List;
 
+/**
+ * Интерфейс, предоставляющий операции по созданию, обновлению задачи и получению списка задач
+ * с фильтрами (по статусу, исполнителю или ключевому слову), без фильтров и задачи по айди.
+ *
+ * @author mrnght
+ * @since 27.07.2025
+ */
 public interface TaskService {
 
+    /**
+     * Создание задачи
+     *
+     * @param createDto передаваемые данные, необходимые для создания задачи
+     */
     TaskViewDto createTask(TaskCreateDto createDto);
 
+    /**
+     * Обновление проекта
+     *
+     * @param id айди обновляемой задачи
+     * @param updateDto передаваемые данные, необходимые для обновления задачи
+     * @return
+     */
     TaskViewDto updateTask(long id, TaskUpdateDto updateDto);
 
+    /**
+     * Возвращает список задач, соответствующих указанным фильтрам.
+     *
+     * @param taskFilterDto параметры фильтрации
+     * @param projectId айди проекта, для получения его задач
+     * @return список задач в виде List<{@link TaskViewDto}>
+     */
     List<TaskViewDto> getByFilter(TaskFilterDto taskFilterDto, Long projectId);
 
+    /**
+     * Получение задачи по её id
+     *
+     * @param id принимаемый id задачи
+     * @return {@link TaskViewDto} возвращаемая задача
+     */
     TaskViewDto getById(long id);
 }
