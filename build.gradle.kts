595a0a930055fc0b77fe2cfc248b7b154b33f624
Viktor Romanenko <romanenkoviktor080@gmail.com>
2025-08-05 09:33:44 +0300

    kafka configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,12 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     /**
+     * Broker
+     */
+    implementation("org.springframework.kafka:spring-kafka")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+
+    /**
      * Database
      */
     implementation("org.liquibase:liquibase-core")
---
+++ src/main/java/faang/school/projectservice/config/kafka/KafkaTopicConfig.java
@@ -0,0 +1,7 @@
+package faang.school.projectservice.config.kafka;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+}
