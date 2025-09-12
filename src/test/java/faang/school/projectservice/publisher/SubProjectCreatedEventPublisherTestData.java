4281812d549133e06377470f8f036457774b3683
Linempy <mychinnik@yandex.ru>
2025-09-12 13:28:31 +0300

    BJS2-84236-leaderboard - небольшой фикс

---
+++ src/test/java/faang/school/projectservice/publisher/SubProjectCreatedEventPublisherTestData.java
@@ -0,0 +1,12 @@
+package faang.school.projectservice.publisher;
+
+/**
+ * Класс с данными для {@link SubProjectCreatedEventPublisherTest}
+ *
+ * @author Linempy
+ * @since 12.09.2025
+ */
+public class SubProjectCreatedEventPublisherTestData {
+
+    public static final String TEST_TOPIC = "test-topic";
+}
