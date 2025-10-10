fa00d8b2e978d42673342d9e07802fea3d6c53e2
Elya Khamzina <ekkhamzi@mail.ru>
2025-10-10 12:11:31 +0300

    test

--- src/test/java/faang/school/projectservice/FailingTest.java
+++ src/test/java/faang/school/projectservice/FailingTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FailingTest {
+class FailingTest {
 
     @Test
     void comprehensiveFailureDemo() {
