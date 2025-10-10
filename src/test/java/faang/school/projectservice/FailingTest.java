197796658974d5d0b88adcf93c49f42488c10618
Elya Khamzina <ekkhamzi@mail.ru>
2025-10-10 12:42:44 +0300

    BJS2-96612

--- src/test/java/faang/school/projectservice/FailingTest.java
+++ src/test/java/faang/school/projectservice/FailingTest.java
@@ -10,7 +10,7 @@ class FailingTest {
     void comprehensiveFailureDemo() {
 
         String expected = "hello";
-        String actual = "helo";
+        String actual = "hllo";
         assertEquals(expected, actual, "Тест упал");
     }
 }
