6a7702762170d8d879f77b0c39088839502bc1bc
Viks <90530830+flesheatingcat@users.noreply.github.com>
2025-10-12 23:17:27 +0300

    --BJS2-96622 - Project Service Spring Context Test (#1295)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.liquibase:liquibase-core")
     implementation("redis.clients:jedis:4.3.2")
     runtimeOnly("org.postgresql:postgresql")
+    testImplementation("com.h2database:h2")
 
     /**
      * Amazon S3
