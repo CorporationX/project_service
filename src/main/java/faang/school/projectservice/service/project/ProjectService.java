04dc616352967b1514cc0bcb9e3b167746f32a9e
Deathroels <loocyep@gmail.com>
2025-10-15 17:01:04 +0300

    BJS2-96054: -fix create method -add update, getAllProjects method -need to impl deleteProjects, getProjectsByFilter methods

--- src/main/java/faang/school/projectservice/service/project/ProjectService.java
+++ src/main/java/faang/school/projectservice/service/project/ProjectService.java
@@ -4,12 +4,15 @@ import faang.school.projectservice.config.context.UserContext;
 import faang.school.projectservice.dto.project.ProjectCreateDto;
 import faang.school.projectservice.dto.project.ProjectFilterDto;
 import faang.school.projectservice.dto.project.ProjectUpdateDto;
+import faang.school.projectservice.mapper.ProjectMapper;
 import faang.school.projectservice.model.Project;
 import faang.school.projectservice.model.ProjectStatus;
+import faang.school.projectservice.model.ProjectVisibility;
 import faang.school.projectservice.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
@@ -17,9 +20,9 @@ import java.util.Objects;
 @Service
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final UserContext userContext;
 
     public Project createProject(ProjectCreateDto projectCreateDto) {
-        UserContext userContext = new UserContext();
         Long userId = userContext.getUserId();
 
         projectRepository.findAll().stream()
@@ -36,26 +39,43 @@ public class ProjectService {
             throw new IllegalArgumentException("Project description cannot be empty");
         }
 
-        Project project = new Project();
-        project.setName(projectCreateDto.name());
-        project.setDescription(projectCreateDto.description());
-        project.setOwnerId(userId);
-        project.setStatus(ProjectStatus.CREATED);
+        Project project = Project.builder()
+                .name(projectCreateDto.name())
+                .description(projectCreateDto.description())
+                .status(ProjectStatus.CREATED)
+                .ownerId(userId)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .visibility(projectCreateDto.visibility())
+                .build();
 
         return projectRepository.save(project);
     }
 
     public Project updateProject(long projectId, ProjectUpdateDto projectUpdateDto) {
         Project project = getProjectById(projectId);
+        Long userId = userContext.getUserId();
 
+        if (!Objects.equals(userId, project.getOwnerId())) {
+            throw new IllegalArgumentException("You can only update your own projects");
+        }
 
-        return
+        ProjectMapper.updateProjectFields(project, projectUpdateDto);
+        project.setUpdatedAt(LocalDateTime.now());
+
+        return  projectRepository.save(project);
     }
 
     public List<Project> getProjectsByFilter(ProjectFilterDto projectFilterDto, long userId) {
     }
 
-    public List<Project> getAllProjects(long userId) {
+    public List<Project> getAllProjects() {
+        long userId = userContext.getUserId();
+
+        return projectRepository.findAll().stream()
+                .filter(project -> project.getVisibility() == ProjectVisibility.PUBLIC ||
+                        project.getVisibility() == ProjectVisibility.PRIVATE && project.getOwnerId() == userId)
+                .toList();
     }
 
     public Project getProjectById(long projectId) {
@@ -64,5 +84,6 @@ public class ProjectService {
     }
 
     public void deleteProject(long projectId) {
+
     }
 }
