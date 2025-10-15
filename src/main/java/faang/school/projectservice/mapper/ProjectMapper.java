5e4568b067f414a381cf8d12730acdb5e9b66acf
Deathroels <loocyep@gmail.com>
2025-10-15 16:58:21 +0300

    BJS2-96054: -add methods inn mapper, for update

--- src/main/java/faang/school/projectservice/mapper/ProjectMapper.java
+++ src/main/java/faang/school/projectservice/mapper/ProjectMapper.java
@@ -1,11 +1,26 @@
 package faang.school.projectservice.mapper;
 
 import faang.school.projectservice.dto.project.ProjectDto;
+import faang.school.projectservice.dto.project.ProjectUpdateDto;
 import faang.school.projectservice.model.Project;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
+import java.util.function.Consumer;
+
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ProjectMapper {
     ProjectDto toProjectDto(Project project);
+
+    static void updateProjectFields(Project project, ProjectUpdateDto dto) {
+        updateIfNotNull(dto.description(), project::setDescription);
+        updateIfNotNull(dto.status(), project::setStatus);
+        updateIfNotNull(dto.visibility(), project::setVisibility);
+    }
+
+    static <T> void updateIfNotNull(T value, Consumer<T> setter) {
+        if (value != null) {
+            setter.accept(value);
+        }
+    }
 }
