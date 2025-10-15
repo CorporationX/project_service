9fd43aecb0b3367f9d78033946112868931c522e
Deathroels <loocyep@gmail.com>
2025-10-15 15:09:51 +0300

    BJS2-96054: -added and implemented the project creation method

--- src/main/java/faang/school/projectservice/controller/facade/project/ProjectFacade.java
+++ src/main/java/faang/school/projectservice/controller/facade/project/ProjectFacade.java
@@ -1,5 +1,9 @@
 package faang.school.projectservice.controller.facade.project;
 
+import faang.school.projectservice.dto.project.ProjectCreateDto;
+import faang.school.projectservice.dto.project.ProjectDto;
+import faang.school.projectservice.mapper.ProjectMapper;
+import faang.school.projectservice.model.Project;
 import faang.school.projectservice.service.project.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -8,4 +12,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProjectFacade {
     private final ProjectService projectService;
+    private final ProjectMapper projectMapper;
+
+    public ProjectDto createProject(ProjectCreateDto projectCreateDto) {
+        Project project = projectService.createProject(projectCreateDto);
+        return projectMapper.toProjectDto(project);
+    }
 }
--- src/main/java/faang/school/projectservice/service/project/ProjectService.java
+++ src/main/java/faang/school/projectservice/service/project/ProjectService.java
@@ -1,9 +1,38 @@
 package faang.school.projectservice.service.project;
 
+import faang.school.projectservice.config.context.UserContext;
+import faang.school.projectservice.dto.project.ProjectCreateDto;
+import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.ProjectStatus;
+import faang.school.projectservice.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
 public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public Project createProject(ProjectCreateDto projectCreateDto) {
+        UserContext userContext = new UserContext();
+        Long userId = userContext.getUserId();
+
+        projectRepository.findAll().stream()
+                .filter(project -> Objects.equals(project.getOwnerId(), userId))
+                .forEach(project -> {
+                    if (Objects.equals(project.getName(), projectCreateDto.name())) {
+                        throw new IllegalArgumentException("Project with the same name already exists for this user");
+                    }
+                });
+
+        Project project = new Project();
+        project.setName(projectCreateDto.name());
+        project.setDescription(projectCreateDto.description());
+        project.setOwnerId(userId);
+        project.setStatus(ProjectStatus.CREATED);
+
+        return projectRepository.save(project);
+    }
 }
