63419d9ba841216f88ea3b7061e3f5420cdb4389
Rukk Taras <tarrukk@gmail.com>
2025-10-17 21:25:08 +0300

    1. Переделал Controller в RestController

--- src/main/java/faang/school/projectservice/controller/moment/MomentController.java
+++ src/main/java/faang/school/projectservice/controller/moment/MomentController.java
@@ -7,40 +7,56 @@ import faang.school.projectservice.dto.moment.UpdateMomentDto;
 import faang.school.projectservice.service.moment.MomentService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MomentController {
     private final MomentService momentService;
 
-    public MomentDto createMoment(@NonNull CreateMomentDto momentDto) throws Exception {
+    @PostMapping("/moments/newmoment")
+    public MomentDto createMoment(@RequestBody @NonNull CreateMomentDto momentDto) throws Exception {
         return momentService.createMoment(momentDto);
     }
 
-    public MomentDto updateMoment(long momentId, @NonNull UpdateMomentDto momentDto) throws Exception {
+    @PutMapping("/moments/updating")
+    public MomentDto updateMoment(
+            @RequestParam("id") long momentId, @RequestBody @NonNull UpdateMomentDto momentDto) throws Exception {
         return momentService.updateMoment(momentId, momentDto);
     }
 
-    public MomentDto getById(long momentId) {
+    @GetMapping("/moments")
+    public MomentDto getById(@RequestParam("id") long momentId) {
         return momentService.getById(momentId);
     }
 
+    @GetMapping("/moments")
     public List<MomentDto> getAllMoments() {
         return momentService.getAllMoments();
     }
 
-    public List<MomentDto> getMomentsByProjectId(long projectId) {
+    @GetMapping("/moments/byproject")
+    public List<MomentDto> getMomentsByProjectId(@RequestParam("id") long projectId) {
         return momentService.getMomentsByProjectId(projectId);
     }
 
-    public List<MomentDto> getMomentsByMonth(SearchMomentDto searchMomentDto) {
+    @PostMapping("/moments/bymonth")
+    public List<MomentDto> getMomentsByMonth(@RequestBody SearchMomentDto searchMomentDto) {
         return momentService.getMomentsByMonth(searchMomentDto);
     }
 
-    public void deleteById(long momentId){
+    @DeleteMapping("/moments")
+    public void deleteById(@RequestParam("id") long momentId){
         momentService.deleteById(momentId);
     }
 }
