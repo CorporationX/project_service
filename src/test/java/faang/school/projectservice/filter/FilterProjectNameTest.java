87b31b5130085bfffa02f70a4f6ff89b063621ac
Deathroels <loocyep@gmail.com>
2025-10-17 14:31:57 +0300

    BJS2-96054: -added tests too filters

--- src/test/java/faang/school/projectservice/filter/FilterProjectNameTest.java
+++ src/test/java/faang/school/projectservice/filter/FilterProjectNameTest.java
@@ -1,4 +1,64 @@
 package faang.school.projectservice.filter;
 
+import faang.school.projectservice.dto.project.ProjectFilterDto;
+import faang.school.projectservice.model.Project;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FilterProjectNameTest {
+    private FilterProjectName filterProjectName = new FilterProjectName();
+    private ProjectFilterDto projectFilterDto;
+
+    @Test
+    public void isApplication_WhenNameIsNotNull_ShouldReturnTrue() {
+        projectFilterDto = new ProjectFilterDto("test", null);
+        boolean result = filterProjectName.isApplication(projectFilterDto);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void isApplication_WhenNameIsNull_ShouldReturnFalse() {
+        projectFilterDto = new ProjectFilterDto(null, null);
+        boolean result = filterProjectName.isApplication(projectFilterDto);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void apply_WhenProjectsContainName_ShouldReturnFilteredStream() {
+        Project project1 = Project.builder().name("test Project 1").build();
+        Project project2 = Project.builder().name("Project test 2").build();
+        Project project3 = Project.builder().name("Project 3").build();
+        Stream<Project> projects = Stream.of(project1, project2, project3);
+        ProjectFilterDto filterDto = new ProjectFilterDto("test", null);
+
+        Stream<Project> result = filterProjectName.apply(projects, filterDto);
+
+        List<Project> resultList = result.toList();
+        assertEquals(2, resultList.size());
+        assertTrue(resultList.contains(project1));
+        assertTrue(resultList.contains(project2));
+        assertFalse(resultList.contains(project3));
+    }
+
+    @Test
+    void apply_WhenNoProjectsContainName_ShouldReturnEmptyStream() {
+        Project project1 = Project.builder().name("apple-project").build();
+        Project project2 = Project.builder().name("banana-project").build();
+
+        Stream<Project> projects = Stream.of(project1, project2);
+        ProjectFilterDto filterDto = new ProjectFilterDto("orange",null);
+
+        Stream<Project> result = filterProjectName.apply(projects, filterDto);
+
+        List<Project> resultList = result.toList();
+        assertTrue(resultList.isEmpty());
+    }
 }
--- src/test/java/faang/school/projectservice/filter/FilterProjectStatusTest.java
+++ src/test/java/faang/school/projectservice/filter/FilterProjectStatusTest.java
@@ -1,4 +1,67 @@
 package faang.school.projectservice.filter;
 
+import faang.school.projectservice.dto.project.ProjectFilterDto;
+import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.ProjectStatus;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FilterProjectStatusTest {
+    private FilterProjectStatus filterProjectStatus = new FilterProjectStatus();
+    private ProjectFilterDto projectFilterDto;
+
+    @Test
+    public void isApplication_WhenStatusIsNotNull_ShouldReturnTrue() {
+        projectFilterDto = new ProjectFilterDto(null, ProjectStatus.IN_PROGRESS);
+        boolean result = filterProjectStatus.isApplication(projectFilterDto);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void isApplication_WhenStatusIsNull_ShouldReturnFalse() {
+        projectFilterDto = new ProjectFilterDto(null, null);
+        boolean result = filterProjectStatus.isApplication(projectFilterDto);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void apply_WhenProjectsContainStatus_ShouldReturnFilteredStream() {
+        Project project1 = Project.builder().status(ProjectStatus.COMPLETED).build();
+        Project project2 = Project.builder().status(ProjectStatus.CREATED).build();
+        Project project3 = Project.builder().status(ProjectStatus.IN_PROGRESS).build();
+        Project project4 = Project.builder().status(ProjectStatus.ON_HOLD).build();
+        Stream<Project> projects = Stream.of(project1, project2, project3, project4);
+        ProjectFilterDto filterDto = new ProjectFilterDto(null, ProjectStatus.IN_PROGRESS);
+
+        Stream<Project> result = filterProjectStatus.apply(projects, filterDto);
+
+        List<Project> resultList = result.toList();
+        assertEquals(1, resultList.size());
+        assertFalse(resultList.contains(project1));
+        assertFalse(resultList.contains(project2));
+        assertTrue(resultList.contains(project3));
+        assertFalse(resultList.contains(project4));
+    }
+
+    @Test
+    void apply_WhenNoProjectsContainStatus_ShouldReturnEmptyStream() {
+        Project project1 = Project.builder().status(ProjectStatus.COMPLETED).build();
+        Project project2 = Project.builder().status(ProjectStatus.CREATED).build();
+
+        Stream<Project> projects = Stream.of(project1, project2);
+        ProjectFilterDto filterDto = new ProjectFilterDto(null,ProjectStatus.IN_PROGRESS);
+
+        Stream<Project> result = filterProjectStatus.apply(projects, filterDto);
+
+        List<Project> resultList = result.toList();
+        assertTrue(resultList.isEmpty());
+    }
 }
