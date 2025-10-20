f8a1c0d60cabe1e8c0fdf361f4fdbdb88a7f5b18
Kirill Mir <loocyep@gmail.com>
2025-10-20 23:35:21 +0300

    BJS2-96054: -remove unused imports and a bit refactor

--- src/main/java/faang/school/projectservice/service/project/ProjectService.java
+++ src/main/java/faang/school/projectservice/service/project/ProjectService.java
@@ -4,7 +4,6 @@ import faang.school.projectservice.config.context.UserContext;
 import faang.school.projectservice.dto.project.ProjectCreateDto;
 import faang.school.projectservice.dto.project.ProjectFilterDto;
 import faang.school.projectservice.dto.project.ProjectUpdateDto;
-import faang.school.projectservice.exception.DataValidationException;
 import faang.school.projectservice.filter.FilterProject;
 import faang.school.projectservice.mapper.ProjectMapper;
 import faang.school.projectservice.model.Project;
@@ -14,9 +13,7 @@ import faang.school.projectservice.service.project.validator.ProjectValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 @RequiredArgsConstructor
@@ -29,13 +26,7 @@ public class ProjectService {
     public Project createProject(ProjectCreateDto projectCreateDto) {
         Long userId = userContext.getUserId();
 
-        projectRepository.findAll().stream()
-                .filter(project -> Objects.equals(project.getOwnerId(), userId))
-                .forEach(project -> {
-                    if (Objects.equals(project.getName(), projectCreateDto.name())) {
-                        throw new DataValidationException(String.format("Project with the same name already exists for user %d", userId));
-                    }
-                });
+        projectRepository.existsByOwnerIdAndName(userId, projectCreateDto.name());
 
         Project project = ProjectMapper.toEntity(projectCreateDto, userId);
 
@@ -44,7 +35,7 @@ public class ProjectService {
 
     public Project updateProject(long projectId, ProjectUpdateDto projectUpdateDto) {
         Project project = getProjectById(projectId);
-        Long userId = userContext.getUserId();
+        long userId = userContext.getUserId();
 
         ProjectValidator.validateProjectOwner(userId, project);
 
@@ -72,7 +63,7 @@ public class ProjectService {
 
     public void deleteProject(long projectId) {
         long userId = userContext.getUserId();
-        Project project = projectRepository.getById(projectId);
+        Project project = getProjectById(projectId);
 
         ProjectValidator.validateProjectExist(projectId, project);
         ProjectValidator.validateProjectOwner(userId, project);
