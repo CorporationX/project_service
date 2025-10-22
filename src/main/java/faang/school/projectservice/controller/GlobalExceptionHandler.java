47f9067f3ade9ef54b84fa59754bed0095df039d
Rukk Taras <tarrukk@gmail.com>
2025-10-22 12:43:56 +0300

    Разработал систему обработки ошибок.

--- src/main/java/faang/school/projectservice/controller/GlobalExceptionHandler.java
+++ src/main/java/faang/school/projectservice/controller/GlobalExceptionHandler.java
@@ -5,9 +5,15 @@ import faang.school.projectservice.exception.EntityNotFoundException;
 import faang.school.projectservice.exception.ErrorResponse;
 import faang.school.projectservice.exception.ForbiddenException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,6 +22,16 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleDataValidationException(DataValidationException e) {
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError) error).getField(),
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), ""))
+                );
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
