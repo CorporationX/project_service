3ebac1f3aee256760d924d982eb230272220b504
Asta <charlyfazbear@gmail.com>
2025-10-23 22:49:07 +0300

    -made corrections in class GlobalExceptionHandler and ProjectController

--- src/main/java/faang/school/projectservice/controller/exeption/GlobalExceptionHandler.java
+++ src/main/java/faang/school/projectservice/controller/exeption/GlobalExceptionHandler.java
@@ -1,24 +1,22 @@
-package faang.school.projectservice.controller;
+package faang.school.projectservice.controller.exeption;
 
 import faang.school.projectservice.exception.project.DuplicateResourceException;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(DuplicateResourceException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    static ErrorResponse handleDuplicateResource(DuplicateResourceException ex) {
-        return new ErrorResponse(ex.getMessage());
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class ErrorResponse {
-        private String error;
+    static Map<String, String> handleDuplicateResource(DuplicateResourceException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("error", ex.getMessage());
+        return errorMap;
     }
 }
