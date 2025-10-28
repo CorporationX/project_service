6a67ddacb8d2962be69e925220f7afe85aabe26a
Deathroels <loocyep@gmail.com>
2025-10-28 16:47:30 +0300

    BJS2-96093: -work on ResourceDto, add validation

--- src/main/java/faang/school/projectservice/dto/resource/ResourceDto.java
+++ src/main/java/faang/school/projectservice/dto/resource/ResourceDto.java
@@ -1,14 +1,30 @@
 package faang.school.projectservice.dto.resource;
 
 import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.ResourceStatus;
+import faang.school.projectservice.model.ResourceType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record ResourceDto(
-        Long id,
+        @NotBlank
         String name,
+
+        @NotBlank
         String key,
+
+        @NotNull
+        @Positive
         Long size,
-        String type,
-        String status,
+
+        @NotNull
+        ResourceType type,
+
+        @NotNull
+        ResourceStatus status,
+
+        @NotNull
         Project project
 ) {
 }
