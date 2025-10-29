7a31721ff970d050f292f9e057e17efa117f0de9
leto007 <liga07@bk.ru>
2025-10-30 01:08:44 +0300

    Realize Vacancy hippogriff12/leto007/BJS2-96262-Vacancy

--- src/main/java/faang/school/projectservice/client/PaymentServiceClient.java
+++ src/main/java/faang/school/projectservice/client/PaymentServiceClient.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @FeignClient(name = "payment-service", url = "${services.payment-service.host}:${services.payment-service.port}")
 public interface PaymentServiceClient {
 
