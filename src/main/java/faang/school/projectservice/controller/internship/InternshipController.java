5c08780723d1aded65a4b24c97db29c291698c0b
Monkeylust <mo9keylust@mail.ru>
2025-10-29 23:42:24 +0300

    BJS2-96258 // In progress

--- src/main/java/faang/school/projectservice/controller/internship/InternshipController.java
+++ src/main/java/faang/school/projectservice/controller/internship/InternshipController.java
@@ -1,4 +1,24 @@
 package faang.school.projectservice.controller.internship;
 
+import faang.school.projectservice.dto.internship.CreateInternshipDto;
+import faang.school.projectservice.dto.internship.InternshipDto;
+import faang.school.projectservice.service.internship.InternshipService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/internships")
+@RequiredArgsConstructor
 public class InternshipController {
+
+    private final InternshipService internshipService;
+
+    @PostMapping
+    public InternshipDto addInternship(@Valid @RequestBody CreateInternshipDto createInternshipDto) {
+        return internshipService.createInternship(createInternshipDto);
+    }
 }
