acdbcd6c89217a319c07b05878b3ca901274ca65
Monkeylust <mo9keylust@mail.ru>
2025-11-04 21:08:17 +0300

    BJS2-96258 // In progress

---
+++ src/test/java/faang/school/projectservice/service/internship/InternshipServiceTest.java
@@ -0,0 +1,13 @@
+package faang.school.projectservice.service.internship;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+class InternshipServiceTest {
+
+    @InjectMocks
+    private InternshipService internshipService;
+
+    @Test
+    void
+}
