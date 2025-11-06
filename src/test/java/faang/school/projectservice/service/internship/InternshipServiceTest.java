a713f79552f43d430596135fd386b66817ce9e41
Monkeylust <mo9keylust@mail.ru>
2025-11-07 01:49:58 +0300

    BJS2-96258 // In progress

--- src/test/java/faang/school/projectservice/service/internship/InternshipServiceTest.java
+++ src/test/java/faang/school/projectservice/service/internship/InternshipServiceTest.java
@@ -1,20 +1,30 @@
 package faang.school.projectservice.service.internship;
 
 import faang.school.projectservice.dto.internship.CreateInternshipDto;
+import faang.school.projectservice.dto.internship.InternshipDto;
 import faang.school.projectservice.mapper.internship.InternshipDtoMapper;
+import faang.school.projectservice.model.Internship;
 import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.Team;
+import faang.school.projectservice.model.TeamMember;
 import faang.school.projectservice.repository.InternshipRepository;
 import faang.school.projectservice.repository.ProjectRepository;
 import faang.school.projectservice.repository.TeamMemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import static faang.school.projectservice.model.InternshipStatus.IN_PROGRESS;
 import static faang.school.projectservice.model.TeamRole.OWNER;
@@ -28,8 +38,9 @@ class InternshipServiceTest {
     @Mock
     private InternshipRepository internshipRepository;
 
-    @Mock
-    private InternshipDtoMapper internshipDtoMapper;
+    @Spy
+    private InternshipDtoMapper internshipDtoMapper =
+            Mappers.getMapper(InternshipDtoMapper.class);
 
     @Mock
     private ProjectRepository projectRepository;
@@ -38,7 +49,7 @@ class InternshipServiceTest {
     private TeamMemberRepository teamMemberRepository;
 
     @Test
-    public void testProjectFound_Success() {
+    public void testCreateInternship_Success() {
         CreateInternshipDto dto = new CreateInternshipDto(
                 "Yandex",
                 "I'm gay",
@@ -51,9 +62,106 @@ class InternshipServiceTest {
                 Collections.singletonList(3L)
         );
 
-        Mockito.when(projectRepository.findByIdOrThrow(1L)).thenReturn(new Project());
-        internshipService.createInternship(dto);
+        TeamMember mentor = new TeamMember();
+
+        Team team = new Team();
+        team.setTeamMembers(List.of(mentor));
+
+        List<Team> teams = new ArrayList<>();
+        teams.add(team);
+
+        Project project = new Project();
+        project.setTeams(teams);
+
+        TeamMember intern = new TeamMember();
+
+        List<TeamMember> interns = new ArrayList<>();
+        interns.add(intern);
+
+        Mockito.when(projectRepository.findByIdOrThrow(1L)).thenReturn(project);
+        Mockito.when(teamMemberRepository.findMentorByIdOrThrow(2L)).thenReturn(mentor);
+        Mockito.when(teamMemberRepository.findAllById(dto.internsIds())).thenReturn(interns);
+        Mockito.when(internshipRepository.save(Mockito.any(Internship.class)))
+                .thenAnswer(invocationOnMock -> {
+                    return invocationOnMock.getArgument(0);
+                });
+
+        InternshipDto result = internshipService.createInternship(dto);
+
+        Assertions.assertEquals("I'm gay", result.description());
+        Assertions.assertDoesNotThrow(() -> internshipService.createInternship(dto));
     }
 
+    @Test
+    public void testValidateInternsNotEmpty_Null() {
+        CreateInternshipDto dto = new CreateInternshipDto(
+                "Yandex",
+                "I'm gay",
+                IN_PROGRESS,
+                OWNER,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(1),
+                1L,
+                2L,
+                Collections.singletonList(3L)
+        );
 
+        TeamMember mentor = new TeamMember();
+
+        Team team = new Team();
+        team.setTeamMembers(List.of(mentor));
+
+        List<Team> teams = new ArrayList<>();
+        teams.add(team);
+
+        Project project = new Project();
+        project.setTeams(teams);
+
+        TeamMember intern = new TeamMember();
+
+        List<TeamMember> interns = new ArrayList<>();
+        interns.add(intern);
+
+        Mockito.when(projectRepository.findByIdOrThrow(1L)).thenReturn(project);
+        Mockito.when(teamMemberRepository.findMentorByIdOrThrow(2L)).thenReturn(mentor);
+        Mockito.when(teamMemberRepository.findAllById(dto.internsIds())).thenReturn(null);
+
+        Assertions.assertThrows(EntityNotFoundException.class,
+                () -> internshipService.createInternship(dto));
+    }
+
+    @Test
+    public void testValidateInternsNotEmpty_Empty() {
+        CreateInternshipDto dto = new CreateInternshipDto(
+                "Yandex",
+                "I'm gay",
+                IN_PROGRESS,
+                OWNER,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(1),
+                1L,
+                2L,
+                Collections.singletonList(3L)
+        );
+
+        TeamMember mentor = new TeamMember();
+
+        Team team = new Team();
+        team.setTeamMembers(List.of(mentor));
+
+        List<Team> teams = new ArrayList<>();
+        teams.add(team);
+
+        Project project = new Project();
+        project.setTeams(teams);
+
+        List<TeamMember> interns = new ArrayList<>();
+
+        Mockito.when(projectRepository.findByIdOrThrow(1L)).thenReturn(project);
+        Mockito.when(teamMemberRepository.findMentorByIdOrThrow(2L)).thenReturn(mentor);
+        Mockito.when(teamMemberRepository.findAllById(dto.internsIds())).thenReturn(interns);
+
+        Assertions.assertThrows(EntityNotFoundException.class,
+                () -> internshipService.createInternship(dto));
+    }
 }
