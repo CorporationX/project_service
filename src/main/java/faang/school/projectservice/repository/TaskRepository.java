8fd771264421c3af22c9634482825c731d806638
Monkeylust <mo9keylust@mail.ru>
2025-11-09 22:44:17 +0300

    BJS2-96258 // In progress

--- src/main/java/faang/school/projectservice/repository/TaskRepository.java
+++ src/main/java/faang/school/projectservice/repository/TaskRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByPerformerUserId(Long performerUserId);
+
     List<Task> findAllByProjectId(Long projectId);
 }
