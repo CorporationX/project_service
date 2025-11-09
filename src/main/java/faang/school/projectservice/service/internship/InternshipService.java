e2f12aeb25083e445741849c73e9440517a5c2fd
Monkeylust <mo9keylust@mail.ru>
2025-11-09 22:44:17 +0300

    BJS2-96258 // In progress

--- src/main/java/faang/school/projectservice/service/internship/InternshipService.java
+++ src/main/java/faang/school/projectservice/service/internship/InternshipService.java
@@ -3,19 +3,26 @@ package faang.school.projectservice.service.internship;
 import faang.school.projectservice.dto.internship.CreateInternshipDto;
 import faang.school.projectservice.dto.internship.InternshipDto;
 import faang.school.projectservice.dto.internship.UpdateInternshipDto;
+import faang.school.projectservice.exception.TasksValidationException;
 import faang.school.projectservice.mapper.internship.InternshipDtoMapper;
 import faang.school.projectservice.mapper.internship.InternshipMapper;
 import faang.school.projectservice.model.Internship;
+import faang.school.projectservice.model.InternshipStatus;
 import faang.school.projectservice.model.Project;
+import faang.school.projectservice.model.Task;
+import faang.school.projectservice.model.TaskStatus;
 import faang.school.projectservice.model.TeamMember;
+import faang.school.projectservice.model.TeamRole;
 import faang.school.projectservice.repository.InternshipRepository;
 import faang.school.projectservice.repository.ProjectRepository;
+import faang.school.projectservice.repository.TaskRepository;
 import faang.school.projectservice.repository.TeamMemberRepository;
 import faang.school.projectservice.validator.internship.InternshipValidator;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -26,6 +33,7 @@ public class InternshipService {
     private final InternshipDtoMapper internshipDtoMapper;
     private final ProjectRepository projectRepository;
     private final TeamMemberRepository teamMemberRepository;
+    private final TaskRepository taskRepository;
 
     public InternshipDto createInternship(CreateInternshipDto createInternshipDto) {
 
@@ -49,8 +57,17 @@ public class InternshipService {
         return internshipDtoMapper.toInternshipDto(internship);
     }
 
-    public InternshipDto updateInternship(UpdateInternshipDto updateInternshipDto) {
+    public InternshipDto updateInternship(long internshipId, UpdateInternshipDto uID) {
+        Internship internship = internshipRepository.findByIdOrThrow(internshipId);
 
+        if (uID.status() == InternshipStatus.COMPLETED) {
+            handleInternshipCompletion(internship);
+        } else {
+            InternshipMapper.update(uID, internship);
+        }
+
+        internship = internshipRepository.save(internship);
+        return internshipDtoMapper.toInternshipDto(internship);
     }
 
     private void validateMentorBelongsToProject(Project project, TeamMember mentor) {
@@ -65,8 +82,26 @@ public class InternshipService {
     }
 
     private void validateInternsNotEmpty(List<TeamMember> interns) {
-        if(interns == null || interns.isEmpty()) {
+        if (interns == null || interns.isEmpty()) {
             throw new EntityNotFoundException("Interns are missing.");
         }
+    }
+
+    private void handleInternshipCompletion(Internship internship) {
+        List<TeamMember> interns = internship.getInterns();
+        for (TeamMember intern : interns) {
+            List<Task> tasks = taskRepository.findByPerformerUserId(intern.getId());
+
+            if (!areAllTasksCompleted(tasks)) {
+                throw new TasksValidationException("Not all tasks were completed by intern: %s".formatted(intern.getNickname()));
+            }
+        }
+        for (TeamMember intern : interns) {
+            intern.setRoles(Collections.singletonList(TeamRole.DEVELOPER));
+        }
+    }
+
+    private boolean areAllTasksCompleted(List<Task> tasks) {
+        return tasks.stream().allMatch(task -> task.getStatus() == TaskStatus.DONE);
     }
 }
