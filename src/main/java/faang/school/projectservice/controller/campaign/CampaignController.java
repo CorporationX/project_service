7f7899068dbe1257d5d9ed7c982196d2d49e2efa
teymuruae <baratov.teymur@yandex.ru>
2025-11-09 21:34:57 +0300

    BJS2-96787 (campaign). review fixes

--- src/main/java/faang/school/projectservice/controller/campaign/CampaignController.java
+++ src/main/java/faang/school/projectservice/controller/campaign/CampaignController.java
@@ -9,12 +9,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +39,7 @@ public class CampaignController {
         return ResponseEntity.ok(campaignService.update(campaignId, updateCampaignDto));
     }
 
-    @PutMapping("/{campaignId}/softDelete")
+    @DeleteMapping("/{campaignId}")
     public void softDelete(@PathVariable Long campaignId) {
         campaignService.softDelete(campaignId);
     }
