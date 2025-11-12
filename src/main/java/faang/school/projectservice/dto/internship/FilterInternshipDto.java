e8428a03097a42857de24882957e52e5e811eb7c
p.dmitrenko <mo9keylust@mail.ru>
2025-11-12 17:22:19 +0300

    BJS2-96258 // in progress.

--- src/main/java/faang/school/projectservice/dto/internship/FilterInternshipDto.java
+++ src/main/java/faang/school/projectservice/dto/internship/FilterInternshipDto.java
@@ -1,11 +1,14 @@
 package faang.school.projectservice.dto.internship;
 
+import faang.school.projectservice.model.Internship;
 import faang.school.projectservice.model.InternshipStatus;
 import faang.school.projectservice.model.TeamMember;
+import jakarta.annotation.Nullable;
 
+@Nullable
 public record FilterInternshipDto(
         InternshipStatus status,
         TeamMember role,
-
+        Internship id
 ) {
 }
