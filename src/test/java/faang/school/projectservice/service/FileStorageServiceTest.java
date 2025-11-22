e3027cf381e8a0b191730ef3b8be528f83ae7b5d
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-11-22 21:42:00 +0100

    fix

--- src/test/java/faang/school/projectservice/service/FileStorageServiceTest.java
+++ src/test/java/faang/school/projectservice/service/FileStorageServiceTest.java
@@ -44,13 +44,17 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
