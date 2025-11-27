ccbc06c9f677e5e8f043de449b99bca310fc5afb
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-11-27 17:26:51 +0100

    migration fix

--- src/main/resources/db/changelog/changeset/project_V022__alter_resources.sql
+++ src/main/resources/db/changelog/changeset/project_V022__alter_resources.sql
@@ -1,45 +1,29 @@
 ALTER TABLE project_resource
-ADD COLUMN IF NOT EXISTS created_by BIGINT,
-ADD COLUMN IF NOT EXISTS updated_by BIGINT;
+ADD CONSTRAINT fk_resource_created_by 
+FOREIGN KEY (created_by) REFERENCES team_member(id);
 
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_constraint 
-        WHERE conname = 'fk_resource_created_by'
-    ) THEN
-        ALTER TABLE project_resource
-        ADD CONSTRAINT fk_resource_created_by 
-        FOREIGN KEY (created_by) REFERENCES team_member(id);
-    END IF;
-    
-    IF NOT EXISTS (
-        SELECT 1 FROM pg_constraint 
-        WHERE conname = 'fk_resource_updated_by'
-    ) THEN
-        ALTER TABLE project_resource
-        ADD CONSTRAINT fk_resource_updated_by 
-        FOREIGN KEY (updated_by) REFERENCES team_member(id);
-    END IF;
-END $$;
+ALTER TABLE project_resource
+ADD CONSTRAINT fk_resource_updated_by 
+FOREIGN KEY (updated_by) REFERENCES team_member(id);
 
 UPDATE project_resource 
-SET created_by = (SELECT id FROM team_member LIMIT 1)
-WHERE created_by IS NULL 
-AND EXISTS (SELECT 1 FROM team_member);
+SET created_by = (
+    SELECT tm.id 
+    FROM team_member tm
+    JOIN team t ON t.id = tm.team_id
+    WHERE t.project_id = project_resource.project_id
+    LIMIT 1
+)
+WHERE created_by IS NULL
+AND EXISTS (
+    SELECT 1 
+    FROM team_member tm
+    JOIN team t ON t.id = tm.team_id
+    WHERE t.project_id = project_resource.project_id
+);
 
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT 1 FROM information_schema.columns 
-        WHERE table_name = 'project_resource' 
-        AND column_name = 'created_by'
-        AND is_nullable = 'YES'
-    ) THEN
-        ALTER TABLE project_resource
-        ALTER COLUMN created_by SET NOT NULL;
-    END IF;
-END $$;
+ALTER TABLE project_resource
+ALTER COLUMN created_by SET NOT NULL;
 
 CREATE INDEX IF NOT EXISTS idx_resource_project ON project_resource(project_id);
 CREATE INDEX IF NOT EXISTS idx_resource_status ON project_resource(status);
