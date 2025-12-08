3e2fbf2e0031aef911d9c5f631a8b9862144a221
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-12-08 19:11:29 +0100

    fix

--- src/test/java/faang/school/projectservice/integration/jira/queue/RabbitMQIntegrationTest.java
+++ src/test/java/faang/school/projectservice/integration/jira/queue/RabbitMQIntegrationTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
+import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -103,6 +104,9 @@ class RabbitMQIntegrationTest {
     
     @MockBean
     private JiraRestClient jiraRestClient;
+    
+    @MockBean
+    private MinioClient minioClient;
     
     @Autowired
     private TaskRepository taskRepository;
